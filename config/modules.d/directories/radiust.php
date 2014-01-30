0d060dd984c0a1b07fc0022209f1862f1ecf6ac3
nightflyza <nightfly@email.ua>
2014-01-30 02:59:28 +0200

    big cleanup

--- config/modules.d/directories/radiust.php
+++
@@ -1,3 +0,0 @@
-if ($alter_conf['RADIUST_ENABLED']) {
-$taskbar.=build_task('RADIUST','?module=radiust','radius.gif',__('Radius attribute templates'));
-}
--- modules/general/radiust/module.php
+++
@@ -1,3 +0,0 @@
-<?php
-$this->registerModule($module, 'main', __('Control radius templates'), 'Nightfly', array('RADIUST' => __('right to control radius templates')));
-?>
