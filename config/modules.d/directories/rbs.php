8724e466626f793f11aa686a9b35b0b3c85e0b65
nightflyza <nightfly@email.ua>
2014-09-09 09:09:25 +0300

    0.5.8 rev 3703

--- config/modules.d/directories/rbs.php
+++ config/modules.d/directories/rbs.php
@@ -1,3 +1,5 @@
-if ( $alter_conf['RBS_ENABLED'] ) {
+if(isset($alter_conf['RBS_ENABLED'])) {
+ if ( $alter_conf['RBS_ENABLED'] ) {
   $taskbar .= build_task('RBS','?module=rbs','rbs.png',__('Registry of banned sites'));
+ }
 }
