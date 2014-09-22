9ede58cb97e6aa7b43e764b9a4aadaff893d73a1
nightflyza <nightfly@email.ua>
2014-09-22 14:44:35 +0300

    0.5.8 rev 3789

--- userstats/modules/general/traffic/index.php
+++ userstats/modules/general/traffic/index.php
@@ -1,8 +1,12 @@
 <?php
 
-$user_ip=zbs_UserDetectIp('debug');
-$user_login=zbs_UserGetLoginByIp($user_ip);
+$usConfig = zbs_LoadConfig();
+if ($usConfig['TRAFFIC_ENABLED']) {
+    $user_ip = zbs_UserDetectIp('debug');
+    $user_login = zbs_UserGetLoginByIp($user_ip);
 
-show_window(__('Traffic stats'),zbs_UserTraffStats($user_login));
-
+    show_window(__('Traffic stats'), zbs_UserTraffStats($user_login));
+} else {
+    show_window(__('Sorry'), __('This module is disabled'));
+}
 ?>
