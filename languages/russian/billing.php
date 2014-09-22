5ade77b1788f3b790e3d9ea11e3f7d92f71e35c4
nightflyza <nightfly@email.ua>
2014-09-22 16:18:18 +0300

    0.5.8 rev 3790

--- languages/russian/billing.php
+++ languages/russian/billing.php
@@ -1740,11 +1740,6 @@ $lang['def']['Additional monitoring']='Дополнительный монито
 $lang['def']['Online module saves state']='Модуль Онлайн сохраняет свое состояние';
 $lang['def']['Show SC shaper info']='Показать информацию шейпера SC';
 $lang['def']['right to show sc info for user']='Право просматривать инфо sc для пользователя';
-$lang['def']['']='';
-$lang['def']['']='';
-$lang['def']['']='';
-$lang['def']['']='';
-$lang['def']['']='';
-$lang['def']['']='';
+
 
 ?>
