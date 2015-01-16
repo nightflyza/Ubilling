8d6ae08ae0c259975aebe6922e2ffcfa6b5a37da
nightflyza <nightfly@email.ua>
2015-01-16 09:09:22 +0200

    0.6.3 rev 4111

---
+++ languages/russian/vlan.php
@@ -0,0 +1,10 @@
+<?
+$lang['def']['First Vlan'] = 'Первый VLAN';
+$lang['def']['Last Vlan'] = 'Последний VLAN';
+$lang['def']['Create vlan pool'] = 'Создать пул Vlan-ов';
+$lang['def']['Use qinq'] = 'Использовать qinq';
+$lang['def']['Vlan assigment'] = 'Присвоение VLAN-а';
+$lang['def']['Current user Vlan'] = 'Текущий VLAN пользователя';
+$lang['def']['Change user Vlan'] = 'Изменить VLAN пользователя';
+$lang['def']['Delete user Vlan'] = 'Удалить VLAN пользователя';
+?>
---
+++ languages/ukrainian/vlan.php
@@ -0,0 +1,10 @@
+<?
+$lang['def']['First Vlan'] = 'Перший VLAN';
+$lang['def']['Last Vlan'] = 'Останній VLAN';
+$lang['def']['Create vlan pool'] = 'Створити пул Vlan-ів';
+$lang['def']['Use qinq'] = 'Використовувати qinq';
+$lang['def']['Vlan assigment'] = 'Призначення VLAN-а';
+$lang['def']['Current user Vlan'] = 'Поточний VLAN пользователя';
+$lang['def']['Change user Vlan'] = 'Змінити VLAN пользователя';
+$lang['def']['Delete user Vlan'] = 'Видалити VLAN пользователя';
+?>
