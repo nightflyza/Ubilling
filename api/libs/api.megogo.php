6417f5606b8258e761c0833d0d1da07f46b06bf7
nightflyza <nightfly@email.ua>
2015-11-25 09:09:24 +0200

    0.7.3 rev 4889

--- api/libs/api.megogo.php
+++ api/libs/api.megogo.php
@@ -8,10 +8,41 @@ class MegogoApi {
      * @var array
      */
     protected $altCfg = array();
+
+    /**
+     * Partner ID property via MG_PARTNERID
+     *
+     * @var string
+     */
     protected $partnerId = '';
+
+    /**
+     * Users ID prefixes via MG_PREFIX
+     *
+     * @var string
+     */
     protected $prefix = '';
+
+    /**
+     * Auth salt value via MG_SALT
+     *
+     * @var string
+     */
     protected $salt = '';
+
+    /**
+     * subscribe/unsubscribe API URL
+     *
+     * @var string 
+     */
     protected $urlApi = '';
+
+    /**
+     * Authorization API URL
+     *
+     * @var string
+     */
+    protected $urlAuth = '';
 
     public function __construct() {
         $this->loadAlter();
@@ -38,6 +69,7 @@ class MegogoApi {
         $this->prefix = $this->altCfg['MG_PREFIX'];
         $this->salt = $this->altCfg['MG_SALT'];
         $this->urlApi = 'http://billing.megogo.net/partners/';
+        $this->urlAuth = 'http://megogo.net/auth/by_partners/';
     }
 
     /**
@@ -49,11 +81,57 @@ class MegogoApi {
      * @return bool
      */
     public function subscribe($login, $service) {
-        $result=true;
-        
+        $result = false;
+        $query = $this->urlApi . $this->partnerId . '/subscription/subscribe?userId=' . $this->prefix . $login . '&serviceId=' . $service;
+        $queryResult = file_get_contents($query);
+        if (!empty($queryResult)) {
+            $queryResult = json_decode($queryResult);
+            if ($queryResult->successful) {
+                $result = true;
+            }
+        }
         return ($result);
     }
 
+    /**
+     * Unsubscribes user for some service
+     * 
+     * @param string $login Existing user login to subscribe
+     * @param string $service Valid serviceid
+     * 
+     * @return bool
+     */
+    public function unsubscribe($login, $service) {
+        $result = false;
+        $query = $this->urlApi . $this->partnerId . '/subscription/unsubscribe?userId=' . $this->prefix . $login . '&serviceId=' . $service;
+        $queryResult = file_get_contents($query);
+        if (!empty($queryResult)) {
+            $queryResult = json_decode($queryResult);
+            if ($queryResult->successful) {
+                $result = true;
+            }
+        }
+        return ($result);
+    }
+
+    /**
+     * Returns auth codes
+     * 
+     * @param string $login Existing user login
+     * @return strig
+     */
+    public function authCode($login) {
+        $result = '';
+        $hashData = $this->prefix . $login . $this->partnerId . $this->salt;
+        $token = md5($hashData);
+        $result = $this->urlAuth . 'dialog?isdn=' . $this->prefix . $login . '&partner_key=' . $this->partnerId . '&token=' . $token;
+        return ($result);
+    }
+
+}
+
+class MegogoInterface {
+    
 }
 
 ?>
