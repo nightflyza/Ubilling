c60580b41ca655b0662e0ccee541ebf897dc7457
nightflyza <nightfly@email.ua>
2017-02-04 13:23:12 +0200

    0.8.2 rev 5334

--- api/api.stargazer.php
+++ api/api.stargazer.php
@@ -1,120 +1,254 @@
 <?php
 
-define('BILLING_CONFIG','config/billing.ini');
-$billing_config=parse_ini_file(BILLING_CONFIG);
-include($billing_config['baseconf'].'/handlers.php');
+define('BILLING_CONFIG', 'config/billing.ini');
+$billing_config = parse_ini_file(BILLING_CONFIG);
+include($billing_config['baseconf'] . '/handlers.php');
 
-/*
+/**
  * Low level billing operations wrapper
  * Class just calls ultra low handlers
  */
-
 class ApiBilling {
 
+    /**
+     * Creates some stargazer user
+     * 
+     * @param string $login
+     * 
+     * @return void
+     */
     function createuser($login) {
-        $login=trim($login);
+        $login = trim($login);
         billing_createuser($login);
     }
 
-    function addcash($login,$cash) {
-        $login=trim($login);
-        $cash=trim($cash);
+    /**
+     * Adds cash to stargazer user account
+     * 
+     * @param string $login
+     * @param float $cash
+     * 
+     * @return void
+     */
+    function addcash($login, $cash) {
+        $login = trim($login);
+        $cash = trim($cash);
         billing_addcash($login, $cash);
     }
-    
-    function setcash($login,$cash) {
-         $login=trim($login);
-         $cash=trim($cash);
-         billing_setcash($login, $cash);
+
+    /**
+     * Sets user account cash to some value
+     * 
+     * @param string $login
+     * @param float $cash
+     * 
+     * @return void
+     */
+    function setcash($login, $cash) {
+        $login = trim($login);
+        $cash = trim($cash);
+        billing_setcash($login, $cash);
     }
 
-    function setcredit($login,$credit) {
-         $login=trim($login);
-         $credit=trim($credit);
-         billing_setcredit($login, $credit);
+    /**
+     * Sets credit for some user account
+     * 
+     * @param string $login
+     * @param float $credit
+     */
+    function setcredit($login, $credit) {
+        $login = trim($login);
+        $credit = trim($credit);
+        billing_setcredit($login, $credit);
     }
 
-    function setcreditexpire($login,$creditexpire) {
-        $login=trim($login);
-        $creditexpire=trim($creditexpire);
+    /**
+     * Sets credit expiration date
+     * 
+     * @param string $login
+     * @param string $creditexpire
+     * 
+     * @return void
+     */
+    function setcreditexpire($login, $creditexpire) {
+        $login = trim($login);
+        $creditexpire = trim($creditexpire);
         billing_setcreditexpire($login, $creditexpire);
     }
 
+    /**
+     * Performs stargazer user reinit
+     * 
+     * @param string $login
+     * 
+     * @return void
+     */
     function resetuser($login) {
-        $login=trim($login);
+        $login = trim($login);
         billing_resetuser($login);
     }
 
-    function setao($login,$state) {
-        $login=trim($login);
-        $state=trim($state);
+    /**
+     * Sets AlwaysOnline flag to existing user
+     * 
+     * @param string $login
+     * @param int $state
+     * 
+     * @return void
+     */
+    function setao($login, $state) {
+        $login = trim($login);
+        $state = trim($state);
         billing_setao($login, $state);
     }
 
-    function setdstat($login,$state) {
-        $login=trim($login);
-        $state=trim($state);
+    /**
+     * Sets DisabledDetailStat flag to existing user
+     * 
+     * @param string $login
+     * @param int $state
+     * 
+     * @return void
+     */
+    function setdstat($login, $state) {
+        $login = trim($login);
+        $state = trim($state);
         billing_setdstat($login, $state);
     }
 
-    function setip($login,$ip) {
-        $login=trim($login);
-        $ip=trim($ip);
+    /**
+     * Sets IP for some staragazer user login
+     * 
+     * @param string $login
+     * @param string $ip
+     * 
+     * @return void
+     */
+    function setip($login, $ip) {
+        $login = trim($login);
+        $ip = trim($ip);
         billing_setip($login, $ip);
     }
 
-    function setpassword($login,$password) {
-        $login=trim($login);
-        $password=trim($password);
+    /**
+     * Sets password for existing user
+     * 
+     * @param string $login
+     * @param string $password
+     * 
+     * @return void
+     */
+    function setpassword($login, $password) {
+        $login = trim($login);
+        $password = trim($password);
         billing_setpassword($login, $password);
     }
 
-    function settariff($login,$tariff) {
-        $login=trim($login);
-        $tariff=trim($tariff);
+    /**
+     * Changes tariff right now for some user
+     * 
+     * @param string $login
+     * @param string $tariff
+     * 
+     * @return void
+     */
+    function settariff($login, $tariff) {
+        $login = trim($login);
+        $tariff = trim($tariff);
         billing_settariff($login, $tariff);
     }
 
-    function settariffnm($login,$tariff) {
-        $login=trim($login);
-        $tariff=trim($tariff);
+    /**
+     * Sets TariffChange for next month
+     * 
+     * @param string $login
+     * @param string $tariff
+     * 
+     * @return void
+     */
+    function settariffnm($login, $tariff) {
+        $login = trim($login);
+        $tariff = trim($tariff);
         billing_settariffnm($login, $tariff);
     }
 
-    function setdown($login,$state) {
-        $login=trim($login);
-        $state=trim($state);
+    /**
+     * Sets Down flag to existing user
+     * 
+     * @param string $login
+     * @param int $state
+     * 
+     * @return void
+     */
+    function setdown($login, $state) {
+        $login = trim($login);
+        $state = trim($state);
         billing_setdown($login, $state);
     }
 
-    function setpassive($login,$state) {
-        $login=trim($login);
-        $state=trim($state);
+    /**
+     * Sets Passive aka Frozen flag to existing user
+     * 
+     * @param string $login
+     * @param int $state
+     * 
+     * @return void
+     */
+    function setpassive($login, $state) {
+        $login = trim($login);
+        $state = trim($state);
         billing_setpassive($login, $state);
     }
 
+    /**
+     * Deletes existing stargazer user
+     * 
+     * @param string $login
+     * 
+     * @return void
+     */
     function deleteuser($login) {
-        $login=trim($login);
+        $login = trim($login);
         billing_deleteuser($login);
     }
 
+    /**
+     * Creates new stargazer tariff
+     * 
+     * @param string $tariff
+     * 
+     * @return void
+     */
     function createtariff($tariff) {
-        $tariff=trim($tariff);
+        $tariff = trim($tariff);
         billing_createtariff($tariff);
     }
 
+    /**
+     * Deletes existing stargazer tariff
+     * 
+     * @param string $tariff
+     * 
+     * @return void
+     */
     function deletetariff($tariff) {
-        $tariff=trim($tariff);
+        $tariff = trim($tariff);
         billing_deletetariff($tariff);
     }
 
+    /**
+     * Changes existing stargazer tariff options
+     * 
+     * @param string $tariff
+     * @param array $options
+     * 
+     * @return void
+     */
     function edittariff($tariff, $options) {
-        $tariff=trim($tariff);
+        $tariff = trim($tariff);
         billing_edittariff($tariff, $options);
     }
 
-    
-    
 }
 
 ?>
--- modules/general/annihilation/index.php
+++ modules/general/annihilation/index.php
@@ -1,13 +1,25 @@
 <?php
+
 // check for right of current admin on this module
 if (cfr('ANNIHILATION')) {
-   
+
+    /**
+     * Deletes existing user
+     * 
+     * @global object $billing
+     * @param string $login
+     * 
+     * @return void
+     */
     function zb_AnnihilateUser($login) {
         global $billing;
-        $alter_conf=rcms_parse_ini_file(CONFIG_PATH."alter.ini");
-        $user_ip=zb_UserGetIP($login);
-        $user_aptdata=zb_AddressGetAptData($login);
-        @$user_aptid=$user_aptdata['aptid'];
+        $alter_conf = rcms_parse_ini_file(CONFIG_PATH . "alter.ini");
+        $user_ip = zb_UserGetIP($login);
+        $user_aptdata = zb_AddressGetAptData($login);
+        @$user_aptid = $user_aptdata['aptid'];
+        //disable user before deletion - for proper OnDisconnect
+        $billing->setdown($login, 1);
+        $billing->setao($login, 0);
         zb_AddressDeleteApartment($user_aptid);
         zb_AddressOrphanUser($login);
         zb_UserDeleteEmail($login);
@@ -16,24 +28,31 @@ if (cfr('ANNIHILATION')) {
         zb_UserDeleteRealName($login);
         zb_UserDeleteSpeedOverride($login);
         if (!$alter_conf['STRICT_CONTRACTS_PROTECT']) {
-        zb_UserDeleteContract($login);
+            zb_UserDeleteContract($login);
         }
         zb_VserviceCashClear($login);
-        log_register("DELETE VCASH (".$login.")");
+        log_register("DELETE VCASH (" . $login . ")");
         cf_FlushAllUserCF($login);
         zb_FlushAllUserTags($login);
-	vlan_delete_host($login);
+        vlan_delete_host($login);
         multinet_delete_host($user_ip);
         multinet_rebuild_all_handlers();
         //destroy stargazer user
         $billing->deleteuser($login);
-        log_register("StgUser DELETE (".$login.")");
+        log_register("StgUser DELETE (" . $login . ")");
     }
-    
+
+    /**
+     * Renders user deletion form
+     * 
+     * @param string $login
+     * 
+     * @return void
+     */
     function web_AnnihilateFormShow($login) {
-        $alladdress=zb_AddressGetFulladdresslist();
-    
-        $inputs=__('Be careful, this module permanently deletes user and all data associated with it. Opportunities to raise from the dead no longer.');
+        $alladdress = zb_AddressGetFulladdresslist();
+
+        $inputs = __('Be careful, this module permanently deletes user and all data associated with it. Opportunities to raise from the dead no longer.');
         $inputs.= wf_tag('br');
         $inputs.=__('To ensure that we have seen the seriousness of your intentions to enter the word сonfirm the field below.');
         $inputs.= wf_tag('br');
@@ -41,31 +60,28 @@ if (cfr('ANNIHILATION')) {
         $inputs.= wf_HiddenInput('anihilation', 'true');
         $inputs.=wf_delimiter();
         $inputs.= wf_Submit(__('I really want to stop suffering User'));
-        $form=  wf_Form('', 'POST', $inputs, 'glamour');
-        
-        show_window(__('Deleting user').' '.@$alladdress[$login].' ('.$login.')', $form);
+        $form = wf_Form('', 'POST', $inputs, 'glamour');
+
+        show_window(__('Deleting user') . ' ' . @$alladdress[$login] . ' (' . $login . ')', $form);
     }
-    
+
     if (isset($_GET['username'])) {
-        
-    $login=$_GET['username'];    
-    web_AnnihilateFormShow($login);
-    //check for delete confirmation
-    if (isset ($_POST['anihilation'])) {
-        if (isset($_POST['confirmation'])) {
-            if ($_POST['confirmation']=='confirm') {
-                zb_AnnihilateUser($login);
-                rcms_redirect("?module=index");
+
+        $login = $_GET['username'];
+        web_AnnihilateFormShow($login);
+        //check for delete confirmation
+        if (isset($_POST['anihilation'])) {
+            if (isset($_POST['confirmation'])) {
+                if ($_POST['confirmation'] == 'confirm') {
+                    zb_AnnihilateUser($login);
+                    rcms_redirect("?module=index");
+                }
             }
         }
-    }
-    
-    show_window('',web_UserControls($login));
-    }
-    
-    
-} else {
-      show_error(__('You cant control this module'));
-}
 
+        show_window('', web_UserControls($login));
+    }
+} else {
+    show_error(__('You cant control this module'));
+}
 ?>
