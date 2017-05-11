5670f3c8e08ff6a0102e7b53b755c1d6a237d7e3
Nightfly <nightfly@email.ua>
2017-05-12 02:00:16 +0300

    branches module initial commit

---
+++ modules/general/branches/index.php
@@ -0,0 +1,72 @@
+<?php
+
+if (cfr('BRANCHES')) {
+    $altCfg = $ubillingConfig->getAlter();
+    if ($altCfg['BRANCHES_ENABLED']) {
+        $branch = new UbillingBranches();
+
+        show_window('', $branch->panel());
+
+        if (wf_CheckGet(array('userlist'))) {
+            if (wf_CheckGet(array('ajaxuserlist'))) {
+                $branch->renderUserListJson();
+            }
+
+            show_window(__('Users'), $branch->renderUserList());
+        }
+
+
+        if (wf_CheckGet(array('finreport'))) {
+            show_window(__('Finance report'), $branch->renderFinanceReport());
+        }
+
+        if (wf_CheckGet(array('sigreport'))) {
+            show_window(__('Signup report'), $branch->renderSignupReport());
+        }
+
+        if (wf_CheckGet(array('settings'))) {
+            //create new branch
+            if (wf_CheckPost(array('newbranch', 'newbranchname'))) {
+                $branch->createBranch($_POST['newbranchname']);
+                rcms_redirect($branch::URL_ME . '&settings=true');
+            }
+
+            //branches editing
+            if (wf_CheckPost(array('editbranch', 'editbranchid', 'editbranchname'))) {
+                $branch->editBranch($_POST['editbranchid'], $_POST['editbranchname']);
+                rcms_redirect($branch::URL_ME . '&settings=true');
+            }
+
+            //branches deletion
+            if (wf_CheckGet(array('deletebranch'))) {
+                if ($branch->isBranchProtected($_GET['deletebranch'])) {
+                    show_error(__('You know, we really would like to let you perform this action, but our conscience does not allow us to do'));
+                } else {
+                    $branch->deleteBranch($_GET['deletebranch']);
+                    rcms_redirect($branch::URL_ME . '&settings=true');
+                }
+            }
+
+            //branches administrators assign
+            if (wf_CheckPost(array('newadminbranch', 'newadminlogin'))) {
+                $branch->adminAssignBranch($_POST['newadminbranch'], $_POST['newadminlogin']);
+                rcms_redirect($branch::URL_ME . '&settings=true');
+            }
+
+            //admin branch deassign
+            if (wf_CheckGet(array('deleteadmin', 'adminbranchid'))) {
+                $branch->adminDeassignBranch($_GET['adminbranchid'], $_GET['deleteadmin']);
+                rcms_redirect($branch::URL_ME . '&settings=true');
+            }
+
+
+
+            show_window(__('Configuration'), $branch->renderSettingsBranches());
+        }
+    } else {
+        show_error(__('This module is disabled'));
+    }
+} else {
+    show_error(__('Access denied'));
+}
+?>
---
+++ modules/general/branches/module.php
@@ -0,0 +1,4 @@
+<?php
+
+$this->registerModule($module, 'main', __('Branches'), 'Nightfly', array('BRANCHES' => __('right to use branches module'), 'BRANCHESCONF' => __('right to control branches configuration')));
+?>
