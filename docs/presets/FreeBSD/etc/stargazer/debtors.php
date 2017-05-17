f9ae368b24f54f476c21e5bd7fcc3fd35464981f
Pautina <alex.chernyk@gmail.com>
2017-05-17 10:01:57 +0300

    Add support MySQLi and sql query optimization for debtors.php

--- docs/presets/FreeBSD/etc/stargazer/debtors.php
+++ docs/presets/FreeBSD/etc/stargazer/debtors.php
@@ -2,11 +2,41 @@
 <?php
  
 $config=parse_ini_file(dirname(__FILE__)."/config");
-$link = mysql_connect($config['host'], $config['username'], $config['password']);
-mysql_select_db($config['database']);
+if (extension_loaded('mysqli')) {
+    $loginDB = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
+    function DB_query($query) {
+        global $loginDB;
+        $result = $loginDB->query($query);
+        return($result);
+    }
+    function DB_real_escape_string($parametr) {
+        global $loginDB;
+        $result = $loginDB->real_escape_string($parametr);
+        return($result);
+    }
+    function DB_fetch_array($data) {
+        $result = mysqli_fetch_assoc($data);
+        return($result);
+    }
+} else {
+    $link = mysql_connect($config['host'], $config['username'], $config['password']);
+    mysql_select_db($config['database']);
+    function DB_query($query) {
+        $result = mysql_query($query);
+        return($result);
+    }
+    function DB_real_escape_string($parametr) {
+        $result = mysql_real_escape_string($parametr);
+        return($result);
+    }
+    function DB_fetch_array($data) {
+        $result = mysql_fetch_array($data, MYSQL_ASSOC);
+        return($result);
+    }
+}
 $data_q='SELECT `ip` FROM `users` WHERE `Cash`< -`Credit`';
-$data=mysql_query($data_q);
-           while ($row = mysql_fetch_array($data, MYSQL_ASSOC)) {
+$data=DB_query($data_q);
+           while ($row = DB_fetch_array($data)) {
                 shell_exec("/sbin/ipfw -q table 47 add ".$row['ip']);
            }
  
