e9a281b7ff103ee26c6a53799f4d08d68a2d2c44
Pautina <alex.chernyk@gmail.com>
2017-07-14 17:20:35 +0300

    Создание задач для пользователя - определена опция TESKMAN_TELEGRAM_PROFILE_CHECK

--- api/libs/api.teskman.php
+++ api/libs/api.teskman.php
@@ -963,8 +963,8 @@ function ts_TaskCreateFormProfile($address, $mobile, $phone, $login) {
     //construct sms sending inputs
     if ($altercfg['SENDDOG_ENABLED']) {
         $smsInputs = wf_CheckInput('newtasksendsms', __('Send SMS'), false, false);
-        // SET checkbed TELEGRAM for creating task from Userprofile if TELEGRAM_CHECKED == 1
-        $telegramInputsCheck = (isset($altercfg['TELEGRAM_CHECKED']) && $altercfg['TELEGRAM_CHECKED']) ? TRUE : FALSE;
+        // SET checkbed TELEGRAM for creating task from Userprofile if TESKMAN_TELEGRAM_PROFILE_CHECK == 1
+        $telegramInputsCheck = (isset($altercfg['TESKMAN_TELEGRAM_PROFILE_CHECK']) && $altercfg['TESKMAN_TELEGRAM_PROFILE_CHECK']) ? TRUE : FALSE;
         $telegramInputs = wf_CheckInput('newtasksendtelegram', __('Telegram'), false, $telegramInputsCheck);
     } else {
         $smsInputs = '';
