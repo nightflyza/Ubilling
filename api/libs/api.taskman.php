230b10b2dfdab1df4b3e2baeeb840995bdeaf535
Nightfly <nightfly@email.ua>
2017-07-20 11:49:13 +0300

    optional task creation time

--- api/libs/api.taskman.php
+++ api/libs/api.taskman.php
@@ -1001,12 +1001,21 @@ function ts_TaskCreateFormProfile($address, $mobile, $phone, $login) {
         $telegramInputs = '';
     }
 
+    //new task creation data/time generation
+    if ($altercfg['TASKMAN_NEWTASK_AUTOTIME']) {
+        $newTaskDate = date("Y-m-d");
+        $newTaskTime = date("H:i", strtotime("+1 hour"));
+    } else {
+        $newTaskDate = '';
+        $newTaskTime = '';
+    }
+
     $sup = wf_tag('sup', false) . '*' . wf_tag('sup', true);
 
     $inputs = '<!--ugly hack to prevent datepicker autoopen --> <input type="text" name="shittyhack" style="width: 0; height: 0; top: -100px; position: absolute;"/>';
     $inputs.=wf_HiddenInput('createtask', 'true');
-    $inputs.=wf_DatePickerPreset('newstartdate', date("Y-m-d"));
-    $inputs.=wf_TimePickerPreset('newstarttime', date( "H:i", strtotime( "+1 hour" ) ), '', false);
+    $inputs.=wf_DatePickerPreset('newstartdate', $newTaskDate);
+    $inputs.=wf_TimePickerPreset('newstarttime', $newTaskTime, '', false);
     $inputs.=wf_tag('label') . __('Target date') . $sup . wf_tag('label', true);
     $inputs.=wf_delimiter();
     $inputs.=wf_TextInput('newtaskaddress', __('Address') . $sup, $address, true, '30');
@@ -1714,7 +1723,7 @@ function ts_TaskChangeForm($taskid) {
 
             $administratorChange = (isset($employeeLogins[$taskdata['change_admin']])) ? $employeeLogins[$taskdata['change_admin']] : $taskdata['change_admin'];
 
-            $donecells = wf_TableCell( __('Administrator'));
+            $donecells = wf_TableCell(__('Administrator'));
             $donecells.=wf_TableCell($administratorChange);
             $donerows.=wf_TableRow($donecells, 'row3');
 
