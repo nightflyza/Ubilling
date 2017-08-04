67aa13b33d5b37f75890309c8a808f59f699e659
Pautina <alex.chernyk@gmail.com>
2017-08-05 02:32:27 +0300

    SQL dump - мелкие исправления индексов print_card

--- docs/test_dump.sql
+++ docs/test_dump.sql
@@ -1882,4 +1882,7 @@ CREATE TABLE IF NOT EXISTS `admacquainted` (
 
 -- 0.8.4
 ALTER TABLE `switches` ADD `snmpwrite` VARCHAR(45) NULL AFTER `swid`;
+
 ALTER TABLE `phones` ADD INDEX (`login`);
+
+ALTER TABLE `stg`.`print_card` ADD UNIQUE (`title`);
