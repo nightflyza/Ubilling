3055586d61ecce665e8e7916459616e2b740bacc
Nightfly <nightfly@email.ua>
2017-08-05 16:41:54 +0300

    jungen in progress

--- api/libs/api.jungen.php
+++ api/libs/api.jungen.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * returns list of available free radius clients/nases
+ * returns list of available free Juniper NASes
  * 
  * @return string
  */
@@ -24,4 +24,103 @@ function web_JuniperListClients() {
     }
 
     return ($result);
+}
+
+class JunGen {
+
+    protected $allUsers = array();
+    protected $allMacs = array();
+    protected $allSpeeds = array();
+    protected $checkTable = 'jun_check';
+    protected $replyTable = 'jun_reply';
+    protected $defaultMxPass = 'mxBras';
+    protected $defaultSessionTimeout = 3600;
+    protected $speedOffset = 1024;
+
+    public function __construct() {
+        $this->loadUsers();
+        $this->loadMacs();
+        $this->loadSpeeds();
+        $this->generateCheckAll();
+        $this->generateReplyAll();
+    }
+
+    protected function loadUsers() {
+        $this->allUsers = zb_UserGetAllStargazerDataAssoc();
+    }
+
+    protected function loadMacs() {
+        $this->allMacs = zb_UserGetAllIpMACs();
+    }
+
+    protected function loadSpeeds() {
+        $tariffSpeeds = zb_TariffGetAllSpeeds();
+        $speedOverrides = array();
+        $speedOverrides_q = "SELECT * from `userspeeds` WHERE `speed` NOT LIKE '0';";
+        $rawOverrides = simple_queryall($speedOverrides_q);
+        if (!empty($rawOverrides)) {
+            foreach ($rawOverrides as $io => $each) {
+                $speedOverrides[$each['login']] = ($each['speed'] / $this->speedOffset);
+            }
+        }
+
+        if (!empty($this->allUsers)) {
+            foreach ($this->allUsers as $login => $userData) {
+                if (isset($tariffSpeeds[$userData['Tariff']])) {
+                    if (!isset($speedOverrides[$login])) {
+                        $this->allSpeeds[$login]['down'] = round(($tariffSpeeds[$userData['Tariff']]['speeddown'] * $this->speedOffset));
+                        $this->allSpeeds[$login]['up'] = round(($tariffSpeeds[$userData['Tariff']]['speedup'] * $this->speedOffset));
+                    } else {
+                        $userSpeedOverride = round(($speedOverrides[$login] * $this->speedOffset));
+                        $this->allSpeeds[$login]['down'] = $userSpeedOverride;
+                        $this->allSpeeds[$login]['up'] = $userSpeedOverride;
+                    }
+                } else {
+                    $this->allSpeeds[$login]['down'] = 0;
+                    $this->allSpeeds[$login]['up'] = 0;
+                }
+            }
+        }
+    }
+
+    protected function generateCheckAll() {
+        nr_query("TRUNCATE TABLE `" . $this->checkTable . "`;");
+        if (!empty($this->allUsers)) {
+            foreach ($this->allUsers as $io => $each) {
+                if (isset($this->allMacs[$each['IP']])) {
+                    $userMac = $this->allMacs[$each['IP']];
+                    $query = "INSERT INTO `" . $this->checkTable . "` (`id`,`username`,`attribute`,`op`,`value`) VALUES " .
+                            "(NULL,'" . $userMac . "','Cleartext-Password',':=','" . $this->defaultMxPass . "');";
+                    nr_query($query);
+                }
+            }
+        }
+    }
+
+    protected function generateReplyAll() {
+        nr_query("TRUNCATE TABLE `" . $this->replyTable . "`;");
+        if (!empty($this->allUsers)) {
+            foreach ($this->allUsers as $io => $each) {
+                if (isset($this->allMacs[$each['IP']])) {
+                    $userMac = $this->allMacs[$each['IP']];
+                    $queryIP = "INSERT INTO `" . $this->replyTable . "` (`id`,`username`,`attribute`,`op`,`value`) VALUES " .
+                            "(NULL,'" . $userMac . "','Framed-IP-Address','=','" . $each['IP'] . "');";
+                    nr_query($queryIP);
+
+                    $queryTimeout = "INSERT INTO `" . $this->replyTable . "` (`id`,`username`,`attribute`,`op`,`value`) VALUES " .
+                            "(NULL,'" . $userMac . "','Session-Timeout','=','" . $this->defaultSessionTimeout . "');";
+                    nr_query($queryTimeout);
+
+                    $queryActivate1 = "INSERT INTO `" . $this->replyTable . "` (`id`,`username`,`attribute`,`op`,`value`) VALUES " .
+                            "(NULL,'" . $userMac . "','Unisphere-Service-Activate:1','+=','service-shape-in(" . $this->allSpeeds[$io]['up'] . ",shape," . $each['IP'] . ",,)');";
+                    nr_query($queryActivate1);
+
+                    $queryActivate2 = "INSERT INTO `" . $this->replyTable . "` (`id`,`username`,`attribute`,`op`,`value`) VALUES " .
+                            "(NULL,'" . $userMac . "','Unisphere-Service-Activate:2','+=','service-shape-out(" . $this->allSpeeds[$io]['down'] . ",shape," . $each['IP'] . ",,)');";
+                    nr_query($queryActivate2);
+                }
+            }
+        }
+    }
+
 }
--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -3,5 +3,7 @@
 //just dummy module for testing purposes
 error_reporting(E_ALL);
 
+$jun=new JunGen();
+
 
 ?>
