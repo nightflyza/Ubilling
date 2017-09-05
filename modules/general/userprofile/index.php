95e8f1ce779f98b74e2ab1a734e22d34617cffc1
Nightfly <nightfly@email.ua>
2017-09-05 13:55:16 +0300

    added missing right notice to user profile

--- modules/general/userprofile/index.php
+++ modules/general/userprofile/index.php
@@ -1,19 +1,19 @@
 <?php
-if(cfr('USERPROFILE')) {
- if (isset ($_GET['username'])) {
-        $login=vf($_GET['username']);
-        $login=trim($login);
-        try {
-        $profile=new UserProfile($login);
-        show_window(__('User profile'),$profile->render());
-        } catch (Exception $exception) {
-            show_window(__('Error'),__('Strange exeption').': '. wf_tag('pre').$exception->getMessage().wf_tag('pre',true));
-        }
-        
-     
-      } else {
-          throw new Exception ('GET_NO_USERNAME');
-      }
 
+if (cfr('USERPROFILE')) {
+    if (isset($_GET['username'])) {
+        $login = vf($_GET['username']);
+        $login = trim($login);
+        try {
+            $profile = new UserProfile($login);
+            show_window(__('User profile'), $profile->render());
+        } catch (Exception $exception) {
+            show_window(__('Error'), __('Strange exeption') . ': ' . wf_tag('pre') . $exception->getMessage() . wf_tag('pre', true));
+        }
+    } else {
+        throw new Exception('GET_NO_USERNAME');
+    }
+} else {
+    show_error(__('Access denied'));
 }
 ?>
