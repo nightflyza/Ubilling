85b2c33310a13acaa95649f24067a03a09659216
Pautina <alex.chernyk@gmail.com>
2017-09-14 23:38:36 +0300

    Добавлен вызов удаленного API для получения данных по сигналам с устройст Mikrotik и UBNT

--- modules/general/remoteapi/index.php
+++ modules/general/remoteapi/index.php
@@ -846,6 +846,16 @@ if ($alterconf['REMOTEAPI_ENABLED']) {
                         }
                     }
 
+                    // Load MIKROTIK and UBNT Signal data
+                    if ($_GET['action'] == 'mtsigmonpoll') {
+                        if ($alterconf['MTSIGMON_ENABLED']) {
+                            $sigmon = new MTsigmon();
+                            $sigmon->MTDevicesPolling();
+                            die('OK:MTPOLL');
+                        } else {
+                            die('ERROR:MTSIGMON_DISABLED');
+                        }
+                    }
 
                     ////
                     //// End of actions
