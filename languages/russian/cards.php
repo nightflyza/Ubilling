02fdc3d301f487e1a520c1d8e4275b36d25c263c
Pautina <alex.chernyk@gmail.com>
2017-10-10 01:08:53 +0300

    Модуль "Платежные карточки": добавлены файлы локализации

---
+++ languages/russian/cards.php
@@ -0,0 +1,6 @@
+<?php
+$lang['def']['Serial number of the card with duplicates'] = 'Серийный номер карточки имеющая дубликаты';
+$lang['def']['There are duplicate serial numbers of cards'] = 'Имеются повторяющиеся серийные номера карт';
+$lang['def']['Serial number length'] = 'Длина серийного номера';
+
+?>
