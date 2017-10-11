6495f0d996c68b5a40b5c12ac21048eb7bf6a527
Pautina <alex.chernyk@gmail.com>
2017-10-11 10:41:16 +0300

    Fix wrong path

--- api/api.autolader.php
+++ api/api.autolader.php
@@ -7,12 +7,15 @@ spl_autoload_register(function ($className) {
     static $libs_directory = 'libs' . DIRECTORY_SEPARATOR;
     static $venor_directory = 'vendor' . DIRECTORY_SEPARATOR;
 
+    // Defined path
     $classFileName = strtolower($className);
+    $apiClassFileName = $api_directory . $libs_directory . 'api.' . $classFileName . '.php';
+    $vendorClassFileName = $api_directory . $venor_directory . strtolower($className) . DIRECTORY_SEPARATOR . $classFileName . '.php';
 
-    if (file_exists($api_directory . $libs_directory)) {
-        include $api_directory . $libs_directory . 'api.' . $classFileName . '.php';
-    } elseif (file_exists($api_directory . $venor_directory)) {
-        include $api_directory . $venor_directory . $className . $classFileName . '.php';
+    if (file_exists($apiClassFileName)) {
+        include $apiClassFileName;
+    } elseif (file_exists($vendorClassFileName)) {
+        include $vendorClassFileName;
     }
 });
 
