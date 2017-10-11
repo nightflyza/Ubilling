8797a534e16c436cd06cc3e0340127e057602624
Pautina <alex.chernyk@gmail.com>
2017-10-11 12:09:48 +0300

    Try fix travis check for php < 5.6

--- api/api.autolader.php
+++ api/api.autolader.php
@@ -3,12 +3,16 @@
 //Register and load file classes 
 spl_autoload_register(function ($className) {
 
-    static $api_directory = 'api' . DIRECTORY_SEPARATOR;
-    static $libs_directory = 'libs' . DIRECTORY_SEPARATOR;
-    static $venor_directory = 'vendor' . DIRECTORY_SEPARATOR;
+    $api_directory = 'api' . DIRECTORY_SEPARATOR;
+    $libs_directory = 'libs' . DIRECTORY_SEPARATOR;
+    $venor_directory = 'vendor' . DIRECTORY_SEPARATOR;
 
     // Defined path
     $classFileName = strtolower($className);
+
+    // For old Class owervide overwind path
+    $classFileName = ($className == 'UbillingWhois') ? 'whois' : $classFileName;
+
     $apiClassFileName = $api_directory . $libs_directory . 'api.' . $classFileName . '.php';
     $vendorClassFileName = $api_directory . $venor_directory . strtolower($className) . DIRECTORY_SEPARATOR . $classFileName . '.php';
 
