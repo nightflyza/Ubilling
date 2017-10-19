c230342b5fca7a60118c4cd72d0098682b52f332
Pautina <alex.chernyk@gmail.com>
2017-10-20 00:36:20 +0300

    Модули Опросы и Результаты опросов: добавлены файлы локализации

---
+++ languages/ukrainian/polls_report.php
@@ -0,0 +1,16 @@
+<?php
+$lang['def']['Polls results'] = 'Результати опитувань';
+$lang['def']['Right to use polls results'] = 'Право використовувати результати опитувань';
+$lang['def']['Show polls'] = 'Показати опитування';
+$lang['def']['Options'] = 'Варіанти';
+$lang['def']['Number of votes'] = 'Кількість голосів';
+$lang['def']['Number of votes on a 3D chart'] = 'Кількість голосів на 3D-діаграмі';
+$lang['def']['Poll results'] = 'Результати опитування';
+$lang['def']['Voting results for option'] = 'Результати голосування для варіанта';
+$lang['def']['The answer option passed to the module does not exist'] = 'Варіант відповіді, переданий модулю, не існує';
+$lang['def']['The module was not given the answer option'] = 'Модулю не був переданий варіант відповіді';
+$lang['def']['For this option, no one voted yet'] = 'За цей варіант ще ніхто не голосував';
+$lang['def']['No one voted on this poll yet'] = 'Ніхто ще не голосував за цим опитуванням';
+$lang['def']['Number of voted users'] = 'Кількість проголосувавших користувачів';
+$lang['def']['Voted Users'] = 'Користувачі котрі проголосували';
+$lang['def']['votes'] = 'голосів';
