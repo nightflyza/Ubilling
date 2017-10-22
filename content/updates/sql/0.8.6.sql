6749c001b441717740fe9f453b3ade2136a940fd
Pautina <alex.chernyk@gmail.com>
2017-10-22 19:03:29 +0300

    Модули Опросы и Результаты опроса: добавлен mysqldump

--- content/updates/sql/0.8.6.sql
+++ content/updates/sql/0.8.6.sql
@@ -3,3 +3,33 @@ ALTER TABLE `salary_jobs` ADD INDEX(`taskid`);
 ALTER TABLE `wh_out` ADD INDEX(`desttype`);
 
 ALTER TABLE `wh_out` ADD INDEX(`destparam`);
+
+CREATE TABLE IF NOT EXISTS `polls` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `start_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_date` datetime DEFAULT '0000-00-00 00:00:00',
+  `params` text NOT NULL,
+  `admin` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `polls_options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `poll_id` int(11) NOT NULL DEFAULT '0',
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `poll_id` (`id`,`poll_id`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `polls_votes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `option_id` int(11) NOT NULL DEFAULT '0',
+  `poll_id` int(11) NOT NULL DEFAULT '0',
+  `login` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login_poll` (`poll_id`,`login`) USING BTREE,
+  UNIQUE KEY `login_poll_option` (`option_id`,`poll_id`,`login`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
