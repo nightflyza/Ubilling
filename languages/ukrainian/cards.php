702ac30f2e7f37f77147185adc46e6d3ecf7df64
Pautina <alex.chernyk@gmail.com>
2017-10-26 15:57:00 +0300

    Добавлена пропущеная локаль для модуля картточек

--- languages/ukrainian/cards.php
+++ languages/ukrainian/cards.php
@@ -2,5 +2,7 @@
 $lang['def']['Serial number of the card with duplicates'] = 'Серійний номер картки має дублікати';
 $lang['def']['There are duplicate serial numbers of cards'] = 'Наявні повторювані серійні номери карт';
 $lang['def']['Serial number length'] = 'Довжина серійного номера';
+$lang['def']['Count of cards cannot be empty'] = 'Кількість карт не може бути порожнім';
+$lang['def']['Price of cards cannot be empty'] = 'Ціна карток не може бути пустою';
 
 ?>
