782eb3aa1c37fa060e1e1e57486b06223520b1ba
Nightfly <nightfly@email.ua>
2017-10-27 03:18:24 +0300

    sorm yahont support begins

---
+++ api/libs/api.sormyahont.php
@@ -0,0 +1,8 @@
+<?php
+
+class SormYahont {
+    protected $altCfg=array();
+    
+}
+
+?>
--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -3,6 +3,6 @@
 //just dummy module for testing purposes
 error_reporting(E_ALL);
 
- 
+$sorm= new SormYahont();
 
 ?>
