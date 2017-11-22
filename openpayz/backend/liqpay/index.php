e670effb6a6cdbe50a09697f178c594ef77c488f
Pautina <alex.chernyk@gmail.com>
2017-11-22 12:52:12 +0200

    Change Openpayz backend LiqPay for new version API, now ver. 3

--- openpayz/backend/liqpay/index.php
+++ openpayz/backend/liqpay/index.php
@@ -16,7 +16,7 @@ function lq_SessionGen($size=16) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
     $string = "LIQPAY_";
     for ($p = 0; $p < $size; $p++) {
-        $string .= $characters[mt_rand(0, (strlen($characters)-1))];
+        $string.= $characters[mt_rand(0, (strlen($characters)-1))];
     }
 
     return ($string);
@@ -37,13 +37,14 @@ function lq_PricesForm() {
            $i=0;
             foreach ($pricesRaw as $eachPrice) {
              $selected = ($i==0) ?'CHECKED' : '' ;
-             $result .='<input type="radio" name="amount" value="'.$eachPrice.'" '.$selected.'> '.$eachPrice.' '.$liqConf['TEMPLATE_CURRENCY'].'<br>';
+             $result.= '<input type="radio" name="amount" value="' . $eachPrice . '" ' . $selected . '> ' . $eachPrice . ' ' . $liqConf['TEMPLATE_CURRENCY'] . '<br>';
              $i++;
             }
         }
     }
-    $result .= '<input type="submit" value="'.$liqConf['TEMPLATE_NEXT'].'">';
-    $result .= '</form>';
+    $result.= '<input type="submit" value="' . $liqConf['TEMPLATE_NEXT'] . '">';
+    $result.= '</form>';
+
     return ($result);
 }
 
@@ -56,49 +57,34 @@ function lq_PricesForm() {
  */
 function lq_PaymentForm($customer_id) {
     global $liqConf;
-    
-$merchant_id = $liqConf['MERCHANT_ID'];
-$signature = $liqConf['SIGNATURE'];
-$url = $liqConf['LIQURL'];
-$method = $liqConf['METHOD'];
-$currency = $liqConf['CURRENCY'];
-$summ = $_POST['amount'];
-$resultUrl = $liqConf['RESULT_URL'];
-$serverUrl = $liqConf['SERVER_URL'];
-$phone = '';
-$session = lq_SessionGen();
+    include('LiqPay.php');
 
-    $xml='<request>      
-            <version>1.2</version>
-            <result_url>'.$resultUrl.'</result_url>
-            <server_url>'.$serverUrl.'</server_url>
-            <merchant_id>'.$merchant_id.'</merchant_id>
-            <order_id>'.$session.'</order_id>
-            <amount>'.$summ.'</amount>
-            <currency>'.$currency.'</currency>
-            <description>'.$customer_id.'</description>
-            <default_phone>'.$phone.'</default_phone>
-            <pay_way>'.$method.'</pay_way> 
-            </request>
-            ';
-	
-	
-	$xml_encoded = base64_encode($xml); 
-	$lqsignature = base64_encode(sha1($signature.$xml.$signature,1));
-	
+    $merchant_id = $liqConf['MERCHANT_ID'];
+    $signature = $liqConf['SIGNATURE'];
+    $method = $liqConf['METHOD'];
+    $currency = $liqConf['CURRENCY'];
+    $summ = $_POST['amount'];
+    $resultUrl = $liqConf['RESULT_URL'];
+    $serverUrl = $liqConf['SERVER_URL'];
+    $session = lq_SessionGen();
 
-
-$result="<h2>".$liqConf['TEMPLATE_ISP_SERVICE']." ".$customer_id."</h2>
-      <form action='".$url."' method='POST'>
-        <input type='hidden' name='operation_xml' value='$xml_encoded' />
-        <input type='hidden' name='signature' value='$lqsignature' />
-	<input type='submit' value='".$liqConf['TEMPLATE_GOPAYMENT']."'/>
-      </form>";
+    $result = "<h2>" . $liqConf['TEMPLATE_ISP_SERVICE'] . " " . $customer_id . "</h2>";
+    $liqpay = new LiqPay($merchant_id, $signature);
+        $result.= $liqpay->cnb_form(array(
+                'action'         => 'pay',
+                'amount'         => $summ,
+                'currency'       => $currency,
+                'description'    => $customer_id,
+                'order_id'       => $session,
+                'result_url'       => $resultUrl,
+                'server_url'       => $serverUrl,
+                'paytypes'       => $method,
+                'version'        => '3'
+                ));
 
 return ($result);
     
 }
-
 
 /*
  * main codepart
@@ -118,4 +104,3 @@ if (isset($_GET['customer_id'])) {
 }
 
 ?>
-	
