494d5f37826bb055c821f9fc18d00852cf994762
Nightfly <nightfly@email.ua>
2018-04-26 14:45:38 +0300

    cleanup some debug code from freezedayscharge

--- modules/general/remoteapi/index.php
+++ modules/general/remoteapi/index.php
@@ -440,22 +440,20 @@ if ($alterconf['REMOTEAPI_ENABLED']) {
                      * running freeze days charge if FREEZE_DAYS_CHARGE_ENABLED
                      */
                     if ($_GET['action'] == 'freezedayscharge') {
-                        if ( isset($alterconf['FREEZE_DAYS_CHARGE_ENABLED']) && $alterconf['FREEZE_DAYS_CHARGE_ENABLED'] ) {
-                            $FreezeDaysInitAmnt                = $alterconf['FREEZE_DAYS_INITIAL_AMOUNT'];
-                            $WrkDaysToRestoreFrzDaysInitAmnt   = $alterconf['FREEZE_DAYS_WORK_TO_RESTORE'];
+                        if (isset($alterconf['FREEZE_DAYS_CHARGE_ENABLED']) && $alterconf['FREEZE_DAYS_CHARGE_ENABLED']) {
+                            $FreezeDaysInitAmnt = $alterconf['FREEZE_DAYS_INITIAL_AMOUNT'];
+                            $WrkDaysToRestoreFrzDaysInitAmnt = $alterconf['FREEZE_DAYS_WORK_TO_RESTORE'];
 
                             $TmpQuery = "SELECT `users`.`login` FROM `users` 
                                             LEFT JOIN `frozen_charge_days` ON `frozen_charge_days`.`login` = `users`.`login`
                                             WHERE `users`.`Passive`='1' AND `frozen_charge_days`.`login` IS NULL;";
                             $AllFrozenNotInCountTab = simple_queryall($TmpQuery);
 
-                            if ( !empty($AllFrozenNotInCountTab) ) {
+                            if (!empty($AllFrozenNotInCountTab)) {
                                 foreach ($AllFrozenNotInCountTab as $usr => $eachlogin) {
-                                    if ( $eachlogin['login'] == '63ap0_6g8g' ) {
-                                        $TmpQuery = "INSERT INTO `frozen_charge_days` (`login`, `freeze_days_amount`, `work_days_restore`)
-                                                                          VALUES  ('" . $eachlogin['login'] . "', '" . $FreezeDaysInitAmnt . "', '" . $WrkDaysToRestoreFrzDaysInitAmnt . "');";
-                                        nr_query($TmpQuery);
-                                    }
+                                    $TmpQuery = "INSERT INTO `frozen_charge_days` (`login`, `freeze_days_amount`, `work_days_restore`)
+                                                VALUES  ('" . $eachlogin['login'] . "', '" . $FreezeDaysInitAmnt . "', '" . $WrkDaysToRestoreFrzDaysInitAmnt . "');";
+                                    nr_query($TmpQuery);
                                 }
                             }
 
@@ -464,20 +462,20 @@ if ($alterconf['REMOTEAPI_ENABLED']) {
                                                   LEFT JOIN `users` ON `frozen_charge_days`.`login` = `users`.`login`;";
                             $FrozenAll = simple_queryall($FrozenAllQuery);
 
-                            if ( !empty($FrozenAll) ) {
+                            if (!empty($FrozenAll)) {
                                 $UsrPassive = 0;
 
                                 foreach ($FrozenAll as $usr => $eachlogin) {
-                                    $UsrLogin   = $eachlogin['login'];
+                                    $UsrLogin = $eachlogin['login'];
                                     $UsrPassive = $eachlogin['Passive'];
-                                    $UsrDown    = $eachlogin['Down'];
-                                    $UsrCredit  = $eachlogin['Credit'];
-                                    $UsrCash    = $eachlogin['Cash'];
+                                    $UsrDown = $eachlogin['Down'];
+                                    $UsrCredit = $eachlogin['Credit'];
+                                    $UsrCash = $eachlogin['Cash'];
 
-                                    $FrzDaysAmount              = $eachlogin['freeze_days_amount'];
-                                    $FrzDaysUsed                = $eachlogin['freeze_days_used'];
-                                    $DaysWorked                 = $eachlogin['days_worked'];
-                                    $WrkDaysToRestoreFrzDays    = $eachlogin['work_days_restore'];
+                                    $FrzDaysAmount = $eachlogin['freeze_days_amount'];
+                                    $FrzDaysUsed = $eachlogin['freeze_days_used'];
+                                    $DaysWorked = $eachlogin['days_worked'];
+                                    $WrkDaysToRestoreFrzDays = $eachlogin['work_days_restore'];
 
                                     if ($UsrPassive) {
                                         $FrzDaysUsed++;
@@ -488,11 +486,11 @@ if ($alterconf['REMOTEAPI_ENABLED']) {
 
                                         simple_update_field('frozen_charge_days', 'freeze_days_used', $FrzDaysUsed, "WHERE `login`='" . $UsrLogin . "' ");
                                     } else {
-                                        if ( ($FrzDaysUsed >= $FrzDaysAmount) && ($UsrCash < '-' . $UsrCredit) && !$UsrDown ) {
+                                        if (($FrzDaysUsed >= $FrzDaysAmount) && ($UsrCash < '-' . $UsrCredit) && !$UsrDown) {
                                             $DaysWorked++;
 
                                             if ($DaysWorked >= $WrkDaysToRestoreFrzDays) {
-                                                $DaysWorked  = 0;
+                                                $DaysWorked = 0;
                                                 $FrzDaysUsed = 0;
 
                                                 simple_update_field('frozen_charge_days', 'freeze_days_used', $FrzDaysUsed, "WHERE `login`='" . $UsrLogin . "' ");
