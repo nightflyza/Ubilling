8953dd60de9d3f012a7251679e09e08a0d9cedb3
l1ght13aby <l1ght13aby@yahoo.com>
2018-05-01 02:16:35 +0300

    Mikbill migration module fixes
    It's working again \o/

--- modules/general/mikbill_migration/index.php
+++ modules/general/mikbill_migration/index.php
@@ -9,25 +9,12 @@ if (cfr('MIKMIGR')) {
             eval($each);
         }
         if (file_exists($beggar['DUMP'])) {
-            if (isset($_POST['netnum'])) {
-                rcms_redirect("?module=mikbill_migration&netnum=" . $_POST['netnum']);
-            }
-            if (isset($_GET['netnum'])) {
-                show_window('networks', $mik->web_MikbillMigrationNetworksForm($_GET['netnum']));
-                $converts = array('db_user', 'db_pass', 'db_host', 'db_name', 'tariff_period', 'network');
-                if (isset($_POST['network'])) {
-                    if (wf_CheckPost($converts)) {
-                        $mik->ConvertMikBill($_POST['db_user'], $_POST['db_pass'], $_POST['db_host'], $_POST['db_name'], $_POST['network'], $_POST['tariff_period']);
-                        rcms_redirect("?module=mikbill_migration&success=1");
-                    } else {
-                        rcms_redirect("?module=mikbill_migration&notall=1");
-                    }
-                }
-            } else {
-                show_window('Networks number', $mik->web_MikbillMigrationNetnumForm());
-            }
-            if (isset($_GET['notall'])) {
-                show_error("No all of required fields is filled");
+
+            show_window('networks', $mik->web_MikbillMigrationNetworksForm());
+            $converts = array('db_user', 'db_pass', 'db_host', 'db_name', 'tariff_period');
+            if (wf_CheckPost($converts)) {
+                $mik->ConvertMikBill($_POST['db_user'], $_POST['db_pass'], $_POST['db_host'], $_POST['db_name'], $_POST['tariff_period']);
+                rcms_redirect("?module=mikbill_migration&success=1");
             }
             if (isset($_GET['success'])) {
                 show_success("sql dump generated find it in ./content/backups/sql/ub_dump.sql");
