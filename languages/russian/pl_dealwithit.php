3d8079e483a67ac31e49c47eda7b0aa812f98d2b
Pautina <alex.chernyk@gmail.com>
2018-05-07 22:19:55 +0300

    Add translate for dealwithit

---
+++ languages/russian/pl_dealwithit.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['def']['You did not select any user'] = 'Вы не выбрали ни одного пользователя';
+$lang['def']['Bulk creation of tasks'] = 'Массовое создание задач';
---
+++ languages/ukrainian/pl_dealwithit.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['def']['You did not select any user'] = 'Ви не обрали жодного користувача';
+$lang['def']['Bulk creation of tasks'] = 'Масове виробництво завдань';
