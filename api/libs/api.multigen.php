1b7af95cafacf03e9eb59f31d7f7e2f004d1836c
Nightfly <nightfly@email.ua>
2018-05-19 11:23:10 +0300

    multigen begins

--- api/libs/api.multigen.php
+++ api/libs/api.multigen.php
@@ -1,7 +1,30 @@
 <?php
 
 class MultiGen {
-    
+
+    /**
+     * Contains system alter.ini config as key=>value
+     *
+     * @var array
+     */
+    protected $altCfg = array();
+
+    public function __construct() {
+        $this->loadConfigs();
+    }
+
+    /**
+     * Loads reqired configss
+     * 
+     * @global object $ubillingConfig
+     * 
+     * @return void
+     */
+    protected function loadConfigs() {
+        global $ubillingConfig;
+        $this->altCfg = $ubillingConfig->getAlter();
+    }
+
 }
 
 ?>
