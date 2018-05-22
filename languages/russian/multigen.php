f038289c0a6476fe3baefb4d321f41fce371aed8
Nightfly <nightfly@email.ua>
2018-05-22 11:45:39 +0300

    democracy in action

--- languages/russian/multigen.php
+++ languages/russian/multigen.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['def']['Multigen NAS parameters'] = 'Параметры серверов доступа МногоГен';
-$lang['def']['right to control multigen'] = 'Право управлять МногоГеном';
-$lang['def']['Multigen support is enabled'] = 'Поддержка МногоГена включена';
-$lang['def']['Configure Multigen NAS'] = 'Сконфигурировать NAS МногоГен';
+$lang['def']['Multigen NAS parameters'] = 'Параметры серверов доступа КучаГен';
+$lang['def']['right to control multigen'] = 'Право управлять КучаГеном';
+$lang['def']['Multigen support is enabled'] = 'Поддержка КучаГена включена';
+$lang['def']['Configure Multigen NAS'] = 'Сконфигурировать NAS КучаГен';
 $lang['def'][''] = '';
 $lang['def'][''] = '';
 $lang['def'][''] = '';
--- languages/ukrainian/multigen.php
+++ languages/ukrainian/multigen.php
@@ -1,4 +1,6 @@
 <?php
 
-$lang['def']['Multigen NAS parameters'] = 'Параметри серверів доступу БагатоГен';
-
+$lang['def']['Multigen NAS parameters'] = 'Параметри сервервів КупаГен';
+$lang['def']['right to control multigen'] = 'Право керувати КупаГеном';
+$lang['def']['Multigen support is enabled'] = 'Підтримка КупаГен увімкнена';
+$lang['def']['Configure Multigen NAS'] = 'Сконфігурувати NAS КупаГен';
