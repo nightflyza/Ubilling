5d7353e8bc251574636c5d11a01a98f6c36d78b3
Nightfly <nightfly@email.ua>
2018-05-31 14:17:08 +0300

    fixed formatting

--- api/libs/api.senddog.php
+++ api/libs/api.senddog.php
@@ -44,7 +44,7 @@ class SendDog {
         $this->loadTurbosmsConfig();
         $this->loadSmsflyConfig();
         $this->loadRedsmsConfig();
-	    $this->loadSmsPilotConfig();
+        $this->loadSmsPilotConfig();
     }
 
     /**
@@ -241,21 +241,22 @@ class SendDog {
         $this->settings['REDSMS_APIKEY'] = $smsapikey;
         $this->settings['REDSMS_SIGN'] = $smssign;
     }
-	/**
-	 * Loads SMSPILOT.RU service config
-	 *
-	 * @return void
-	 */
-	protected function loadSmsPilotConfig() {
-		$smsapikey = zb_StorageGet('SENDDOG_SMSPILOT_APIKEY');
-		if (empty($smsapikey)) {
-			$smsapikey = 'XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ';
-			zb_StorageSet('SENDDOG_SMSPILOT_APIKEY', $smsapikey);
-		}
-		$smssign = zb_StorageGet('SENDDOG_SMSPILOT_SIGN');
-		$this->settings['SMSPILOT_APIKEY'] = $smsapikey;
-		$this->settings['SMSPILOT_SIGN'] = $smssign;
-	}
+
+    /**
+     * Loads SMSPILOT.RU service config
+     *
+     * @return void
+     */
+    protected function loadSmsPilotConfig() {
+        $smsapikey = zb_StorageGet('SENDDOG_SMSPILOT_APIKEY');
+        if (empty($smsapikey)) {
+            $smsapikey = 'XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ';
+            zb_StorageSet('SENDDOG_SMSPILOT_APIKEY', $smsapikey);
+        }
+        $smssign = zb_StorageGet('SENDDOG_SMSPILOT_SIGN');
+        $this->settings['SMSPILOT_APIKEY'] = $smsapikey;
+        $this->settings['SMSPILOT_SIGN'] = $smssign;
+    }
 
     /**
      * Render TurboSMS server-side queue
@@ -402,22 +403,22 @@ class SendDog {
         return ($result);
     }
 
-	/**
-	 * Renders SMSPILOT user balance
-	 *
-	 * @return string
-	 */
-	public function renderSMSPILOTBalance() {
+    /**
+     * Renders SMSPILOT user balance
+     *
+     * @return string
+     */
+    public function renderSMSPILOTBalance() {
 
-		$balance = file_get_contents( 'http://smspilot.ru/api.php'
-			.'?balance=rur'
-			.'&apikey='.$this->settings['SMSPILOT_APIKEY']
-		);
+        $balance = file_get_contents('http://smspilot.ru/api.php'
+                . '?balance=rur'
+                . '&apikey=' . $this->settings['SMSPILOT_APIKEY']
+        );
 
-		$result = wf_BackLink(self::URL_ME, '', true);
-		$result .= $this->messages->getStyledMessage(__('Current account balance') . ': ' . $balance . ' RUR', 'info');
-		return $result;
-	}
+        $result = wf_BackLink(self::URL_ME, '', true);
+        $result .= $this->messages->getStyledMessage(__('Current account balance') . ': ' . $balance . ' RUR', 'info');
+        return $result;
+    }
 
     /**
      * Renders current telegram bot contacts
@@ -499,20 +500,20 @@ class SendDog {
         $inputs.= wf_RadioInput('defaultsmsservice', __('Use RED-Sms as default SMS service'), 'redsms', true, $smsServiceFlag);
         return ($inputs);
     }
-	/**
-	 * Returns set of inputs, required for SMSPILOT configuration
-	 *
-	 * @return string
-	 */
-	protected function renderSmsPilotConfigInputs() {
-		$inputs = wf_tag('h2') . __('SMSPILOT') . ' ' . wf_Link(self::URL_ME . '&showmisc=smspilotbalance', wf_img_sized('skins/icon_dollar.gif', __('Balance'), '10', '10'), true) . wf_tag('h2', true);
-		$inputs.= wf_TextInput('editsmspilotapikey', __('User API key for access SMSPILOT API'), $this->settings['SMSPILOT_APIKEY'], true, 20);
-		$inputs.= wf_TextInput('editsmspilotsign', __('SMSPILOT') . ' ' . __('Sign') . ' (' . __('Alphaname') . ')', $this->settings['SMSPILOT_SIGN'], true, 20);
-		$smsServiceFlag = $this->settings['SMS_SERVICE'] === 'smspilot';
-		$inputs.= wf_RadioInput('defaultsmsservice', __('Use SMSPILOT as default SMS service'), 'smspilot', true, $smsServiceFlag);
-		return $inputs;
-	}
 
+    /**
+     * Returns set of inputs, required for SMSPILOT configuration
+     *
+     * @return string
+     */
+    protected function renderSmsPilotConfigInputs() {
+        $inputs = wf_tag('h2') . __('SMSPILOT') . ' ' . wf_Link(self::URL_ME . '&showmisc=smspilotbalance', wf_img_sized('skins/icon_dollar.gif', __('Balance'), '10', '10'), true) . wf_tag('h2', true);
+        $inputs.= wf_TextInput('editsmspilotapikey', __('User API key for access SMSPILOT API'), $this->settings['SMSPILOT_APIKEY'], true, 20);
+        $inputs.= wf_TextInput('editsmspilotsign', __('SMSPILOT') . ' ' . __('Sign') . ' (' . __('Alphaname') . ')', $this->settings['SMSPILOT_SIGN'], true, 20);
+        $smsServiceFlag = $this->settings['SMS_SERVICE'] === 'smspilot';
+        $inputs.= wf_RadioInput('defaultsmsservice', __('Use SMSPILOT as default SMS service'), 'smspilot', true, $smsServiceFlag);
+        return $inputs;
+    }
 
     /**
      * Returns set of inputs, required for SMS-Fly service configuration
@@ -536,7 +537,7 @@ class SendDog {
         $inputs = $this->renderTsmsConfigInputs();
         $inputs.= $this->renderSmsflyConfigInputs();
         $inputs.= $this->renderRedsmsConfigInputs();
-	    $inputs.= $this->renderSmsPilotConfigInputs();
+        $inputs.= $this->renderSmsPilotConfigInputs();
         $inputs.= $this->renderTelegramConfigInputs();
 
         $inputs.= wf_Submit(__('Save'));
@@ -609,15 +610,15 @@ class SendDog {
             log_register('SENDDOG CONFIG SET REDSMSSIGN `' . $_POST['editredsmssign'] . '`');
         }
 
-	    //SMSPILOT configuration
-	    if ($_POST['editsmspilotapikey'] != $this->settings['SMSPILOT_APIKEY']) {
-		    zb_StorageSet('SENDDOG_SMSPILOT_APIKEY', $_POST['editsmspilotapikey']);
-		    log_register('SENDDOG CONFIG SET SMSPILOT_APIKEY `' . $_POST['editsmspilotapikey'] . '`');
-	    }
-	    if ($_POST['editsmspilotsign'] != $this->settings['SMSPILOT_SIGN']) {
-		    zb_StorageSet('SENDDOG_SMSPILOT_SIGN', $_POST['editsmspilotsign']);
-		    log_register('SENDDOG CONFIG SET SMSPILOT_SIGN `' . $_POST['editsmspilotsign'] . '`');
-	    }
+        //SMSPILOT configuration
+        if ($_POST['editsmspilotapikey'] != $this->settings['SMSPILOT_APIKEY']) {
+            zb_StorageSet('SENDDOG_SMSPILOT_APIKEY', $_POST['editsmspilotapikey']);
+            log_register('SENDDOG CONFIG SET SMSPILOT_APIKEY `' . $_POST['editsmspilotapikey'] . '`');
+        }
+        if ($_POST['editsmspilotsign'] != $this->settings['SMSPILOT_SIGN']) {
+            zb_StorageSet('SENDDOG_SMSPILOT_SIGN', $_POST['editsmspilotsign']);
+            log_register('SENDDOG CONFIG SET SMSPILOT_SIGN `' . $_POST['editsmspilotsign'] . '`');
+        }
 
 
         //telegram bot token configuration
@@ -762,43 +763,45 @@ class SendDog {
             }
         }
     }
-	/**
-	 * Sends all sms storage via SMSPILOT.RU service
-	 *
-	 * @return void
-	 */
-	protected function smspilotPushMessages() {
 
-		$apikey = $this->settings['SMSPILOT_APIKEY'];
-		$sender = $this->settings['SMSPILOT_SIGN'];
+    /**
+     * Sends all sms storage via SMSPILOT.RU service
+     *
+     * @return void
+     */
+    protected function smspilotPushMessages() {
 
-		$allSmsQueue = $this->smsQueue->getQueueData();
-		if (!empty($allSmsQueue)) {
-			foreach ($allSmsQueue as $sms) {
+        $apikey = $this->settings['SMSPILOT_APIKEY'];
+        $sender = $this->settings['SMSPILOT_SIGN'];
 
-				$url = 'http://smspilot.ru/api.php'
-					.'?send='.urlencode( $sms['message'] )
-					.'&to='.urlencode( $sms['number'] )
-					.'&from='.urlencode( $sender )
-					.'&apikey='.urlencode( $apikey )
-					.'&format=json';
+        $allSmsQueue = $this->smsQueue->getQueueData();
+        if (!empty($allSmsQueue)) {
+            foreach ($allSmsQueue as $sms) {
 
-				$ch = curl_init( $url );
-				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-				curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				$json = curl_exec($ch);
-				curl_close($ch);
+                $url = 'http://smspilot.ru/api.php'
+                        . '?send=' . urlencode($sms['message'])
+                        . '&to=' . urlencode($sms['number'])
+                        . '&from=' . urlencode($sender)
+                        . '&apikey=' . urlencode($apikey)
+                        . '&format=json';
 
-				$j = json_decode( $json );
-				if ( $j && isset($j->error)) {
-					trigger_error( $j->description_ru, E_USER_WARNING );
-				}
-				//remove old sent message
-				$this->smsQueue->deleteSms( $sms['filename'] );
-			}
-		}
-	}
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $json = curl_exec($ch);
+                curl_close($ch);
+
+                $j = json_decode($json);
+                if ($j && isset($j->error)) {
+                    trigger_error($j->description_ru, E_USER_WARNING);
+                }
+                //remove old sent message
+                $this->smsQueue->deleteSms($sms['filename']);
+            }
+        }
+    }
+
     /**
      * Loads and sends all email messages from system queue
      * 
@@ -837,10 +840,9 @@ class SendDog {
                 case 'redsms':
                     $this->redsmsPushMessages();
                     break;
-	            case 'smspilot':
-		            $this->smspilotPushMessages();
-		            break;
-
+                case 'smspilot':
+                    $this->smspilotPushMessages();
+                    break;
             }
         }
         return ($smsCount);
