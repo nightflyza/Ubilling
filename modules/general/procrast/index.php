80c1cadaf153d9fa57151adf91dcd1e06969a691
Nightfly <nightfly@email.ua>
2018-06-22 15:16:11 +0300

    new procrastination interface

--- modules/general/procrast/index.php
+++ modules/general/procrast/index.php
@@ -1,6 +1,31 @@
 <?php
 
 if (cfr('PROCRAST')) {
+
+    /**
+     * Returns game icon and link as standard panel
+     * 
+     * @return string
+     */
+    function zb_buildGameIcon($link, $icon, $text) {
+        $icon_path = 'modules/jsc/procrastdata/icons/';
+
+        $task_link = $link;
+        $task_icon = $icon_path . $icon;
+        $task_text = $text;
+
+        $tbiconsize = '128';
+        $template = wf_tag('div', false, 'dashtask', 'style="height:' . ($tbiconsize + 30) . 'px; width:' . ($tbiconsize + 30) . 'px;"');
+        $template.= wf_tag('a', false, '', 'href="' . $task_link . '"');
+        $template.= wf_tag('img', false, '', 'src="' . $task_icon . '" border="0" width="' . $tbiconsize . '"  height="' . $tbiconsize . '" alt="' . $task_text . '" title="' . $task_text . '"');
+        $template.= wf_tag('a', true);
+        $template.= wf_tag('br');
+        $template.= wf_tag('br');
+        $template.= $task_text;
+        $template.= wf_tag('div', true);
+        return ($template);
+    }
+
     if (wf_CheckGet(array('run'))) {
         $application = vf($_GET['run']);
         switch ($application) {
@@ -11,17 +36,17 @@ if (cfr('PROCRAST')) {
                 $jsTetris = str_replace('ROWS_LABEL', __('rows'), $jsTetris);
                 show_window(__('Tetris'), $jsTetris);
                 break;
-               case '2048':
+            case '2048':
                 $jsCode = file_get_contents('modules/jsc/procrastdata/2048/2048.html');
                 show_window(__('2048'), $jsCode);
                 break;
-             case 'motox3m':
+            case 'motox3m':
                 $jsCode = file_get_contents('modules/jsc/procrastdata/motox3m.html');
-                show_window(__('Moto X3M'), $jsCode,'center');
+                show_window(__('Moto X3M'), $jsCode, 'center');
                 break;
             case 'happywheels':
                 $jsCode = file_get_contents('modules/jsc/procrastdata/happywheels.html');
-                show_window(__('Happy Wheels'), $jsCode,'center');
+                show_window(__('Happy Wheels'), $jsCode, 'center');
                 break;
         }
         show_window('', wf_BackLink('?module=procrast'));
@@ -34,11 +59,14 @@ if (cfr('PROCRAST')) {
             'happywheels' => __('Happy Wheels')
         );
 
+
         if (!empty($applicationArr)) {
             foreach ($applicationArr as $io => $each) {
-                $applicationsList.=wf_Link('?module=procrast&run=' . $io, wf_img_sized('skins/gamepad.png', '', '16', '16') . ' ' . $each, false, 'ubButton') . ' ';
+                $applicationsList.=zb_buildGameIcon('?module=procrast&run=' . $io, $io . '.png', $each);
             }
         }
+
+        $applicationsList.=wf_CleanDiv();
         show_window(__('Procrastination helper'), $applicationsList);
     }
 } else {
