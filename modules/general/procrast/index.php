be4dcb03bd54b87b20eb0769d2edff999334cc52
Nightfly <nightfly@email.ua>
2018-06-22 14:55:03 +0300

    procrast rework

--- modules/general/procrast/index.php
+++ modules/general/procrast/index.php
@@ -5,15 +5,23 @@ if (cfr('PROCRAST')) {
         $application = vf($_GET['run']);
         switch ($application) {
             case 'tetris':
-                $jsTetris = file_get_contents('modules/jsc/jstetris/tetris.html');
+                $jsTetris = file_get_contents('modules/jsc/procrastdata/jstetris/tetris.html');
                 $jsTetris = str_replace('START_LABEL', __('Press space to play'), $jsTetris);
                 $jsTetris = str_replace('SCORE_LABEL', __('score'), $jsTetris);
                 $jsTetris = str_replace('ROWS_LABEL', __('rows'), $jsTetris);
                 show_window(__('Tetris'), $jsTetris);
                 break;
                case '2048':
-                $jsCode = file_get_contents('modules/jsc/2048/2048.html');
+                $jsCode = file_get_contents('modules/jsc/procrastdata/2048/2048.html');
                 show_window(__('2048'), $jsCode);
+                break;
+             case 'motox3m':
+                $jsCode = file_get_contents('modules/jsc/procrastdata/motox3m.html');
+                show_window(__('Moto X3M'), $jsCode,'center');
+                break;
+            case 'happywheels':
+                $jsCode = file_get_contents('modules/jsc/procrastdata/happywheels.html');
+                show_window(__('Happy Wheels'), $jsCode,'center');
                 break;
         }
         show_window('', wf_BackLink('?module=procrast'));
@@ -21,7 +29,9 @@ if (cfr('PROCRAST')) {
         $applicationsList = '';
         $applicationArr = array(
             'tetris' => __('Tetris'),
-            '2048' => __('2048')
+            '2048' => __('2048'),
+            'motox3m' => __('Moto X3M'),
+            'happywheels' => __('Happy Wheels')
         );
 
         if (!empty($applicationArr)) {
