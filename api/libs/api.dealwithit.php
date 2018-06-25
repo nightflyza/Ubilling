ae4e76876db0489f723c80749ffae2309173339f
Alex <alex.chernyk@gmail.com>
2018-06-25 09:45:21 +0300

    Small fix code for Issue #275

--- api/libs/api.dealwithit.php
+++ api/libs/api.dealwithit.php
@@ -213,8 +213,8 @@ class DealWithIt {
         if ($done) {
             $query = "UPDATE `dealwithithist` SET `done` = '1', `datetimedone` = '" . $mtime . "' WHERE `dealwithithist`.`originalid` = '" . $id . "'";
         } else {
-            $query = "INSERT INTO `dealwithithist` (`id`,`originalid`,`mtime`,`date`, `datetimedone`, `login`,`action`,`param`,`note`,`admin`,`done`) VALUES";
-            $query.="(NULL,'" . $id . "','" . $mtime . "','" . $date . "', '0000-00-00 00:00:00', '" . $login . "','" . $action . "','" . $param . "','" . $note . "','" . $admin . "','0');";
+            $query = "INSERT INTO `dealwithithist` (`id`,`originalid`,`mtime`,`date`, `login`,`action`,`param`,`note`,`admin`,`done`) VALUES";
+            $query.= "(NULL,'" . $id . "','" . $mtime . "','" . $date . "', '" . $login . "','" . $action . "','" . $param . "','" . $note . "','" . $admin . "','0')";
         }
         nr_query($query);
     }
@@ -725,7 +725,7 @@ class DealWithIt {
      */
     public function renderTasksListAjax($login = '') {
         $result = '';
-        $columns = array('ID', 'Date', 'Login', 'Address', 'Real name', 'Task', 'Parameter', 'Notes', 'Actions');
+        $columns = array('ID', 'Target date', 'Login', 'Address', 'Real name', 'Task', 'Parameter', 'Notes', 'Actions');
         $opts = '"order": [[ 0, "desc" ]]';
         $module_link = (empty($login)) ? '?module=report_dealwithit&ajax=true' : '?module=pl_dealwithit&ajax=true&username=' . $login;
         $result = wf_JqDtLoader($columns, $module_link, false, 'Tasks', 100, $opts);
@@ -784,7 +784,7 @@ class DealWithIt {
      */
     public function renderTasksHistoryAjax() {
         $result = '';
-        $columns = array('ID', 'Date', 'Create date', 'Changed', 'Login', 'Address', 'Real name', 'Task', 'Parameter', 'Notes', 'Done', 'Admin');
+        $columns = array('ID', 'Target date', 'Create date', 'Changed', 'Login', 'Address', 'Real name', 'Task', 'Parameter', 'Notes', 'Done', 'Admin');
         $opts = '"order": [[ 0, "desc" ]]';
         $result = wf_JqDtLoader($columns, '?module=report_dealwithit&history=true&ajax=true', false, 'Tasks', 100, $opts);
         return ($result);
