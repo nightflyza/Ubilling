3e1c4b0e0aa5bb57c595e943b57718102adf5cce
Nightfly <nightfly@email.ua>
2018-08-16 18:25:01 +0300

    resolve merge conflicts

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -8,7 +8,7 @@ debarr($hls->getUserInfo(1));
 
 debarr($hls->getTariffsBase()); // 1036 avail
 //debarr($hls->getTariffsBundle()); // 1046 avail
-//debarr($hls->setUserTariff(1,array('base' =>1042, 'bundle' => 1046)));
+//debarr($hls->setUserTariff(1,array('base' =>1036, 'bundle' => 1046)));
 //debarr($hls->setUserBlock(1));
 //debarr($hls->setUserActivate(1));
 //debarr($hls->getDeviceCode(1));
