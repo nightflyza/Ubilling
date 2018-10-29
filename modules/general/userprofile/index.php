282f0104e27879e6975ed9db5e26eb0890a594cb
bobr-kun <demon.bobr@gmail.com>
2018-10-29 16:42:08 +0200

    Removed some shit from userprofile module.
    Signed-off-by: bobr-kun <demon.bobr@gmail.com>

--- modules/general/userprofile/index.php
+++ modules/general/userprofile/index.php
@@ -1,22 +1,6 @@
 <?php
 
 if (cfr('USERPROFILE')) {
-    /*global $ubillingConfig;
-
-    if ( $ubillingConfig->getAlterParam('SMS_SERVICES_ADVANCED_ENABLED') and wf_CheckGet(array('ajax')) ) {
-        if ( wf_CheckPost(array('action')) == 'BindSMSSrv' ) {
-            if ( wf_CheckPost(array('createrec')) ) {
-                $tQuery = "INSERT INTO `sms_services_relations` (`sms_srv_id`, `user_login`)
-                                                         VALUES ('" . $_POST['smssrvid'] . "', '" . $_POST['username'] . "')";
-                nr_query($tQuery);
-            } else {
-                simple_update_field('sms_services_relations', 'sms_srv_id', $_POST['smssrvid'], "WHERE `user_login`='" . $_POST['username'] . "' ");
-            }
-
-            log_register("Prefered SMS service changed from [" . $_POST['oldsmssrvid'] . "] to [" . $_POST['smssrvid'] . "] for user (" . $_POST['username'] . ")");
-        }
-    }*/
-
     if (isset($_GET['username'])) {
         $login = vf($_GET['username']);
         $login = trim($login);
