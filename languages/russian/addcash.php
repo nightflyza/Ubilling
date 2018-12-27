1afdd8d8f18ccc55dfab79e9553e62a4b4c96484
Pautina <alex.chernyk@gmail.com>
2018-12-27 15:28:08 +0200

    add translate for limiting paymants by emploee

---
+++ languages/russian/addcash.php
@@ -0,0 +1,4 @@
+<?
+$lang['def']['Payment amount exceeded per month'] = 'Сумма платежа превышена за месяц';
+$lang['def']['You can top up for the amount of:'] = 'Вы можете пополнить на сумму:';
+?>
---
+++ languages/ukrainian/addcash.php
@@ -0,0 +1,4 @@
+<?
+$lang['def']['Payment amount exceeded per month'] = 'Сума платежу перевищена за місяць';
+$lang['def']['You can top up for the amount of:'] = 'Ви можете поповнити на суму:';
+?>
