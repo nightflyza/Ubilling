d0e17fb134fcdf9feaac4a3a433ae7bb9b97fe00
Nightfly <nightfly@email.ua>
2019-01-14 13:37:05 +0200

    callshist table added

--- docs/test_dump.sql
+++ docs/test_dump.sql
@@ -2315,4 +2315,14 @@ CREATE TABLE IF NOT EXISTS `pononuextusers` (
 
 ALTER TABLE `corp_persons` ADD COLUMN `notes` TEXT NULL AFTER `appointment`;
 
+-- 0.9.5 update
 ALTER TABLE `employee` ADD `amountLimit` VARCHAR(45) NOT NULL DEFAULT '0';
+
+CREATE TABLE IF NOT EXISTS `callshist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `number` varchar(120) NOT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `login` (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
