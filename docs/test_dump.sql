81ae3a4735fc53f48fc6b7ff04d44dfe8338dd72
Nightfly <nightfly@email.ua>
2019-03-01 12:41:16 +0200

    switchgroups default db scheme

--- docs/test_dump.sql
+++ docs/test_dump.sql
@@ -2383,3 +2383,19 @@ CREATE TABLE IF NOT EXISTS `ddt_users` (
   `dwiid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `switch_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(255) NOT NULL,
+  `groupdescr` varchar(500) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`groupname`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `switch_groups_relations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `switch_id` int(11) NOT NULL,
+  `sw_group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`switch_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
