0dfe2ed7213fa0e594412ecfc46fd623d423dd55
Nightfly <nightfly@email.ua>
2019-04-05 15:25:30 +0300

    ddt in progress

---
+++ languages/russian/ddt.php
@@ -0,0 +1,16 @@
+<?php
+$lang['def']['Duration'] = 'Длительность';
+$lang['def']['Take into account the current period'] = 'Учитывать текущий период';
+$lang['def']['Charge current tariff fee'] = 'Начислять абонплату по текущему тарифу';
+$lang['def']['Charge current tariff fee if day less then'] = 'Снимать абонплату текущего тарифа только если дата меньше';
+$lang['def']['Move to tariff after ending of periods'] = 'Перевести на тариф после окончания периодов';
+$lang['def']['Tariffs must be different'] = 'Тарифы должны быть разными';
+$lang['def']['Duration cannot be empty'] = 'Длительность не может быть нулевой или пустой';
+$lang['def']['There is nothing to watch'] = 'Здесь не на что смотреть, расходимся';
+$lang['def']['Start at this period'] = '';
+$lang['def']['Charge fee'] = 'Чарджить АП';
+$lang['def']['Charge until day'] = 'Чарджить до числа';
+$lang['def']['New tariff'] = 'Новый тариф';
+$lang['def'][''] = '';
+$lang['def'][''] = '';
+
--- modules/general/ddt/index.php
+++ modules/general/ddt/index.php
@@ -2,18 +2,58 @@
 
 class DoomsDayTariffs {
 
+    /**
+     * Contains available DDT options aka tariffs as id=>data
+     *
+     * @var array
+     */
     protected $allOptions = array();
+
+    /**
+     * Contains available system tariffs as tariffname=>tariffdata
+     *
+     * @var array
+     */
     protected $allTariffs = array();
+
+    /**
+     * Contains available system tariffs as tariffname=>tariffname
+     *
+     * @var array
+     */
     protected $allTariffNames = array();
+
+    /**
+     * Contains default periods descriptions as period=>periodname
+     *
+     * @var array
+     */
     protected $periods = array();
+
+    /**
+     * System message helper object placeholder
+     *
+     * @var object
+     */
     protected $messages = '';
 
+    /**
+     * Default control module URL
+     */
     const URL_ME = '?module=ddt';
 
+    /**
+     * Creates new DoomsDay instance
+     */
     public function __construct() {
         $this->loadData();
     }
 
+    /**
+     * Loads required datasets at object init
+     * 
+     * @return void
+     */
     protected function loadData() {
         $this->initMessages();
         $this->loadTariffs();
@@ -22,10 +62,20 @@ class DoomsDayTariffs {
         $this->setOptions();
     }
 
+    /**
+     * Inits default system message helper object into protected prop
+     * 
+     * @return void
+     */
     protected function initMessages() {
         $this->messages = new UbillingMessageHelper();
     }
 
+    /**
+     * Loads existing doomsday tariffs
+     * 
+     * @return void
+     */
     protected function loadOptionsDDT() {
         $query = "SELECT * from `ddt_options`";
         $all = simple_queryall($query);
@@ -36,15 +86,30 @@ class DoomsDayTariffs {
         }
     }
 
+    /**
+     * Sets default periods id-s and their localized names
+     * 
+     * @return void
+     */
     protected function setOptions() {
         $this->periods['month'] = __('Month');
         $this->periods['day'] = __('Day');
     }
 
+    /**
+     * Loads existing DDT users database
+     * 
+     * @return void
+     */
     protected function loadUsersDDT() {
         
     }
 
+    /**
+     * Loads available system tariffs into protected prop for further usage
+     * 
+     * @return void
+     */
     protected function loadTariffs() {
         $this->allTariffs = zb_TariffGetAllData();
         if (!empty($this->allTariffs)) {
@@ -54,12 +119,17 @@ class DoomsDayTariffs {
         }
     }
 
+    /**
+     * Renders default DDT tariff creation form
+     * 
+     * @return string
+     */
     public function renderCreateForm() {
         $result = '';
         $inputs = wf_HiddenInput('createnewddtsignal', 'true');
         $inputs.= wf_Selector('createnewddttariff', $this->allTariffNames, __('Tariff'), '', true);
         $inputs.= wf_Selector('createnewddtperiod', $this->periods, __('Period'), '', true);
-        $inputs.= wf_TextInput('createnewddtduration', __('Duration'), '', true, 4, 'digits');
+        $inputs.= wf_TextInput('createnewddtduration', __('Duration'), '1', true, 4, 'digits');
         $inputs.= wf_CheckInput('createnewddtstartnow', __('Take into account the current period'), true, false);
         $inputs.= wf_CheckInput('createnewddtchargefee', __('Charge current tariff fee'), true, false);
         $inputs.= wf_TextInput('createnewddtchargeuntilday', __('Charge current tariff fee if day less then'), '1', true, 2, 'digits');
@@ -71,10 +141,78 @@ class DoomsDayTariffs {
         return ($result);
     }
 
+    /**
+     * Catches DDT Tariff creation request and creates it into database
+     * 
+     * @return void/string on error
+     */
     public function createTariffDDT() {
+        $result = '';
         if (wf_CheckPost(array('createnewddtsignal', 'createnewddttariff', 'createnewddtperiod', 'createnewddtduration', 'createnewddttariffmove'))) {
-            
+            $newTariff = $_POST['createnewddttariff'];
+            $newTariff_f = mysql_real_escape_string($newTariff);
+            $newTariffMove = $_POST['createnewddttariffmove'];
+            $newTariffMove_f = mysql_real_escape_string($_POST['createnewddttariffmove']);
+            $newPeriod = vf($_POST['createnewddtperiod']);
+            $newDuration = vf($_POST['createnewddtduration'], 3);
+            $newStartNow = (wf_CheckPost(array('createnewddtstartnow'))) ? 1 : 0;
+            $newChargeFee = (wf_CheckPost(array('createnewddtchargefee'))) ? 1 : 0;
+            $newChargeDay = vf($_POST['createnewddtchargeuntilday'], 3);
+
+            if ($newTariff != $newTariffMove) {
+                if (!empty($newDuration)) {
+                    $query = "INSERT INTO `ddt_options` (`id`,`tariffname`,`period`,`startnow`,`duration`,`chargefee`,`chargeuntilday`,`tariffmove`)"
+                            . " VALUES (NULL,'" . $newTariff_f . "','" . $newPeriod . "','" . $newStartNow . "','" . $newDuration . "','" . $newChargeFee . "','" . $newChargeDay . "','" . $newTariffMove_f . "'); ";
+                    nr_query($query);
+                    $newId = simple_get_lastid('ddt_options');
+                    log_register('DDT CREATE [' . $newId . '] TARIFF `' . $newTariff . '` MOVE ON `' . $newTariffMove . '` IN ' . $newDuration . ' `' . $newPeriod . '`');
+                } else {
+                    $result = __('Duration cannot be empty');
+                }
+            } else {
+                $result = __('Tariffs must be different');
+            }
         }
+        return ($result);
+    }
+
+    /**
+     * Renders available DDT tariffs list with some controls
+     * 
+     * @return string
+     */
+    public function renderTariffsList() {
+        $result = '';
+        if (!empty($this->allOptions)) {
+            $cells = wf_TableCell(__('ID'));
+            $cells.= wf_TableCell(__('Tariff'));
+            $cells.= wf_TableCell(__('Period'));
+            $cells.= wf_TableCell(__('Start at this period'));
+            $cells.= wf_TableCell(__('Duration'));
+            $cells.= wf_TableCell(__('Charge fee'));
+            $cells.= wf_TableCell(__('Charge until day'));
+            $cells.= wf_TableCell(__('New tariff'));
+            $cells.= wf_TableCell(__('Actions'));
+            $rows = wf_TableRow($cells, 'row1');
+            
+            foreach ($this->allOptions as $io => $each) {
+                $cells = wf_TableCell($each['id']);
+                $cells.= wf_TableCell($each['tariffname']);
+                $cells.= wf_TableCell($this->periods[$each['period']]);
+                $cells.= wf_TableCell(web_bool_led($each['startnow']));
+                $cells.= wf_TableCell($each['duration']);
+                $cells.= wf_TableCell(web_bool_led($each['chargefee']));
+                $cells.= wf_TableCell($each['chargeuntilday']);
+                $cells.= wf_TableCell($each['tariffmove']);
+                $cells.= wf_TableCell(__('Actions'));
+                $rows.= wf_TableRow($cells, 'row3');
+            }
+
+            $result.=wf_TableBody($rows, '100%', 0, 'sortable');
+        } else {
+            $result.=$this->messages->getStyledMessage(__('There is nothing to watch') . '.', 'info');
+        }
+        return ($result);
     }
 
 }
@@ -82,9 +220,16 @@ class DoomsDayTariffs {
 $ddt = new DoomsDayTariffs();
 
 if (wf_CheckPost(array('createnewddtsignal'))) {
-    $ddt->createTariffDDT();
+    $creationResult = $ddt->createTariffDDT();
+    if (empty($creationResult)) {
+        rcms_redirect($ddt::URL_ME);
+    } else {
+        show_error($creationResult);
+        show_window('', wf_BackLink($ddt::URL_ME));
+    }
 }
 
 
 deb($ddt->renderCreateForm());
+deb($ddt->renderTariffsList());
 ?>
