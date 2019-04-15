4e4f1ddd6ed11e8902944bcb14ff8810c371b791
bobr-kun <demon.bobr@gmail.com>
2019-04-15 16:54:51 +0300

    New module for printing receipts for Internet and UKV users. New alter.ini option PRINT_RECEIPTS_MODULE_ENABLED to enable/disable the receipts printing module. New template examples for receipts(QR-codes may be used if necessary). Some new macro options added for this template. New JS library for in-document(without producing any excess files) QR-codes generating added. Added all necessary translations and icon files.

---
+++ languages/russian/printreceipt.php
@@ -0,0 +1,7 @@
+<?php
+$lang['def']['right to print receipts'] = 'Право печатать квитанции';
+$lang['def']['Print receipts'] = 'Печать квитанций';
+$lang['def']['Subscriber\'s account status'] = 'Состояние счета абонента';
+$lang['def']['Cable television'] = 'Кабельное телевидение';
+$lang['def']['Amount of months to be payed(will be multiplied on tariff cost)'] = 'Количество месяцев к оплате(будет умножено на стоимость тарифа)';
+$lang['def']['Pay till date'] = 'Оплатить до';
