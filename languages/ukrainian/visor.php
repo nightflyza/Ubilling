c7adae1572912c0716842846dc0dc3e5d89db276
Nightfly <nightfly@email.ua>
2019-07-02 19:14:48 +0300

    visor module i18n update

--- languages/ukrainian/visor.php
+++ languages/ukrainian/visor.php
@@ -4,3 +4,4 @@ $lang['def']['Cams'] = 'Камери';
 $lang['def']['DVRs'] = 'DVRи';
 $lang['def']['Charge money from primary account for linked camera users if required'] = 'Чарджити кошти з основного аккаунту для зв`язаних камер, якщо потрібно';
 $lang['def']['User have some cameras associated'] = 'В користувача є пов`язані з ним камери';
+$lang['def']['right to control visor module'] = 'Право керувати модулем Visor';
