f5d947878fcab8b131a9ba8ea757613fe79d8538
Nightfly <nightfly@email.ua>
2019-07-09 18:17:35 +0300

    leaflet works with swmap, usermaps, custmaps and other basic modules

--- api/libs/api.lmaps.php
+++ api/libs/api.lmaps.php
@@ -23,6 +23,66 @@ function generic_MapContainer($width = '', $height = '', $id = '') {
 }
 
 /**
+ * Translates yandex to google icon code
+ * 
+ * @param string $icon
+ * @return string
+ */
+function lm_GetIconUrl($icon) {
+    $result = '';
+    switch ($icon) {
+        case 'twirl#lightblueIcon':
+            $result = 'skins/mapmarks/blue.png';
+            break;
+        case 'twirl#lightblueStretchyIcon':
+            $result = 'skins/mapmarks/blue.png';
+            break;
+        case 'twirl#redStretchyIcon':
+            $result = 'skins/mapmarks/red.png';
+            break;
+        case 'twirl#yellowIcon':
+            $result = 'skins/mapmarks/yellow.png';
+            break;
+        case 'twirl#greenIcon':
+            $result = 'skins/mapmarks/green.png';
+            break;
+        case 'twirl#pinkDotIcon':
+            $result = 'skins/mapmarks/pink.png';
+            break;
+        case 'twirl#brownIcon':
+            $result = 'skins/mapmarks/brown.png';
+            break;
+        case 'twirl#nightDotIcon':
+            $result = 'skins/mapmarks/darkblue.png';
+            break;
+        case 'twirl#redIcon':
+            $result = 'skins/mapmarks/red.png';
+            break;
+        case 'twirl#orangeIcon':
+            $result = 'skins/mapmarks/orange.png';
+            break;
+        case 'twirl#greyIcon':
+            $result = 'skins/mapmarks/grey.png';
+            break;
+        case 'twirl#buildingsIcon':
+            $result = 'skins/mapmarks/build.png';
+            break;
+        case 'twirl#houseIcon':
+            $result = 'skins/mapmarks/house.png';
+            break;
+        case 'twirl#campingIcon':
+            $result = 'skins/mapmarks/camping.png';
+            break;
+
+        default :
+            $result = 'skins/mapmarks/blue.png';
+            show_warning('Unknown icon received: ' . $icon);
+            break;
+    }
+    return ($result);
+}
+
+/**
  * Returns placemark code
  * 
  * @param string $coords
@@ -41,9 +101,34 @@ function generic_MapAddMark($coords, $title = '', $content = '', $footer = '', $
     $title = str_replace('"', '\"', $title);
     $content = str_replace('"', '\"', $content);
     $footer = str_replace('"', '\"', $footer);
+    $iconCode = '';
+    $iconDefines = '';
 
-    $result .= 'L.marker([' . $coords . ']).addTo(map)
+    if (!empty($icon)) {
+        $iconFile = lm_GetIconUrl($icon);
+        $iconDefines .= "var LeafIcon = L.Icon.extend({
+		options: {
+			iconSize:     [42, 42],
+			iconAnchor:   [22, 41],
+			popupAnchor:  [-3, -44]
+		}
+	});
+        
+
+      	var customIcon = new LeafIcon({iconUrl: '" . $iconFile . "'});
+
+";
+        $iconCode .= ', {icon: customIcon}';
+    }
+
+    $result .= $iconDefines;
+    $result .= 'var placemark=L.marker([' . $coords . ']' . $iconCode . ').addTo(map)
 		.bindPopup("<b>' . $title . '</b><br />' . $content . '<br>' . $footer . '");';
+
+    if (!empty($content)) {
+        $result .= 'placemark.bindTooltip("' . $content . '", { sticky: true});';
+    }
+
     return($result);
 }
 
@@ -95,7 +180,8 @@ function generic_MapAddCircle($coords, $radius, $content = '', $hint = '') {
 function generic_MapInit($center, $zoom, $type, $placemarks = '', $editor = '', $lang = 'ru-RU', $container = 'ubmap') {
     global $ubillingConfig;
     $mapsCfg = $ubillingConfig->getYmaps();
-
+    $result = '';
+    $tileLayerCustoms = '';
     if (empty($center)) {
         //autolocator here
         $mapCenter = 'map.locate({setView: true, maxZoom: ' . $zoom . '});';
@@ -109,17 +195,27 @@ function generic_MapInit($center, $zoom, $type, $placemarks = '', $editor = '', 
         $mapCenter = 'map.setView([' . $center . '], ' . $zoom . ');';
     }
 
-    //default tile layer
-    $tileLayer = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    //default OSM tile layer
+    $tileLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
     //custom tile layer
     if (isset($mapsCfg['LEAFLET_TILE_LAYER'])) {
         if ($mapsCfg['LEAFLET_TILE_LAYER']) {
             $tileLayer = $mapsCfg['LEAFLET_TILE_LAYER'];
+            //Visicom custom options
+            if (ispos($tileLayer, 'visicom')) {
+                $tileLayerCustoms = "subdomains: '123',
+                tms: true";
+            }
+            
+            //google satellite
+            if (ispos($tileLayer, 'google.com')) {
+                $tileLayerCustoms="subdomains:['mt0','mt1','mt2','mt3']";
+            }
         }
     }
 
-    $result = '';
+
 
     $result .= '<link rel="stylesheet" href="modules/jsc/leaflet/leaflet.css"/>';
     $result .= wf_tag('script', false, '', 'src="modules/jsc/leaflet/leaflet.js"');
@@ -132,7 +228,8 @@ function generic_MapInit($center, $zoom, $type, $placemarks = '', $editor = '', 
 	L.tileLayer(\'' . $tileLayer . '\', {
 		maxZoom: 18,
 		attribution: \'\',
-		id: \'mapbox.streets\'
+		id: \'mapbox.streets\',
+                ' . $tileLayerCustoms . '
 	}).addTo(map);
 
 	' . $placemarks . '
@@ -163,12 +260,12 @@ function generic_MapEditor($name, $title = '', $data = '') {
     $windowCode = '<b>' . $title . '</b><br>' . $content;
     $result = 'var popup = L.popup();
 
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-                        .setContent("' . $windowCode . '<br>" + e.latlng.lat + ", " + e.latlng.lng)
-			.openOn(map);
-	}
+                function onMapClick(e) {
+                        popup
+                                .setLatLng(e.latlng)
+                                .setContent("' . $windowCode . '<br>" + e.latlng.lat + ", " + e.latlng.lng)
+                                .openOn(map);
+                }
 
 	map.on(\'click\', onMapClick);';
 
@@ -188,7 +285,7 @@ function generic_MapEditor($name, $title = '', $data = '') {
 function generic_MapAddLine($coord1, $coord2, $color = '', $hint = '', $width = '') {
     $lineId = wf_InputId();
     $color = (!empty($color)) ? $color : '#000000';
-    $width = (!empty($color)) ? $width : '1';
+    $width = (!empty($color)) ? $width + 1 : '1';
 
     $result = '';
     $result .= '
