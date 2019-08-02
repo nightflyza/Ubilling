0249dbac42650a84ddccba92fdee9d046f5769b1
Nightfly <nightfly@email.ua>
2019-08-02 18:56:25 +0300

    fdb archive display basicly works

--- api/libs/api.fdbarchive.php
+++ api/libs/api.fdbarchive.php
@@ -17,6 +17,41 @@ class FDBArchive {
     protected $allSwitches = array();
 
     /**
+     * Contains existing switches MAC addresses as mac=>id
+     *
+     * @var array
+     */
+    protected $allSwitchesMac = array();
+
+    /**
+     * Contains available users data from cache
+     *
+     * @var array
+     */
+    protected $allUserData = array();
+
+    /**
+     * Contains available users mac address as mac=>login
+     *
+     * @var array
+     */
+    protected $allUsersMac = array();
+
+    /**
+     * Contains available ONU devices mac address as mac=>id
+     *
+     * @var array
+     */
+    protected $allOnuMac = array();
+
+    /**
+     * Contains available ONU devices assigned users id=>login
+     *
+     * @var array
+     */
+    protected $allOnuUsers = array();
+
+    /**
      * Protected database model placeholder
      *
      * @var object
@@ -54,6 +89,18 @@ class FDBArchive {
      * Contains default module controller URL
      */
     const URL_ME = '?module=fdbarchive';
+
+    /**
+     * Contains default fdb cache module URL
+     */
+    const URL_CACHE = '?module=switchpoller';
+
+    /**
+     * Another required URLs for internal routing
+     */
+    const URL_SWITCHPROFILE = '?module=switches&edit=';
+    const URL_USERPROFILE = '?module=userpfofile&username=';
+    const URL_ONUPROFILE = '?module=ponizer&editonu=';
 
     public function __construct() {
         $this->loadConfigs();
@@ -100,6 +147,46 @@ class FDBArchive {
     protected function loadSwitches() {
         $switches = new nya_switches();
         $this->allSwitches = $switches->getAll('id');
+        if (!empty($this->allSwitches)) {
+            foreach ($this->allSwitches as $io => $each) {
+                if (!empty($each['swid'])) {
+                    $this->allSwitchesMac[$each['swid']] = $each['id'];
+                }
+            }
+        }
+    }
+
+    /**
+     * Loads user data into protected properties for further usage
+     * 
+     * @return void
+     */
+    protected function loadUserData() {
+        $this->allUserData = zb_UserGetAllDataCache();
+        if (!empty($this->allUserData)) {
+            $this->allUsersMac = zb_UserGetAllMACs();
+            $this->allUsersMac = array_flip($this->allUsersMac);
+        }
+    }
+
+    /**
+     * Loads PON ONU devices data into protected properties for further usage
+     * 
+     * @return void
+     */
+    protected function loadOnuData() {
+        $onu = new nya_pononu();
+        $allOnu = $onu->getAll();
+        if (!empty($allOnu)) {
+            foreach ($allOnu as $io => $each) {
+                if (!empty($each['mac'])) {
+                    $this->allOnuMac[$each['mac']] = $each['id'];
+                    if (!empty($each['login'])) {
+                        $this->allOnuUsers[$each['id']] = $each['login'];
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -244,7 +331,44 @@ class FDBArchive {
     public function renderArchive() {
         $result = '';
         $columns = array('Date', __('Switch') . ' / ' . __('OLT'), 'Port', 'Location', 'MAC', __('User') . ' / ' . __('Device'));
-        $result .= wf_JqDtLoader($columns, self::URL_ME . '&ajax=true', false, 'records', 100);
+        $result .= wf_JqDtLoader($columns, self::URL_ME . '&ajax=true', true, 'records', 100);
+        return($result);
+    }
+
+    /**
+     * Trying to detect is device switch/user or ONU by mac. Returns profile view control.
+     * 
+     * @param string $mac
+     * 
+     * @return string
+     */
+    protected function getEntityControl($mac) {
+        $result = '';
+        if (isset($this->allUsersMac[$mac])) {
+            $userLogin = $this->allUsersMac[$mac];
+            $result .= wf_Link(self::URL_USERPROFILE . $userLogin, web_profile_icon() . ' ' . @$this->allUserData[$userLogin]['fulladress']);
+            return($result);
+        }
+
+        if (isset($this->allSwitchesMac[$mac])) {
+            $switchId = $this->allSwitchesMac[$mac];
+            $switchIcon = wf_img('skins/menuicons/switches.png', __('Switch')) . ' ';
+            $result .= wf_Link(self::URL_SWITCHPROFILE . $switchId, $switchIcon . @$this->allSwitches[$switchId]['location']);
+            return($result);
+        }
+
+        if (isset($this->allOnuMac[$mac])) {
+            $onuId = $this->allOnuMac[$mac];
+            $onuIcon = wf_img('skins/switch_models.png', __('ONU')) . ' ';
+            $onuAssignedUser = '';
+            if (isset($this->allOnuUsers[$onuId])) {
+                $onuUserLogin = $this->allOnuUsers[$onuId];
+                if (isset($this->allUserData[$onuUserLogin])) {
+                    $onuAssignedUser .= @$this->allUserData[$onuUserLogin]['fulladress'];
+                }
+            }
+            $result .= wf_Link(self::URL_ONUPROFILE . $onuId, $onuIcon . $onuAssignedUser);
+        }
         return($result);
     }
 
@@ -268,15 +392,15 @@ class FDBArchive {
                     foreach ($fdbData as $eachMac => $eachPort) {
                         $switchLink = $switchIcon . ' ' . __('Not exists');
                         if (!empty($recordId)) {
-                            $switchLink = wf_Link('?module=switches&editid=' . $recordId, $switchIcon . @$this->allSwitches[$recordId]['location']);
+                            $switchLink = wf_Link(self::URL_SWITCHPROFILE . $recordId, $switchIcon . @$this->allSwitches[$recordId]['location']);
                         }
                         $data[] = $recordDate;
                         $data[] = $recordIp;
                         $data[] = $eachPort;
                         $data[] = $switchLink;
                         $data[] = $eachMac;
-                        $data[] = 'USER_TODO';
-                        $this->json->addRow($data); // HELL YEAH!!!!
+                        $data[] = $this->getEntityControl($eachMac);
+                        $this->json->addRow($data);
                         unset($data);
                     }
                 }
@@ -294,6 +418,10 @@ class FDBArchive {
     public function ajArchiveData() {
         $allArchiveRecords = $this->archive->getAll();
         if (!empty($allArchiveRecords)) {
+            $this->loadUserData();
+            if ($this->altCfg['PON_ENABLED']) {
+                $this->loadOnuData();
+            }
             foreach ($allArchiveRecords as $io => $each) {
                 $this->parseData($each);
             }
@@ -307,8 +435,8 @@ class FDBArchive {
      * @return string
      */
     public static function renderNavigationPanel() {
-        $result = wf_Link('?module=switchpoller', wf_img('skins/fdbmacsearch.png') . ' ' . __('Current FDB cache'), false, 'ubButton') . ' ';
-        $result .= wf_Link(self::URL_ME, wf_img('skins/time_machine.png') . ' ' . __('FDB') . ' ' . __('Archive'), false, 'ubButton') . ' ';
+        $result = wf_Link(self::URL_CACHE, wf_img_sized('skins/fdbmacsearch.png','','16','16') . ' ' . __('Current FDB cache'), false, 'ubButton') . ' ';
+        $result .= wf_Link(self::URL_ME, wf_img('skins/time_machine.png','','16','16') . ' ' . __('FDB') . ' ' . __('Archive'), false, 'ubButton') . ' ';
         return($result);
     }
 
