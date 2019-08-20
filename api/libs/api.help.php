66f9dfff5ae4886387fd463cd1792f1a0e45d433
Nightfly <nightfly@email.ua>
2019-08-20 18:05:49 +0300

    now for empty module route showing help chapter of taskbar

--- api/libs/api.help.php
+++ api/libs/api.help.php
@@ -12,7 +12,7 @@ function web_HelpChapterGet($chapter) {
     $chapter = vf($chapter);
     $result = '';
     if (file_exists(DATA_PATH . "help/" . $lang . "/" . $chapter)) {
-        $result.=file_get_contents(DATA_PATH . "help/" . $lang . "/" . $chapter);
+        $result .= file_get_contents(DATA_PATH . "help/" . $lang . "/" . $chapter);
         $result = nl2br($result);
     }
     return ($result);
@@ -29,13 +29,15 @@ function web_HelpIconShow() {
     if (cfr('HELP')) {
         if (isset($_GET['module'])) {
             $modulename = vf($_GET['module']);
-            if (file_exists(DATA_PATH . "help/" . $lang . "/" . $modulename)) {
-                $help_chapter = web_HelpChapterGet($modulename);
-                if (cfr('PROCRAST')) {
-                    $help_chapter.=wf_delimiter() . wf_Link('?module=procrast', wf_img('skins/gamepad.png', __('Procrastination helper')));
-                }
-                $result = wf_modal(wf_img_sized("skins/help.gif", __('Context help'), 20), __('Context help'), $help_chapter, '', '600', '300');
+        } else {
+            $modulename = 'taskbar';
+        }
+        if (file_exists(DATA_PATH . "help/" . $lang . "/" . $modulename)) {
+            $help_chapter = web_HelpChapterGet($modulename);
+            if (cfr('PROCRAST')) {
+                $help_chapter .= wf_delimiter() . wf_Link('?module=procrast', wf_img('skins/gamepad.png', __('Procrastination helper')));
             }
+            $result = wf_modal(wf_img_sized("skins/help.gif", __('Context help'), 20), __('Context help'), $help_chapter, '', '600', '300');
         }
     }
     return ($result);
