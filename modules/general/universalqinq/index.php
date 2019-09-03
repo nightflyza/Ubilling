bd22b30bbc9d0336a321a19dd79e7e6da618061e
l1ght13aby <l1ght13aby@yahoo.com>
2019-09-03 16:22:55 +0200

    universal qinq draft #1

--- modules/general/universalqinq/index.php
+++ modules/general/universalqinq/index.php
@@ -4,6 +4,12 @@ if (cfr('UNIVERSALQINQCONFIG')) {
     $altCfg = $ubillingConfig->getAlter();
     if ($altCfg['UNIVERSAL_QINQ_ENABLED']) {
         $qinq = new UniversalQINQ();
+
+        if ($qinq->routing->checkGet('ajax')) {
+            $qinq->ajaxData();
+        }
+
+        $qinq->addForm();
         $qinq->showAll();
     }
 }
