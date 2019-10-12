f56425b14d5eb2b11de6c38c6b3da8f43b4fb2e3
discapacidad5 <discapacidad5@gmail.com>
2019-10-11 22:43:25 -0400

    translation to Spanish
    module were translated and some words were corrected

--- languages/spanish/taskman.php
+++ languages/spanish/taskman.php
@@ -1,3 +1,3 @@
 <?
-$lang['def']['can watch log change for tasks'] = 'Право смотреть лог изменений задач';
+$lang['def']['can watch log change for tasks'] = 'derecho a ver el registro de cambios de tareas';
 ?>
