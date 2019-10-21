251e82f6cda4c5e4b5b795f5ca7b56984473dd79
l1ght13aby <l1ght13aby@yahoo.com>
2019-10-22 00:17:13 +0300

    new table

--- content/updates/sql/1.0.2.sql
+++ content/updates/sql/1.0.2.sql
@@ -36,3 +36,14 @@ UPDATE `switches_qinq` AS `swq`, `qinq_svlan` AS `qsv` SET `swq`.`svlan_id` = `q
 
 ALTER TABLE `switches_qinq` DROP `svlan`;
 
+CREATE TABLE IF NOT EXISTS `zte_qinq` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `swid` int(11) NOT NULL,
+    `slot_number` int(11) NOT NULL,
+    `port` int(4) NOT NULL,
+    `svlan_id` int(11) NOT NULL,
+    `cvlan` int(4) NOT NULL,        
+    PRIMARY KEY (`id`),
+    KEY (`svlan_id`),
+    KEY (`cvlan`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
