59ec1ade5ce9939d1a8cfecfe8c210b26d32fa41
l1ght13aby <l1ght13aby@yahoo.com>
2019-11-06 15:12:34 +0100

    adding option to alter.ini and added new table to dump

--- docs/test_dump.sql
+++ docs/test_dump.sql
@@ -2618,3 +2618,17 @@ ALTER TABLE `switches_qinq` ADD `svlan_id` int(11) NOT NULL AFTER `switchid`;
 ALTER TABLE `switches_qinq` ADD KEY (`svlan_id`);
 
 ALTER TABLE `switches_qinq` DROP `svlan`;
+
+CREATE TABLE IF NOT EXISTS `zte_qinq` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `swid` int(11) NOT NULL,
+    `slot_number` int(11) NOT NULL,
+    `port` int(4) NOT NULL,
+    `svlan_id` int(11) NOT NULL,
+    `cvlan` int(4) NOT NULL,        
+    PRIMARY KEY (`id`),
+    KEY (`svlan_id`),
+    KEY (`cvlan`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
