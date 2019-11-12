74d2f050cf9dab71ae613ec492b0af785879111b
l1ght13aby <l1ght13aby@yahoo.com>
2019-11-12 15:21:11 +0100

    Change vlan check for onureg

--- modules/general/zteunreg/index.php
+++ modules/general/zteunreg/index.php
@@ -39,59 +39,65 @@ if (@$altcfg[OnuRegister::MODULE_CONFIG]) {
                     show_window(__('Register'), $register->registerOnuForm());
                 }
             }
-            if (wf_CheckPost(array(OnuRegister::TYPE_FIELD, OnuRegister::INTERFACE_FIELD, OnuRegister::OLTIP_FIELD, OnuRegister::MODELID_FIELD, OnuRegister::VLAN_FIELD, OnuRegister::OLTID_FIELD))) {
+            if (wf_CheckPost(array(OnuRegister::TYPE_FIELD, OnuRegister::INTERFACE_FIELD, OnuRegister::OLTIP_FIELD, OnuRegister::MODELID_FIELD, OnuRegister::OLTID_FIELD))) {
                 if ($_POST[OnuRegister::MODELID_FIELD] != OnuRegister::MODELID_PLACEHOLDER) {
-                    $register->addMac = OnuRegister::EMPTY_FIELD;
-                    $save = false;
-                    $router = false;
-                    $register->login = OnuRegister::EMPTY_FIELD;
-                    $PONizerAdd = false;
-                    if (wf_CheckPost(array(OnuRegister::LOGIN_FIELD))) {
-                        $register->login = $_POST[OnuRegister::LOGIN_FIELD];
-                    }
-                    if (wf_CheckPost(array(OnuRegister::MAC_FIELD))) {
-                        $onuIdentifier = $_POST[OnuRegister::MAC_FIELD];
-                    }
-                    if (wf_CheckPost(array(OnuRegister::SN_FIELD))) {
-                        $onuIdentifier = $_POST[OnuRegister::SN_FIELD];
-                    }
-                    if (isset($_POST[OnuRegister::ROUTER_FIELD])) {
-                        $router = $_POST[OnuRegister::ROUTER_FIELD];
-                    }
-                    if (isset($_POST[OnuRegister::MAC_ONU_FIELD])) {
-                        $register->addMac = $_POST[OnuRegister::MAC_ONU_FIELD];
-                    }
-                    if (isset($_POST[OnuRegister::RANDOM_MAC_FIELD])) {
-                        $register->addMac = $register->generateRandomOnuMac();
-                    }
-                    if (isset($_POST[OnuRegister::SAVE_FIELD])) {
-                        $save = $_POST[OnuRegister::SAVE_FIELD];
-                    }
-                    if (isset($_POST[OnuRegister::PONIZER_ADD_FIELD])) {
-                        $register->ponizerAdd = true;
-                    }
-                    $register->currentOltIp = $_POST[OnuRegister::OLTIP_FIELD];
-                    $register->currentOltInterface = $_POST[OnuRegister::INTERFACE_FIELD];
-                    $register->currentPonType = $_POST[OnuRegister::TYPE_FIELD];
-                    $register->onuIdentifier = $onuIdentifier;
-                    $register->currentOltSwId = $_POST[OnuRegister::OLTID_FIELD];
-                    $register->save = $save;
-                    $register->router = $router;
-                    $register->vlan = $_POST[OnuRegister::VLAN_FIELD];
-                    $register->onuModel = $_POST[OnuRegister::MODELID_FIELD];
-                    if (isset($_POST[OnuRegister::GET_UNIVERSALQINQ])) {
-                        $register->useUniversalQINQ = $_POST[OnuRegister::GET_UNIVERSALQINQ];
-                    }
-                    $loginCheck = $register->checkOltParams();
-                    if ($loginCheck !== OnuRegister::NO_ERROR_CONNECTION) {
-                        show_error(__($loginCheck));
-                    } else {
-                        $register->$avidity_w();
-                        if (empty($register->error)) {
-                            show_window(__('Result'), $register->result);
-                        } else {
-                            show_error(__($register->error) . ': ' . count($register->existId));
+                    if (wf_CheckPost(array(OnuRegister::VLAN_FIELD) OR $_POST[OnuRegister::GET_UNIVERSALQINQ] != 'none')) {
+
+                        $register->addMac = OnuRegister::EMPTY_FIELD;
+                        $save = false;
+                        $router = false;
+                        $register->login = OnuRegister::EMPTY_FIELD;
+                        $PONizerAdd = false;
+                        if (wf_CheckPost(array(OnuRegister::LOGIN_FIELD))) {
+                            $register->login = $_POST[OnuRegister::LOGIN_FIELD];
                         }
+                        if (wf_CheckPost(array(OnuRegister::MAC_FIELD))) {
+                            $onuIdentifier = $_POST[OnuRegister::MAC_FIELD];
+                        }
+                        if (wf_CheckPost(array(OnuRegister::SN_FIELD))) {
+                            $onuIdentifier = $_POST[OnuRegister::SN_FIELD];
+                        }
+                        if (isset($_POST[OnuRegister::ROUTER_FIELD])) {
+                            $router = $_POST[OnuRegister::ROUTER_FIELD];
+                        }
+                        if (isset($_POST[OnuRegister::MAC_ONU_FIELD])) {
+                            $register->addMac = $_POST[OnuRegister::MAC_ONU_FIELD];
+                        }
+                        if (isset($_POST[OnuRegister::RANDOM_MAC_FIELD])) {
+                            $register->addMac = $register->generateRandomOnuMac();
+                        }
+                        if (isset($_POST[OnuRegister::SAVE_FIELD])) {
+                            $save = $_POST[OnuRegister::SAVE_FIELD];
+                        }
+                        if (isset($_POST[OnuRegister::PONIZER_ADD_FIELD])) {
+                            $register->ponizerAdd = true;
+                        }
+                        $register->currentOltIp = $_POST[OnuRegister::OLTIP_FIELD];
+                        $register->currentOltInterface = $_POST[OnuRegister::INTERFACE_FIELD];
+                        $register->currentPonType = $_POST[OnuRegister::TYPE_FIELD];
+                        $register->onuIdentifier = $onuIdentifier;
+                        $register->currentOltSwId = $_POST[OnuRegister::OLTID_FIELD];
+                        $register->save = $save;
+                        $register->router = $router;
+                        $register->vlan = $_POST[OnuRegister::VLAN_FIELD];
+                        $register->onuModel = $_POST[OnuRegister::MODELID_FIELD];
+                        if (isset($_POST[OnuRegister::GET_UNIVERSALQINQ])) {
+                            $register->useUniversalQINQ = $_POST[OnuRegister::GET_UNIVERSALQINQ];
+                        }
+                        $loginCheck = $register->checkOltParams();
+                        if ($loginCheck !== OnuRegister::NO_ERROR_CONNECTION) {
+                            show_error(__($loginCheck));
+                        } else {
+                            $register->$avidity_w();
+                            if (empty($register->error)) {
+                                show_window(__('Result'), $register->result);
+                            } else {
+                                show_error(__($register->error) . ': ' . count($register->existId));
+                            }
+                        }
+                    } else {
+                        show_error(__(OnuRegister::ERROR_NOT_ALL_FIELDS));
+                        show_error(__(OnuRegister::ERROR_NO_VLAN_SET));
                     }
                 } else {
                     show_error(__(OnuRegister::ERROR_WRONG_MODELID));
@@ -103,9 +109,6 @@ if (@$altcfg[OnuRegister::MODULE_CONFIG]) {
                 }
                 if (!wf_CheckPost(array(OnuRegister::OLTIP_FIELD))) {
                     show_error(__(OnuRegister::ERROR_NO_OLTIP_SET));
-                }
-                if (!wf_CheckPost(array(OnuRegister::VLAN_FIELD))) {
-                    show_error(__(OnuRegister::ERROR_NO_VLAN_SET));
                 }
             }
         } else {
