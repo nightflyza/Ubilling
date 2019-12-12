9829ffe181f6180d8902949caafa3e1475d27d36
Nightfly <nightfly@email.ua>
2019-12-12 12:05:25 +0200

    cleanup

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -2,21 +2,3 @@
 
 //just dummy module for testing purposes
 error_reporting(E_ALL);
-
-//
-//
-//$dvrDb = new nya_visor_dvrs();
-//$dvrDb->where('id', '=', '10');
-//$dvrData = $dvrDb->getAll();
-//$dvrData = $dvrData[0];
-//
-//
-//
-//$trassir = new TrassirServer($dvrData['ip'], $dvrData['login'], $dvrData['password'], $dvrData['apikey']);
-//$health = $trassir->getHealth(); //by some reason now everything works only after this request
-//if (!empty($health)) {
-//    debarr($trassir->getChannels());
-//    //$obj = $trassir->getLiveVideoStream('LL0c1qK0', 'main','mjpeg');
-//    debarr($obj);
-//}
-//
