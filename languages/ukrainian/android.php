a6ac43de7d89f70bce5ef14dd19457c2d4834680
Nightfly <nightfly@email.ua>
2019-12-20 12:26:02 +0200

    i18n update

---
+++ languages/ukrainian/android.php
@@ -0,0 +1,7 @@
+<?php
+$lang['def']['Right to use android integration API'] = 'Право використовувати API інтеграції Android';
+$lang['def']['Right to use android API debug information'] = 'Право використовувати зневаджувальну інформацію API для Android';
+$lang['def']['Username cannot be empty'] = 'Ім`я користувача не може бути порожнім';
+
+
+?>
