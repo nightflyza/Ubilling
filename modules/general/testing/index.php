055137252d9037f035b0eca7f98206cdcbbe9701
Nightfly <nightfly@email.ua>
2020-01-22 23:05:05 +0200

    fuck that shit

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -3,14 +3,3 @@
 //just dummy module for testing purposes
 error_reporting(E_ALL);
 
-//
-//$allSwitches = zb_SwitchesGetAllGeoId();
-//if (!empty($allSwitches)) {
-//    foreach ($allSwitches as $io => $each) {
-//        if (!empty($each)) {
-//            if (ispos($each, ' ')) {
-//                debarr($each);    
-//            }
-//        }
-//    }
-//}
