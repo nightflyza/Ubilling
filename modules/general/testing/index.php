b044d88314f4eeb6fe68b9645f65043431dfdab3
Nightfly <nightfly@email.ua>
2020-02-01 14:40:30 +0200

    aha

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -3,15 +3,3 @@
 //just dummy module for testing purposes
 error_reporting(E_ALL);
 
-
-//$dvrs = new nya_visor_dvrs();
-//$dvrs->where('id', '=', '10');
-//$dvrData = $dvrs->getAll();
-//$dvrData = $dvrData[0];
-//
-//$trassir = new TrassirServer($dvrData['ip'], $dvrData['login'], $dvrData['password'], $dvrData['apikey'], 8080, true);
-//
-//debarr($trassir->getHealth());
-
-//$visor=new UbillingVisor();
-//$visor->regenerateDvrChannelAcl(13, 10);
