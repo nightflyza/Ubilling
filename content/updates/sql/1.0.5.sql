72f327c82400ee1ab5aa409f6b31b71b3d3c318c
bobr-kun <demon.bobr@gmail.com>
2020-02-18 22:16:09 +0200

    Rebased and made suitable for 1.0.6

--- content/updates/sql/1.0.5.sql
+++ content/updates/sql/1.0.5.sql
@@ -18,17 +18,3 @@ ALTER TABLE `frozen_charge_days` ADD `last_freeze_charge_dt` datetime NOT NULL A
 ALTER TABLE `frozen_charge_days` ADD `last_workdays_upd_dt` datetime NOT NULL;
 
 ALTER TABLE `visor_dvrs` ADD `camlimit` int(11) NULL DEFAULT 0 AFTER `type`;
-
-ALTER TABLE `vservices` MODIFY `price` double NOT NULL DEFAULT 0;
-ALTER TABLE `vservices` ADD `charge_period_days` tinyint(3) NOT NULL DEFAULT 0;
-
-CREATE TABLE IF NOT EXISTS `invoices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
-  `invoice_num` varchar(40) NOT NULL DEFAULT '',
-  `invoice_date` datetime NOT NULL,
-  `invoice_sum` double NOT NULL DEFAULT 0,
-  `invoice_body` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`invoice_num`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
