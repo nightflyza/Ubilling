0fe320e7791e6cb2ee31306e06e8e98c3a487111
Nightfly <nightfly@email.ua>
2020-02-20 16:12:23 +0200

    cleanup

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -3,13 +3,5 @@
 //just dummy module for testing purposes
 error_reporting(E_ALL);
 
-//$dvrs = new NyanORM('visor_dvrs');
-//$dvrs->where('id', '=', 10);
-//$dvrData = $dvrs->getAll();
-//$dvrData = $dvrData[0];
-//
-//$trassir = new TrassirServer($dvrData['ip'], $dvrData['login'], $dvrData['password'], $dvrData['apikey'], $dvrData['port'], false);
-//
-//
-//deb($trassir->camtest('TRASSIR', 'Autodetect', '192.168.0.153', '80', 'admin', 'admin'));
+
 
