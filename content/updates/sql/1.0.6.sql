19d5c709a53d18920901606939eff684a52d4fe8
Nightfly <nightfly@email.ua>
2020-03-24 22:31:18 +0200

    moved SQL update to 1.0.7

--- content/updates/sql/1.0.6.sql
+++ content/updates/sql/1.0.6.sql
@@ -11,16 +11,3 @@ CREATE TABLE IF NOT EXISTS `invoices` (
   PRIMARY KEY (`id`),
   UNIQUE KEY (`invoice_num`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `address_extended` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
-  `postal_code` varchar(10) NOT NULL DEFAULT '',
-  `town_district` varchar(150) NOT NULL DEFAULT '',
-  `address_exten` varchar(250) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`login`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-ALTER TABLE `payments` MODIFY `note` varchar(200) NULL DEFAULT NULL;
-ALTER TABLE `paymentscorr` MODIFY `note` varchar(200) NULL DEFAULT NULL;
