c2774b09c11d8cac9a065b2552db7d91dafe70c3
Nightfly <nightfly@email.ua>
2020-04-14 01:16:24 +0300

    better display of user dhcp log report

--- modules/general/pl_dhcp/index.php
+++ modules/general/pl_dhcp/index.php
@@ -1,42 +1,45 @@
 <?php
+
 if (cfr('PLDHCP')) {
-    
+
 
     if (isset($_GET['username'])) {
-        $login=$_GET['username'];
-        $config=$ubillingConfig->getBilling();
-        $alter_conf=$ubillingConfig->getAlter();
-        $cat_path=$config['CAT'];
-        $grep_path=$config['GREP'];
-        $tail_path=$config['TAIL'];
-        $sudo_path=$config['SUDO'];
-        $leasefile=$alter_conf['NMLEASES'];
-        $userdata=zb_UserGetStargazerData($login);
-        $user_ip=$userdata['IP'];
-        $user_mac=zb_MultinetGetMAC($user_ip);
-        show_window(__('Current MAC').' '.$user_mac,'');
-        $command=$sudo_path.' '.$cat_path.' '.$leasefile.' | '.$grep_path.' '.$user_mac.' | '.$tail_path.'  -n 30';
-        $output=shell_exec($command);
-        if (!empty ($output)) {
-            $result='';
-            $rowdata='';
-            $allrows=explodeRows($output);
+        $login = $_GET['username'];
+        $config = $ubillingConfig->getBilling();
+        $alter_conf = $ubillingConfig->getAlter();
+        $cat_path = $config['CAT'];
+        $grep_path = $config['GREP'];
+        $tail_path = $config['TAIL'];
+        $sudo_path = $config['SUDO'];
+        $leasefile = $alter_conf['NMLEASES'];
+        $userdata = zb_UserGetStargazerData($login);
+        $user_ip = $userdata['IP'];
+        $user_mac = zb_MultinetGetMAC($user_ip);
+        $messages = new UbillingMessageHelper();
+        $currentMacLabel = $messages->getStyledMessage(wf_tag('h2') . __('Current MAC') . ': ' . $user_mac . wf_tag('h2', true), 'info');
+        show_window('', $currentMacLabel);
+        $command = $sudo_path . ' ' . $cat_path . ' ' . $leasefile . ' | ' . $grep_path . ' ' . $user_mac . ' | ' . $tail_path . '  -n 30';
+        $output = shell_exec($command);
+        if (!empty($output)) {
+            $result = '';
+            $rowdata = '';
+            $allrows = explodeRows($output);
             foreach ($allrows as $eachrow) {
                 if (!empty($eachrow)) {
-                $celldata=wf_TableCell($eachrow);
-                $rowdata.=wf_TableRow($celldata, 'row3');
+                    $celldata = wf_TableCell($eachrow);
+                    $rowdata .= wf_TableRow($celldata, 'row3');
                 }
             }
-            $result=wf_TableBody($rowdata, '100%', 0);
-            show_window(__('User DHCP log'),$result);
+            $result = wf_TableBody($rowdata, '100%', 0);
+            show_window(__('User DHCP log'), $result);
+        } else {
+            show_warning(__('User DHCP log') . ': ' . __('Nothing found'));
         }
-        
-        
-        show_window('',  web_UserControls($login));
+
+
+        show_window('', web_UserControls($login));
     }
-
 } else {
-      show_error(__('You cant control this module'));
+    show_error(__('You cant control this module'));
 }
-
 ?>
