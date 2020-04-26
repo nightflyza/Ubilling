f4b493c1c33d03dd2f6e089c7f6ce71ecab6387f
Nightfly <nightfly@email.ua>
2020-04-26 12:09:41 +0300

    large photos scale fix for raw preview in taskman etc

--- api/libs/api.photostorage.php
+++ api/libs/api.photostorage.php
@@ -245,7 +245,7 @@ class PhotoStorage {
             foreach ($this->allimages as $io => $eachimage) {
                 if (($eachimage['scope'] == $this->scope) AND ( $eachimage['item'] == $this->itemId)) {
                     $imgPreview = wf_img_sized(self::STORAGE_PATH . $eachimage['filename'], __('Show'), $this->photoCfg['IMGLIST_PREV_W'], $this->photoCfg['IMGLIST_PREV_H']);
-                    $imgFull = wf_img_sized(self::STORAGE_PATH . $eachimage['filename'],'','100%');
+                    $imgFull = wf_img_sized(self::STORAGE_PATH . $eachimage['filename'], '', '100%');
                     $imgFull .= wf_tag('br');
                     $imgFull .= __('Date') . ': ' . $eachimage['date'] . ' / ';
                     $imgFull .= __('Admin') . ': ' . $eachimage['admin'];
@@ -281,7 +281,7 @@ class PhotoStorage {
             foreach ($this->allimages as $io => $eachimage) {
                 if (($eachimage['scope'] == $this->scope) AND ( $eachimage['item'] == $this->itemId)) {
                     $imgPreview = wf_img_sized(self::STORAGE_PATH . $eachimage['filename'], __('Show'), $this->photoCfg['IMGLIST_PREV_W'], $this->photoCfg['IMGLIST_PREV_H']);
-                    $imgFull = wf_img(self::STORAGE_PATH . $eachimage['filename']);
+                    $imgFull = wf_img_sized(self::STORAGE_PATH . $eachimage['filename'], '', '100%');
 
                     $dimensions = 'width:' . ($this->photoCfg['IMGLIST_PREV_W'] + 10) . 'px;';
                     $dimensions .= 'height:' . ($this->photoCfg['IMGLIST_PREV_H'] + 10) . 'px;';
