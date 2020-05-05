a1d99c76a805bb580419c40c164c76b23448c2e6
Nightfly <nightfly@email.ua>
2020-05-05 16:41:34 +0300

    streets report is back to life

--- modules/general/report_streets/index.php
+++ modules/general/report_streets/index.php
@@ -7,13 +7,65 @@ if ($system->checkForRight('STREETEPORT')) {
      */
     class ReportStreets {
 
+        /**
+         * Contains cities as id=>name
+         * 
+         * @var array
+         */
         protected $cities = array();
+
+        /**
+         * Contains streets as id=>streetdata 
+         *
+         * @var array
+         */
         protected $streets = array();
+
+        /**
+         * Contains builds as id=>builddata
+         *
+         * @var array
+         */
         protected $builds = array();
+
+        /**
+         * contains apt related shit
+         *
+         * @var array
+         */
         protected $apts = array();
+
+        /**
+         * Payments abstraction layer placeholder
+         *
+         * @var object
+         */
+        protected $payments = '';
+
+        /**
+         * Total user counter
+         *
+         * @var int
+         */
         protected $totalusercount = 0;
 
+        /**
+         * Contains payments search year
+         *
+         * @var int
+         */
+        protected $year = '';
+
+        /**
+         * Contains payments search month with lezding zero
+         *
+         * @var string
+         */
+        protected $month = '';
+
         public function __construct() {
+            $this->setDates();
+            $this->initPayments();
             $this->loadCities();
             $this->loadStreets();
             $this->loadBuilds();
@@ -21,6 +73,35 @@ if ($system->checkForRight('STREETEPORT')) {
 
             $this->countApts();
             $this->countBuilds();
+        }
+
+        /**
+         * Internal dates setter
+         * 
+         * @return void
+         */
+        protected function setDates() {
+            if (ubRouting::checkPost(array('showyear', 'showmonth'))) {
+                $this->year = ubRouting::post('showyear', 'int');
+                $this->month = ubRouting::post('showmonth', 'int');
+            } else {
+                $this->year = curyear();
+                $this->month = date("m");
+            }
+        }
+
+        /**
+         * Inits payments abstraction layer
+         * 
+         * @return void
+         */
+        protected function initPayments() {
+            $this->payments = new NyanORM('payments');
+            if (!empty($this->year) AND ! empty($this->month)) {
+                $dateFilter = $this->year . '-' . $this->month . '-%';
+                $this->payments->where('date', 'LIKE', $dateFilter);
+                $this->payments->where('summ', '>', 0);
+            }
         }
 
         /**
@@ -157,38 +238,81 @@ if ($system->checkForRight('STREETEPORT')) {
          * @return string
          */
         public function render() {
+            $addrPayments = array(); //city + street => payments total
+            $allPayments = $this->payments->getAll();
+            $totalPaymentsSumm = 0;
+            if (!empty($allPayments)) {
+                $allUsers = zb_UserGetAllDataCache();
+                foreach ($allPayments as $io => $each) {
+                    if (isset($allUsers[$each['login']])) {
+                        $userData = $allUsers[$each['login']];
+                        $userCity = $userData['cityname'];
+                        $userStreet = $userData['streetname'];
+                        $userAddr = $userCity . ' ' . $userStreet;
+                        if (isset($addrPayments[$userAddr])) {
+                            $addrPayments[$userAddr] += $each['summ'];
+                        } else {
+                            $addrPayments[$userAddr] = $each['summ'];
+                        }
+                    }
+                }
+            }
+
             if (!empty($this->streets)) {
 
                 $cells = wf_TableCell(__('ID'));
-                $cells.= wf_TableCell(__('City'));
-                $cells.= wf_TableCell(__('Street'));
-                $cells.= wf_TableCell(__('Builds'));
-                $cells.= wf_TableCell(__('Registered'));
-                $cells.= wf_TableCell(__('Visual'));
-                $cells.= wf_TableCell(__('Level'));
+                $cells .= wf_TableCell(__('City'));
+                $cells .= wf_TableCell(__('Street'));
+                $cells .= wf_TableCell(__('Builds'));
+                $cells .= wf_TableCell(__('Users'));
+                $cells .= wf_TableCell(__('Visual'));
+                $cells .= wf_TableCell(__('Level'));
+                $cells .= wf_TableCell(__('Money'));
                 $rows = wf_TableRow($cells, 'row1');
 
                 foreach ($this->streets as $streetid => $each) {
                     $cells = wf_TableCell($streetid);
-                    $cells.= wf_TableCell(@$this->cities[$each['cityid']]);
-                    $cells.= wf_TableCell($each['streetname']);
-                    $cells.= wf_TableCell($each['buildcount']);
-                    $cells.= wf_TableCell($each['usercount']);
-                    $cells.=wf_TableCell(web_bar($each['usercount'], $this->totalusercount), '50%', '', 'sorttable_customkey="' . $each['usercount'] . '"');
-                    $cells.= wf_TableCell($this->getLevel($each['usercount'], $each['buildcount']));
-                    $rows.= wf_TableRow($cells, 'row3');
+                    $cells .= wf_TableCell(@$this->cities[$each['cityid']]);
+                    $cells .= wf_TableCell($each['streetname']);
+                    $cells .= wf_TableCell($each['buildcount']);
+                    $cells .= wf_TableCell($each['usercount']);
+                    $cells .= wf_TableCell(web_bar($each['usercount'], $this->totalusercount), '50%', '', 'sorttable_customkey="' . $each['usercount'] . '"');
+                    $cells .= wf_TableCell($this->getLevel($each['usercount'], $each['buildcount']));
+                    $addrString = @$this->cities[$each['cityid']] . ' ' . $each['streetname'];
+                    $paymentsSumm = (isset($addrPayments[$addrString])) ? $addrPayments[$addrString] : '0';
+                    $totalPaymentsSumm += $paymentsSumm;
+                    $cells .= wf_TableCell($paymentsSumm);
+                    $rows .= wf_TableRow($cells, 'row5');
                 }
 
                 $result = wf_TableBody($rows, '100%', '0', 'sortable');
+                $result .= __('Users') . ': ' . $this->totalusercount;
+                $result .= wf_tag('br');
+                $result .= __('Payments') . ': ' . $totalPaymentsSumm;
             } else {
                 $result = __('Nothing found');
             }
             return ($result);
         }
 
+        /**
+         * Renders payments date selection form
+         * 
+         * @return string
+         */
+        public function renderDateForm() {
+            $result = '';
+            $inputs = wf_YearSelectorPreset('showyear', __('Year'), false, $this->year) . ' ';
+            $inputs .= wf_MonthSelector('showmonth', __('Month'), $this->month, false) . ' ';
+            $inputs .= wf_Submit(__('Search') . ' ' . __('payments'));
+            $result .= wf_Form('', 'POST', $inputs, 'glamour');
+            return($result);
+        }
+
     }
 
     $streetReport = new ReportStreets();
+    show_window(__('Payments'), $streetReport->renderDateForm());
     show_window(__('Streets report'), $streetReport->render());
 } else {
     show_error(__('Access denied'));
