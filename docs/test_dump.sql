e062687b74957593a41526ee1ca69076a084e1a1
Nightfly <nightfly@email.ua>
2020-06-09 14:29:33 +0300

    pon boxes accounting dev begin

--- docs/test_dump.sql
+++ docs/test_dump.sql
@@ -2777,3 +2777,19 @@ CREATE TABLE IF NOT EXISTS `ponifdesc` (
   PRIMARY KEY (`id`),
   KEY (`oltid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `ponboxes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `geo` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `ponboxeslinks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `boxid` int(11) NOT NULL,
+  `login` varchar(64) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `onuid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
