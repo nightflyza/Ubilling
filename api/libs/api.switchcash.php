a1f29f2b2ca446132134fd1a857d7abbbb579c3a
Nightfly <nightfly@email.ua>
2020-08-14 13:52:16 +0300

    swcash creation/saving done

--- api/libs/api.switchcash.php
+++ api/libs/api.switchcash.php
@@ -32,6 +32,7 @@ class SwitchCash {
     const ROUTE_EDIT = 'switchid';
     const PROUTE_CREATE = 'createswitchid';
     const PROUTE_SAVE = 'saveswitchid';
+    const PROUTE_RECORD = 'swcashrecordid';
     const PROUTE_PLACECONTRACT = 'newplacecontract';
     const PROUTE_PLACEPRICE = 'newplaceprice';
     const PROUTE_POWERCONTRACT = 'newpowercontract';
@@ -42,6 +43,7 @@ class SwitchCash {
     const PROUTE_SWITCHDATE = 'newswitchdate';
 
     public function __construct() {
+        $this->initMessages();
         $this->initDatabase();
         $this->loadAllCashData();
     }
@@ -53,6 +55,15 @@ class SwitchCash {
      */
     protected function initDatabase() {
         $this->swCashDb = new NyanORM(self::TABLE_FINANCE);
+    }
+
+    /**
+     * Inits system message helper object
+     * 
+     * @return void
+     */
+    protected function initMessages() {
+        $this->messages = new UbillingMessageHelper();
     }
 
     /**
@@ -115,7 +126,47 @@ class SwitchCash {
     }
 
     /**
-     * Renders switch financial data editing form
+     * Saves database record on request
+     * 
+     * @return void/string on error
+     */
+    public function catchSave() {
+        $result = '';
+        if (ubRouting::checkPost(self::PROUTE_SAVE) AND ubRouting::checkPost(self::PROUTE_RECORD)) {
+            $switchId = ubRouting::post(self::PROUTE_CREATE, 'int');
+            $recordId = ubRouting::post(self::PROUTE_RECORD, 'int');
+
+            $placecontract = ubRouting::post(self::PROUTE_PLACECONTRACT, 'mres');
+            $placeprice = ubRouting::post(self::PROUTE_PLACEPRICE, 'mres');
+            $powercontract = ubRouting::post(self::PROUTE_POWERCONTRACT, 'mres');
+            $powerprice = ubRouting::post(self::PROUTE_POWERPRICE, 'mres');
+            $transportcontract = ubRouting::post(self::PROUTE_TRANSPORTCONTRACT, 'mres');
+            $transportprice = ubRouting::post(self::PROUTE_TRANSPORTPRICE, 'mres');
+            $switchprice = ubRouting::post(self::PROUTE_SWITCHPRICE, 'mres');
+            $switchdate = ubRouting::post(self::PROUTE_SWITCHDATE, 'mres');
+
+            if (zb_checkDate($switchdate)) {
+                $this->swCashDb->where('id', '=', $recordId);
+                $this->swCashDb->data('placecontract', $placecontract);
+                $this->swCashDb->data('placeprice', $placeprice);
+                $this->swCashDb->data('powercontract', $powercontract);
+                $this->swCashDb->data('powerprice', $powerprice);
+                $this->swCashDb->data('transportcontract', $transportcontract);
+                $this->swCashDb->data('transportprice', $transportprice);
+                $this->swCashDb->data('switchprice', $switchprice);
+                $this->swCashDb->data('switchdate', $switchdate);
+                $this->swCashDb->save();
+                log_register('SWCASH EDIT SWID [' . $switchId . ']');
+            } else {
+                $result .= __('Wrong date format');
+            }
+        }
+
+        return($result);
+    }
+
+    /**
+     * Renders switch financial data creation form
      * 
      * @param int $switchId
      * 
@@ -123,38 +174,7 @@ class SwitchCash {
      */
     public function renderCreateForm($switchId) {
         $result = '';
-        if (isset($this->allCashData[$switchId])) {
-            $switchId = ubRouting::filters($switchId, 'int');
-            //creation flag
-            $inputs = wf_HiddenInput(self::PROUTE_SAVE, $switchId);
-            //placement data
-            $inputs .= wf_TextInput(self::PROUTE_PLACECONTRACT, __('Placement contract'), '', true, 20);
-            $inputs .= wf_TextInput(self::PROUTE_PLACEPRICE, __('Placement price') . ' / ' . __('month'), '0', true, 5, 'finance');
-            //power data
-            $inputs .= wf_TextInput(self::PROUTE_POWERCONTRACT, __('Power contract'), '', true, 20);
-            $inputs .= wf_TextInput(self::PROUTE_POWERPRICE, __('Power price') . ' / ' . __('month'), '0', true, 5, 'finance');
-            //transport data
-            $inputs .= wf_TextInput(self::PROUTE_TRANSPORTCONTRACT, __('Transport contract'), '', true, 20);
-            $inputs .= wf_TextInput(self::PROUTE_TRANSPORTPRICE, __('Transport price') . ' / ' . __('month'), '0', true, 5, 'finance');
-            //switch pricing and installation date
-            $inputs .= wf_TextInput(self::PROUTE_SWITCHDATE, __('Switch price'), '0', true, 5, 'finance');
-            $inputs .= wf_DatePickerPreset(self::PROUTE_SWITCHDATE, curdate(), true) . ' ' . __('Switch installation date');
-            $inputs .= wf_delimiter();
-            $inputs .= wf_Submit(__('Create'));
-            $result .= wf_Form('', 'POST', $inputs, 'glamour');
-        }
-        return($result);
-    }
 
-    /**
-     * Renders switch financial data editing form
-     * 
-     * @param int $switchId
-     * 
-     * @return string
-     */
-    public function renderEditForm($switchId) {
-        $result = '';
         $switchId = ubRouting::filters($switchId, 'int');
         //creation flag
         $inputs = wf_HiddenInput(self::PROUTE_CREATE, $switchId);
@@ -168,11 +188,49 @@ class SwitchCash {
         $inputs .= wf_TextInput(self::PROUTE_TRANSPORTCONTRACT, __('Transport contract'), '', true, 20);
         $inputs .= wf_TextInput(self::PROUTE_TRANSPORTPRICE, __('Transport price') . ' / ' . __('month'), '0', true, 5, 'finance');
         //switch pricing and installation date
-        $inputs .= wf_TextInput(self::PROUTE_SWITCHDATE, __('Switch price'), '0', true, 5, 'finance');
+        $inputs .= wf_TextInput(self::PROUTE_SWITCHPRICE, __('Switch price'), '0', true, 5, 'finance');
         $inputs .= wf_DatePickerPreset(self::PROUTE_SWITCHDATE, curdate(), true) . ' ' . __('Switch installation date');
         $inputs .= wf_delimiter();
         $inputs .= wf_Submit(__('Create'));
         $result .= wf_Form('', 'POST', $inputs, 'glamour');
+
+        return($result);
+    }
+
+    /**
+     * Renders switch financial data editing form
+     * 
+     * @param int $switchId
+     * 
+     * @return string
+     */
+    public function renderEditForm($switchId) {
+        $result = '';
+        //TODO: file attaches
+        $switchId = ubRouting::filters($switchId, 'int');
+        if (isset($this->allCashData[$switchId])) {
+            $switchData = $this->allCashData[$switchId];
+            //save flag and record id
+            $inputs = wf_HiddenInput(self::PROUTE_SAVE, $switchId);
+            $inputs .= wf_HiddenInput(self::PROUTE_RECORD, $switchData['id']);
+            //placement data
+            $inputs .= wf_TextInput(self::PROUTE_PLACECONTRACT, __('Placement contract'), $switchData['placecontract'], true, 20);
+            $inputs .= wf_TextInput(self::PROUTE_PLACEPRICE, __('Placement price') . ' / ' . __('month'), $switchData['placeprice'], true, 5, 'finance');
+            //power data
+            $inputs .= wf_TextInput(self::PROUTE_POWERCONTRACT, __('Power contract'), $switchData['powercontract'], true, 20);
+            $inputs .= wf_TextInput(self::PROUTE_POWERPRICE, __('Power price') . ' / ' . __('month'), $switchData['powerprice'], true, 5, 'finance');
+            //transport data
+            $inputs .= wf_TextInput(self::PROUTE_TRANSPORTCONTRACT, __('Transport contract'), $switchData['transportcontract'], true, 20);
+            $inputs .= wf_TextInput(self::PROUTE_TRANSPORTPRICE, __('Transport price') . ' / ' . __('month'), $switchData['transportprice'], true, 5, 'finance');
+            //switch pricing and installation date
+            $inputs .= wf_TextInput(self::PROUTE_SWITCHPRICE, __('Switch price'), $switchData['switchprice'], true, 5, 'finance');
+            $inputs .= wf_DatePickerPreset(self::PROUTE_SWITCHDATE, $switchData['switchdate'], true) . ' ' . __('Switch installation date');
+            $inputs .= wf_delimiter();
+            $inputs .= wf_Submit(__('Save'));
+            $result .= wf_Form('', 'POST', $inputs, 'glamour');
+        } else {
+            $result .= $this->messages->getStyledMessage(__('Something went wrong') . ': EX_NO_SWCASHDATA', 'error');
+        }
         return($result);
     }
 
--- modules/general/swcash/index.php
+++ modules/general/swcash/index.php
@@ -15,16 +15,28 @@ if (@$altCfg['SW_CASH_ENABLED']) {
             }
         }
 
+        //editing existing financial data for some switch
+        if (ubRouting::checkPost($swCash::PROUTE_SAVE)) {
+            $saveResult = $swCash->catchSave();
+
+            if (empty($saveResult)) {
+                ubRouting::nav($swCash::URL_ME . '&' . $swCash::ROUTE_EDIT . '=' . ubRouting::post($swCash::PROUTE_SAVE));
+            } else {
+                show_error($saveResult);
+            }
+        }
+
         //rendering create/edit forms
         if (ubRouting::checkGet($swCash::ROUTE_EDIT)) {
             $switchId = ubRouting::get($swCash::ROUTE_EDIT, 'int');
             if (!$swCash->haveFinancialData($switchId)) {
                 //creation form
                 show_window(__('Create') . ' ' . __('Financial data'), $swCash->renderCreateForm($switchId));
-                show_window('', wf_BackLink($swCash::URL_SWITCHPROFILE.$switchId));
+                show_window('', wf_BackLink($swCash::URL_SWITCHPROFILE . $switchId));
             } else {
                 //editing form
-                //TODO
+                show_window(__('Edit') . ' ' . __('Financial data'), $swCash->renderEditForm($switchId));
+                show_window('', wf_BackLink($swCash::URL_SWITCHPROFILE . $switchId));
             }
         }
     }
