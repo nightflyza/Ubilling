1799b921d92e928c7f1187758fec64ba06236fc7
Nightfly <nightfly@email.ua>
2020-08-24 12:10:10 +0300

    fixed 7.4 compat

--- api/vendor/PHPExcel/Classes/PHPExcel/Cell/DefaultValueBinder.php
+++ api/vendor/PHPExcel/Classes/PHPExcel/Cell/DefaultValueBinder.php
@@ -79,7 +79,7 @@ class PHPExcel_Cell_DefaultValueBinder implements PHPExcel_Cell_IValueBinder
             return PHPExcel_Cell_DataType::TYPE_STRING;
         } elseif ($pValue instanceof PHPExcel_RichText) {
             return PHPExcel_Cell_DataType::TYPE_INLINE;
-        } elseif ($pValue{0} === '=' && strlen($pValue) > 1) {
+        } elseif (@$pValue{0} === '=' && strlen($pValue) > 1) {
             return PHPExcel_Cell_DataType::TYPE_FORMULA;
         } elseif (is_bool($pValue)) {
             return PHPExcel_Cell_DataType::TYPE_BOOL;
