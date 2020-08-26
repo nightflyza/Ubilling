66e48a80d9dd971e0d47ad161897603e50766d1b
discapacidad5 <discapacidad5@gmail.com>
2020-08-26 16:19:52 -0400

    Spanish language update

--- languages/spanish/onumaster.php
+++ languages/spanish/onumaster.php
@@ -36,5 +36,24 @@ $lang['def']['No interface value found.'] = 'No se ha encontrado ningún valor d
 $lang['def']['No OLT IP address value found.'] = 'No se encontró el valor de la dirección IP OLT.';
 $lang['def']['No VLAN value found.'] = 'No se encontró valor de VLAN.';
 $lang['def']['Registered ONU count is'] = 'El recuento de ONU registrado es';
+$lang['def']['Ask for new license. ETTO cards not supported for 64+ ONT installation in this license version.'] = 'Solicite nueva licencia. Las tarjetas ETTO no son compatibles con la instalación de 64+ ONT en esta versión de licencia.';
+$lang['def']['Function is not supported by this OLT'] = 'La función no es compatible con este OLT';
+$lang['def']['Deregister onu'] = 'Dar de baja ONU';
+$lang['def']['Delete onu'] = 'Eliminar ONU';
+$lang['def']['SNMP template is not found or not exists'] = 'La plantilla SNMP no se encuentra o no existe';
+$lang['def']['ONU data is empty'] = 'Faltan datos en ONU';
+$lang['def']['Wrong MAC format (should be XX:XX:XX:XX:XX:XX)'] = 'Formato de dirección MAC incorrecto (debe ser XX:XX:XX:XX:XX:XX)';
+$lang['def']['Essential SNMP options are missing in template'] = 'La plantilla carece de las opciones SNMP necesarias';
+$lang['def']['Operation unsuccessful'] = 'Operación fallida';
+$lang['def']['ONU not found'] = 'ONU no encontrada';
+$lang['def']['IMPORTANT NOTE: before ONU deletion it must be set offline, like with reboot action'] = 'NOTA IMPORTANTE: antes de quitar la ONU, colóquela en modo fuera de línea, por ejemplo, reiniciándola';
+$lang['def']['ONU operations for login'] = 'Operaciones ONU para inicio de sesión';
+$lang['def']['User has no ONU assigned'] = 'El usuario no tiene ONU asociadas';
+$lang['def']['ONU description same as login'] = 'Descripción ONU igual que inicio de sesión';
+$lang['def']['ONU description'] = 'Descripción de ONU';
+$lang['def']['Seems no options for describe, reboot, deregister or delete actions are enabled. Check ONUAUTO_CONFIG_* options statuses in alter.ini.'] = 'Parece que ninguna de las opciones para las acciones de ONU disponibles está habilitada. Verifique el estado de las opciones ONUAUTO_CONFIG_ * en alter.ini.';
+$lang['def']['Enable DHCP snooping'] = 'Habilitar DHCP snooping';
+$lang['def']['Enable loopdetect'] = 'Activar detección de bucle (detección de bucle)';
+$lang['def'][''] = '';
 
 
