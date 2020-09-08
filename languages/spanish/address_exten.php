9f32c428bb1ff75d874831e118446fcfd3c25c89
discapacidad5 <discapacidad5@gmail.com>
2020-09-08 08:16:28 -0400

    he Spanish translation of the migration module and extended address
    he Spanish translation of the migration module and extended address

---
+++ languages/spanish/address_exten.php
@@ -0,0 +1,6 @@
+<?
+$lang['def']['Postal code'] = 'Código postal';
+$lang['def']['Town/District/Region'] = 'Ciudad/Distrito/Región';
+$lang['def']['Extended address'] = 'Dirección ampliada';
+$lang['def']['Extended address info'] = 'Información de dirección ampliada';
+?>
