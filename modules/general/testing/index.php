f007372b278f4c8afeec6f03d306ab5ef08533e1
Nightfly <nightfly@email.ua>
2020-11-06 14:46:36 +0200

    cleanup

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -6,38 +6,24 @@ error_reporting(E_ALL);
 if (cfr('ROOT')) {
 
 
-//    $inputData = array(
-//        28.4 => 1,
-//        2840 => 100,
-//        28400 => 1000,
-//        284000 => 10056.72,
-//    );
 
     $inputData = array(
-        9230 => 208.47,
-        9204 => 211.59,
-        9218 => 206.75,
-        8827 => 213.45,
-        9050 => 213.86,
-        9148 => 216.97,
-        9246 => 218.49,
-        9355 => 216.24,
-        9576 => 215.14,
+        32 => 64,
+        3 => 6,
+        4 => 8,
+        5 => 10,
     );
 
 
     $mrnn = new MRNN();
-    //$mrnn->setDebug(true);
+    $mrnn->setDebug(0);
     $accel = true;
     $mrnn->learnDataSet($inputData, $accel);
     $trainStats = $mrnn->getTrainStats();
     
-
-    
-
-    //most low effective UAH to USD converter ever!!!! OMG!
-    $inputValue = 9763;
-    show_info($inputValue . ' users do  ' . $mrnn->processInputData($inputValue) . ' ARPU in october');
+ 
+    $inputValue = 365535;
+    show_info($inputValue . ' *2 = ' . $mrnn->processInputData($inputValue) . ' <- Oo ');
 
     deb($mrnn->visualizeTrain($trainStats));
 }
