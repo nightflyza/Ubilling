d88126947124445123f831f34b36d00e3380bd65
Nightfly <nightfly@email.ua>
2020-11-16 17:47:00 +0200

    cleanup

--- modules/general/testing/index.php
+++ modules/general/testing/index.php
@@ -4,10 +4,5 @@
 error_reporting(E_ALL);
 
 if (cfr('ROOT')) {
-    $test = new MapOn();
-    
-    $curday = curdate();
-    $routes = $test->getRoutes($curday . 'T00:00:00Z', $curday . 'T23:59:59Z');
-    
-    debarr($routes);
+ 
 }
