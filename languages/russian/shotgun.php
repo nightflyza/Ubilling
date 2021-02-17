6e68b639c684a0bc961561f720e5e25e04c5b1b2
Nightfly <nightfly@email.ua>
2021-02-17 20:55:03 +0200

    typo fix

--- languages/russian/shotgun.php
+++ languages/russian/shotgun.php
@@ -1,4 +1,4 @@
 <?php
 
 $lang['def']['Cobains shotgun'] = 'Ружье Кобейна';
-$lang['def']['right to use shotgun'] = 'Право Застрелиться из ружья Кобейна';
+$lang['def']['right to use shotgun'] = 'Право застрелиться из ружья Кобейна';
