956b1c761189cd7c00a097908a3a4a21426a6aa7
Nightfly <nightfly@email.ua>
2021-03-01 15:55:15 +0200

    update for pullreq #676 with config updates, missed option check, and some back to profile controls

--- modules/general/pl_dayx/index.php
+++ modules/general/pl_dayx/index.php
@@ -1,11 +1,17 @@
 <?php
+
 if (cfr('DAYX')) {
-    if (ubRouting::checkGet('username')) {
-        $username = ubRouting::get('username');
-        $fundsflow = new FundsFlow();
-        show_window('', $fundsflow->getOnlineLeftCount($username, false, $ubillingConfig->getAlterParam('FUNDSFLOW_CONSIDER_VSERVICES')));
+    if ($ubillingConfig->getAlterParam('DAYX_ENABLED')) {
+        if (ubRouting::checkGet('username')) {
+            $username = ubRouting::get('username');
+            $fundsflow = new FundsFlow();
+            show_window('', $fundsflow->getOnlineLeftCount($username, false, $ubillingConfig->getAlterParam('FUNDSFLOW_CONSIDER_VSERVICES')));
+            show_window('', web_UserControls($username));
+        } else {
+            show_error(__('Strange exeption') . ': EX_NO_USERNAME');
+        }
     } else {
-        show_error(__('Strange exeption') . ': EX_NO_USERNAME');
+        show_error(__('This module is disabled'));
     }
 } else {
     show_error(__('You cant control this module'));
