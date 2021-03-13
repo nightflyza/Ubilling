360a0daee0d1cce9f620479130b174f362a4f8ee
Nightfly <nightfly@email.ua>
2021-03-13 13:37:00 +0200

    send messages moved from black magic to previous tickets

--- modules/general/pl_sendmessage/index.php
+++ modules/general/pl_sendmessage/index.php
@@ -1,61 +1,81 @@
 <?php
+
 if (cfr('PLSENDMESSAGE')) {
-    
+
+    /**
+     * Renders message sending form
+     * 
+     * @return void
+     */
     function web_MessageSendForm() {
-        $form='
-            <form action="" method="POST">
-             <textarea name="messagetext" cols="60" rows="10"></textarea> <br>
-             <input type="submit" value="'.__('Send').'">
-            </form>
-            ';
+        $inputs = wf_TextArea('messagetext', '', '', true, '60x10');
+        $inputs .= wf_Submit(__('Send'));
+        $form = wf_Form('', 'POST', $inputs, 'glamour');
+
         show_window(__('Send message'), $form);
     }
-    
-    
+
+    /**
+     * Renders previous messages for some user
+     * 
+     * @param string $login
+     * 
+     * @return
+     */
     function web_MessagesShowPrevious($login) {
-        $login=mysql_real_escape_string($login);
-        $query="SELECT * from `ticketing` WHERE `to`='".$login."' AND `from`='NULL' AND `status`='1' ORDER BY `date` DESC";
-        $allmessages=simple_queryall($query);
-        $result='<table width="100%" class="sortable">';
-          $result.='
-                    <tr class="row1">
-                    <td>'.__('Date').'</td>
-                    <td>'.__('Text').'</td>
-                    </tr>
-                    ';
-        if (!empty ($allmessages)) {
-            foreach ($allmessages as $io=>$eachmessage) {
-                $result.='
-                    <tr class="row3">
-                    <td>'.$eachmessage['date'].'</td>
-                    <td>'.$eachmessage['text'].'</td>
-                    </tr>
-                    ';
+        $result = '';
+        $login = ubRouting::filters($login, 'mres');
+
+        $ticketingDb = new NyanORM('ticketing');
+        $ticketingDb->where('to', '=', $login);
+        $ticketingDb->whereRaw("`from`='NULL'");
+        $ticketingDb->where('status', '=', '1');
+        $ticketingDb->orderBy('date', 'DESC');
+        $allmessages = $ticketingDb->getAll();
+
+        if (!empty($allmessages)) {
+            $cells = wf_TableCell(__('Date'), '15%');
+            $cells .= wf_TableCell(__('Text'));
+            $rows = wf_TableRow($cells, 'row1');
+
+            foreach ($allmessages as $io => $each) {
+                $cells = wf_TableCell($each['date']);
+                $cells .= wf_TableCell($each['text']);
+                $rows .= wf_TableRow($cells, 'row3');
             }
+
+            $result .= wf_TableBody($rows, '100%', 0, 'sortable');
+        } else {
+            $messages = new UbillingMessageHelper();
+            $result .= $messages->getStyledMessage(__('Nothing to show'), 'info');
         }
-        $result.='</table>';
-        
-        show_window(__('Previous messages'),$result);
+
+        show_window(__('Previous messages'), $result);
     }
-  
-    if (isset($_GET['username'])) {
-        $login=$_GET['username'];
-        web_MessagesShowPrevious($login);
-        
-        if (isset ($_POST['messagetext'])) {
-            zb_TicketCreate('NULL', $login, $_POST['messagetext'],'NULL',whoami());
-            $newid=simple_get_lastid('ticketing');
+
+    if (ubRouting::checkGet('username')) {
+        $login = ubRouting::get('username');
+
+        //creating new message
+        if (ubRouting::checkPost('messagetext')) {
+            zb_TicketCreate('NULL', $login, ubRouting::post('messagetext'), 'NULL', whoami());
+            $newid = simple_get_lastid('ticketing');
             zb_TicketSetDone($newid);
-            rcms_redirect("?module=pl_sendmessage&username=".$login);
-            
+            ubRouting::nav('?module=pl_sendmessage&username=' . $login);
         }
+
+
+        show_window('', wf_BackLink('?module=pl_ticketing&username=' . $login));
+        //render previous messages
+        web_MessagesShowPrevious($login);
+
+        //and new message form
         web_MessageSendForm();
         show_window('', web_UserControls($login));
-        
+    } else {
+        show_error(__('User not exist'));
     }
-
 } else {
-      show_error(__('You cant control this module'));
+    show_error(__('You cant control this module'));
 }
-
 ?>
--- modules/general/pl_ticketing/index.php
+++ modules/general/pl_ticketing/index.php
@@ -40,23 +40,23 @@ if (cfr('TICKETING')) {
             $result = '';
             if (!empty($this->alltickets)) {
                 $cells = wf_TableCell(__('ID'));
-                $cells.= wf_TableCell(__('Date'));
-                $cells.= wf_TableCell(__('Text'));
-                $cells.= wf_TableCell(__('Processed'));
-                $cells.= wf_TableCell(__('Actions'));
+                $cells .= wf_TableCell(__('Date'));
+                $cells .= wf_TableCell(__('Text'));
+                $cells .= wf_TableCell(__('Processed'));
+                $cells .= wf_TableCell(__('Actions'));
                 $rows = wf_TableRow($cells, 'row1');
                 foreach ($this->alltickets as $io => $each) {
                     $cells = wf_TableCell($each['id']);
-                    $cells.= wf_TableCell($each['date']);
+                    $cells .= wf_TableCell($each['date']);
                     if (strlen($each['text']) > 140) {
                         $textPreview = mb_substr(strip_tags($each['text']), 0, 140, 'utf-8') . '...';
                     } else {
                         $textPreview = strip_tags($each['text']);
                     }
-                    $cells.= wf_TableCell($textPreview);
-                    $cells.= wf_TableCell(web_bool_led($each['status']));
-                    $cells.= wf_TableCell(wf_Link('?module=ticketing&showticket=' . $each['id'], wf_img_sized('skins/icon_search_small.gif', '', '12') . ' ' . __('Show'), false, 'ubButton'));
-                    $rows.= wf_TableRow($cells, 'row3');
+                    $cells .= wf_TableCell($textPreview);
+                    $cells .= wf_TableCell(web_bool_led($each['status']));
+                    $cells .= wf_TableCell(wf_Link('?module=ticketing&showticket=' . $each['id'], wf_img_sized('skins/icon_search_small.gif', '', '12') . ' ' . __('Show'), false, 'ubButton'));
+                    $rows .= wf_TableRow($cells, 'row3');
                 }
                 $result = wf_TableBody($rows, '100%', 0, 'sortable');
             } else {
@@ -74,15 +74,13 @@ if (cfr('TICKETING')) {
          */
 
         public function renderCalendar() {
-
             $calendarData = '';
-
             if (!empty($this->alltickets)) {
                 foreach ($this->alltickets as $io => $each) {
                     $timestamp = strtotime($each['date']);
                     $date = date("Y, n-1, j", $timestamp);
                     $rawTime = date("H:i:s", $timestamp);
-                    $calendarData.="
+                    $calendarData .= "
                       {
                         title: '" . $rawTime . "',
                         url: '?module=ticketing&showticket=" . $each['id'] . "',
@@ -105,7 +103,10 @@ if (cfr('TICKETING')) {
 
         //controls
         $actionLinks = wf_Link('?module=pl_ticketing&username=' . $login, wf_img('skins/icon_table.png') . ' ' . __('Grid view'), false, 'ubButton');
-        $actionLinks.= wf_Link('?module=pl_ticketing&username=' . $login . '&calendarview=true', wf_img('skins/icon_calendar.gif') . ' ' . __('As calendar'), false, 'ubButton');
+        $actionLinks .= wf_Link('?module=pl_ticketing&username=' . $login . '&calendarview=true', wf_img('skins/icon_calendar.gif') . ' ' . __('As calendar'), false, 'ubButton');
+        if (cfr('PLSENDMESSAGE')) {
+            $actionLinks .= wf_Link('?module=pl_sendmessage&username=' . $login, wf_img('skins/icon_chat_small.png') . ' ' . __('Send message'), false, 'ubButton');
+        }
         show_window('', $actionLinks);
 
         //display results
@@ -117,7 +118,7 @@ if (cfr('TICKETING')) {
 
         show_window('', web_UserControls($login));
     } else {
-        show_window(__('Error'), __('No username specified'));
+        show_error(__('User not exist'));
     }
 } else {
     show_error(__('You cant control this module'));
