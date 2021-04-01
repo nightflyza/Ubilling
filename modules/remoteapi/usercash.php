b50b8ee9eafb7e951aa4bbe85e613b8fcaf5e016
Nightfly <nightfly@email.ua>
2021-04-01 12:20:11 +0300

    belochka is here

--- modules/remoteapi/usercash.php
+++ modules/remoteapi/usercash.php
@@ -2,7 +2,20 @@
 
 if (ubRouting::get('action') == 'usercash') {
     if (ubRouting::checkGet(array('login', 'summ'))) {
-
+        /**
+         * 
+         *    )" .
+         *   /    \      (\-./
+         *   /     |    _/ o. \
+         *   |      | .-"      y)-
+         *   |      |/       _/ \
+         *   \     /j   _".\(@)
+         *   \   ( |    `.''  )
+         *   \  _`-     |   /
+         *   "  `-._  <_ (
+         *   `-.,),)
+         *  ^^ ETO BELOCHKA! ^^
+         */
         $userLogin = ubRouting::get('login');
         $summ = ubRouting::get('summ');
         $cashType = (ubRouting::get('ct', 'int')) ? (ubRouting::get('ct', 'int')) : 1;
