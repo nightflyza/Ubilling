bc10ffcc53a2c6b83f9124ac8994845b57f984c7
Nightfly <nightfly@email.ua>
2021-06-30 17:04:37 +0300

    missing option notice

--- api/libs/api.megogo.php
+++ api/libs/api.megogo.php
@@ -1151,7 +1151,13 @@ class MegogoInterface {
                 $controls .= wf_Link($baseUrl . '&maction=unsubscribe', web_bool_star(0) . ' ' . __('Unubscribe with Megogo API'), true, 'ubButton') . wf_tag('br');
                 $controls .= wf_Link($baseUrl . '&maction=activate', web_bool_led(1) . ' ' . __('Activate subscription'), true, 'ubButton') . wf_tag('br');
                 $controls .= wf_Link($baseUrl . '&maction=deactivate', web_bool_led(0) . ' ' . __('Deactivate subscription'), true, 'ubButton') . wf_tag('br');
+                if (@$this->altCfg['MG_DOMAIN']) {
                 $controls .= wf_Link($baseUrl . '&maction=newcredentials', wf_img('skins/icon_key.gif') . ' ' . __('User registration') . ' ' . __('on') . ' ' . __('Megogo.net'), true, 'ubButton') . wf_tag('br');
+                } else {
+                    $result.= $this->messages->getStyledMessage(__('Missed config option').' MG_DOMAIN', 'error');
+                    $result.= wf_delimiter();
+                } 
+                
                 $controls .= wf_JSAlertStyled($baseUrl . '&maction=delete', web_delete_icon() . ' ' . __('Delete subscription'), $this->messages->getDeleteAlert(), 'ubButton');
                 $result .= $controls;
             }
