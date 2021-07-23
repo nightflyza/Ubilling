3ec8e158f7990d95708e2aee63cf49ea87d120fe
Nightfly <nightfly@email.ua>
2021-07-23 13:21:29 +0300

    multigenpod RemoteAPI call implemented

--- modules/remoteapi/multigen.php
+++ modules/remoteapi/multigen.php
@@ -1,23 +1,47 @@
 <?php
 
 // multigen attributes regeneration
-if (($_GET['action'] == 'multigen') OR ( $_GET['action'] == 'multigentotal') OR ( $_GET['action'] == 'multigentraff')) {
+$remoteApiAction = ubRouting::get('action');
+if (($remoteApiAction == 'multigen') OR ( $remoteApiAction == 'multigentotal') OR ( $remoteApiAction == 'multigentraff') OR ( $remoteApiAction == 'multigenpod')) {
     if ($alterconf['MULTIGEN_ENABLED']) {
         $multigen = new MultiGen();
-        if ($_GET['action'] == 'multigen') {
+        if ($remoteApiAction == 'multigen') {
             $multigen->generateNasAttributes();
             die('OK: MULTIGEN');
         }
 
-        if ($_GET['action'] == 'multigentotal') {
+        if ($remoteApiAction == 'multigentotal') {
             $multigen->flushAllScenarios();
             $multigen->generateNasAttributes();
             die('OK: MULTIGEN_TOTAL');
         }
 
-        if ($_GET['action'] == 'multigentraff') {
+        if ($remoteApiAction == 'multigentraff') {
             $multigen->aggregateTraffic();
             die('OK: MULTIGEN_TRAFF');
+        }
+
+        if ($remoteApiAction == 'multigenpod') {
+            $login = ubRouting::get('param');
+            if (!empty($login)) {
+                $userData = zb_UserGetAllData($login);
+                $userData = $userData[$login];
+                $newUserData = $userData;
+                if ($alterconf['MULTIGEN_POD_ON_MAC_CHANGE']) {
+                    if ($alterconf['MULTIGEN_POD_ON_MAC_CHANGE'] == 2) {
+                        $multigen->podOnExternalEvent($login, $userData, $newUserData);
+                        $multigen->podOnExternalEvent($login, $newUserData);
+                    }
+                    if ($alterconf['MULTIGEN_POD_ON_MAC_CHANGE'] == 1) {
+                        $multigen->podOnExternalEvent($login, $newUserData);
+                    }
+                    die('OK: MULTIGENPOD');
+                } else {
+                    die('ERROR: MULTIGEN_POD_ON_MAC_CHANGE DISABLED');
+                }
+            } else {
+                die('ERROR: NO_LOGIN');
+            }
         }
     } else {
         die('ERROR: MULTIGEN DISABLED');
