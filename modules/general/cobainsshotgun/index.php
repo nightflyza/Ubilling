9bb511efb814ac2e8a9f377acb5df2b6b4201fd1
vitaliy-t0 <vitaliy.tatomyr@vakoms.com>
2021-08-06 15:36:59 +0300

    Add ZenFlow to CobainsShotgun Module.

--- modules/general/cobainsshotgun/index.php
+++ modules/general/cobainsshotgun/index.php
@@ -4,8 +4,15 @@ if (cfr('SHOTGUN')) {
     $shotgunEnabled = $ubillingConfig->getAlterParam('COBAINS_SHOTGUN');
     if ($shotgunEnabled) {
         $shotgun = new CobainsShotgun();
-        show_window(__('Cobains shotgun'),$shotgun->renderReport());
-        
+
+        // Show primary module controls
+        show_window('', $shotgun->renderControls());
+        if (ubRouting::checkGet($shotgun::ROUTE_ZEN)) {
+            $shotgunZen = new ZenFlow('zncobainshotgun', $shotgun->renderReportZen());
+            show_window(__('Zen'), $shotgunZen->render());
+        } else {
+            show_window(__('Cobains shotgun'), $shotgun->renderReport());
+        }
     } else {
         show_error(__('This module is disabled'));
     }
