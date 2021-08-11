664cf1fb8e74a91c29bb01c070f3e54cb57ccb99
bobr-kun <demon.bobr@gmail.com>
2021-08-11 16:51:34 +0300

    api.userprofile: Added check for OLT to be alive before making actual SNMP request for real-time values
    OpenPayz:
        FC-Sistema fronted updated to support "strict" mode

--- openpayz/frontend/fcsistemamulti/index.php
+++ openpayz/frontend/fcsistemamulti/index.php
@@ -12,9 +12,21 @@ include ("../../libs/api.openpayz.php");
 define('FCS_MERCH_ID', 'fcsistema');
 
 // Если: FCS_USE_AGENTCODES = 1 и/или FCS_DEFAULT_AGENTCODE != '' - будет использован режим "с кодами контрагентов"
+// ВНИМАНИЕ - ещё раз: если параметр FCS_DEFAULT_AGENTCODE НЕ пуст - даже при условии отключенного параметра FCS_USE_AGENTCODES
+// - все равно будет использоваться режим "с контрагентами".
+// Нужен режим БЕЗ контрагентов? - FCS_USE_AGENTCODES = 0 и FCS_DEFAULT_AGENTCODE = ''
+// Нужен "строгий" режим БЕЗ контрагентов? - FCS_USE_AGENTCODES_STRICT = 2
+// - в этом случае мы можем использовать FCS_DEFAULT_AGENTCODE без риска "включить" режим с контрагентами
 
-//Использовать ли внешний кодификатор контрагентов из agentcodes.ini?
+// Использовать ли внешний кодификатор контрагентов из agentcodes.ini?
 define('FCS_USE_AGENTCODES', 0);
+
+// Использовать ли строгий поиск юзеров согласно привязки к контрагенту?
+// Протокол FC-Sistema имеет параметр "id_project", который передается в запросе "verify" - запрос на поиск юзера
+// В agentcodes_strict.ini указываем маппинг: ID контрагента(предпринимателя) = id_project
+// id_project передается в запросе от FC-Sistema и ДОЛЖЕН быть с ними ПРЕДВАРИТЕЛЬНО согласован для каждого контрагента
+// Специальное значние "2" позволяет использовать "строгий" режим, но БЕЗ учета контрагентов
+define('FCS_USE_AGENTCODES_STRICT', 0);
 
 // будет передано в <provider_id_s></provider_id_s> - уникальный идентификатор  в системе FcSistema
 // в случае если у Провайдера несколько контрагентов и есть необходимость перечисления денежных средств на разных получателей
@@ -33,6 +45,8 @@ define('FCS_NO_SUCH_USER', '-40');
 define('FCS_ERROR_PAYMENT', '80');
 define('FCS_TRANSACTION_NOT_FOUND', '-27');
 define('FCS_TRANSACTION_DUPLICATE', '-10');
+define('FCS_USER_BALANCE_DECIMALS', -1);    // Сколько знаков после запятой возвращать в балансе абонента 0 - возвращать только целую часть
+define('FCS_LOCALE', 'UA');     // на каком языке отображать данные абонента: RU или UA
 
 // mandatory GET parameters
 $requiredGETParams = array('cmd', 'merchantid');
@@ -146,44 +160,182 @@ function fcs_GetFCSAgentCode($userLogin) {
 }
 
 /**
+ * Try to get contragent agent code assigned to $userLogin
+ *
+ * @param $userLogin
+ *
+ * @return string
+ */
+function fcs_GetFCSAgentCodeStrict($userLogin) {
+    $userAgentData = fcs_GetAgentData($userLogin);
+    $agentData = json_decode($userAgentData, true);
+    $agentStrict = (empty($agentData['id']) ? FCS_DEFAULT_AGENTCODE : $agentData['id']);
+    $agentsCodesStrictList = parse_ini_file('agentcodes_strict.ini');
+    $agentCodeStrict = (empty($agentsCodesStrictList[$agentStrict]) ? 'none' : $agentsCodesStrictList[$agentStrict]);
+
+    return ($agentCodeStrict);
+}
+
+/**
+ * Returns all user RealNames
+ *
+ * @return array
+ */
+function fcs_UserGetAllRealnames() {
+    $query = "SELECT * from `realname`";
+    $all = simple_queryall($query);
+    $result = array();
+    if (!empty($all)) {
+        foreach ($all as $io => $each) {
+            $result[$each['login']] = $each['realname'];
+        }
+    }
+    return($result);
+}
+
+/**
+ * Returns user stargazer data by login
+ *
+ * @param string $login existing stargazer login
+ *
+ * @return array
+ */
+function fcs_UserGetStargazerData($login) {
+    $login = mysql_real_escape_string($login);
+    $query = "SELECT * from `users` WHERE `login`='" . $login . "';";
+    $result = simple_query($query);
+    return ($result);
+}
+
+/**
+ * Returns all user mobile phones
+ *
+ * @return array
+ */
+function fcs_UserGetAllMobiles() {
+    $query = "SELECT * from `phones`";
+    $all = simple_queryall($query);
+    $result = array();
+    if (!empty($all)) {
+        foreach ($all as $io => $each) {
+            $result[$each['login']] = $each['mobile'];
+        }
+    }
+    return($result);
+}
+
+/**
+ * Returns array of availble user address as login=>address
+ *
+ * @return array
+ */
+function fcs_AddressGetFulladdresslist() {
+//наглая заглушка
+    $alterconf['ZERO_TOLERANCE'] = 0;
+    $alterconf['CITY_DISPLAY'] = 0;
+    $result = array();
+    $query_full = "
+        SELECT `address`.`login`,`city`.`cityname`,`street`.`streetname`,`build`.`buildnum`,`apt`.`apt` FROM `address`
+        INNER JOIN `apt` ON `address`.`aptid`= `apt`.`id`
+        INNER JOIN `build` ON `apt`.`buildid`=`build`.`id`
+        INNER JOIN `street` ON `build`.`streetid`=`street`.`id`
+        INNER JOIN `city` ON `street`.`cityid`=`city`.`id`";
+    $full_adress = simple_queryall($query_full);
+    if (!empty($full_adress)) {
+        foreach ($full_adress as $ArrayData) {
+            // zero apt handle
+            if ($alterconf['ZERO_TOLERANCE']) {
+                $apartment_filtered = ($ArrayData['apt'] == 0) ? '' : '/' . $ArrayData['apt'];
+            } else {
+                $apartment_filtered = '/' . $ArrayData['apt'];
+            }
+            if ($alterconf['CITY_DISPLAY']) {
+                $result[$ArrayData['login']] = $ArrayData['cityname'] . ' ' . $ArrayData['streetname'] . ' ' . $ArrayData['buildnum'] . $apartment_filtered;
+            } else {
+                $result[$ArrayData['login']] = $ArrayData['streetname'] . ' ' . $ArrayData['buildnum'] . $apartment_filtered;
+            }
+        }
+    }
+    return($result);
+}
+
+/**
  * Reply to paysys on user payment ID check
  *
  * @param $customerID
  * @param bool $useAgentCodes
+ * @param string $strictID
  *
  * @return string
  */
-function fcs_ReplyCheck($customerID, $useAgentCodes = false) {
+function fcs_ReplyCheck($customerID, $useAgentCodes = false, $strictID = '') {
+    $UsrBalanceDecimals = FCS_USER_BALANCE_DECIMALS;
     $allcustomers = op_CustomersGetAll();
     $agentCode = '';
+    $strictCheckPassed = false;
+    $noSuchUserReply = '
+<?xml version="1.0" encoding="windows-1251"?>
+<response>
+<result>' . FCS_NO_SUCH_USER . '</result>
+<comment>Account does not exist.</comment>                        
+</response>
+';
 
     if (isset($allcustomers[$customerID])) {
         $userLogin = $allcustomers[$customerID];
 
-        if ($useAgentCodes) {
-            $agentCode = fcs_GetFCSAgentCode($userLogin);
+        if (FCS_USE_AGENTCODES_STRICT and !empty($strictID)) {
+            $curStrcitID = fcs_GetFCSAgentCodeStrict($userLogin);
+            $strictCheckPassed = ($strictID == $curStrcitID);
+        } else {
+            $strictCheckPassed = true;
         }
 
-        $companyCode = (empty($agentCode)) ? '' : '<provider_id_s>' . $agentCode . '</provider_id_s>';
+        if ($strictCheckPassed) {
+            if ($useAgentCodes) {
+                $agentCode = fcs_GetFCSAgentCode($userLogin);
+            }
 
-        $apiReply = '
+            $companyCode = (empty($agentCode)) ? '' : '<provider_id_s>' . $agentCode . '</provider_id_s>';
+
+            $allRealnames = fcs_UserGetAllRealnames();
+            $allAddress = fcs_AddressGetFulladdresslist();
+            $allMobiles = fcs_UserGetAllMobiles();
+            $userData = fcs_UserGetStargazerData($userLogin);
+            $userBalance = ($UsrBalanceDecimals < 0) ? $userData['Cash'] : ($UsrBalanceDecimals == 0) ? intval($userData['Cash'], 10) : round($userData['Cash'], $UsrBalanceDecimals, PHP_ROUND_HALF_EVEN);
+            $realName = (empty($allRealnames[$userLogin]) ? '' : $allRealnames[$userLogin]);
+            $cellPhone = (empty($allMobiles[$userLogin]) ? '' : $allMobiles[$userLogin]);
+            $address = (empty($allAddress[$userLogin]) ? '' : $allAddress[$userLogin]);
+
+            if (FCS_LOCALE == 'UA') {
+                $captRealname = 'П.І.Б.';
+                $captCellPhone = 'Мобільний телефон';
+                $captAddress = 'Адреса';
+            } else {
+                $captRealname = 'Ф.И.О.';
+                $captCellPhone = 'Мобильный телефон';
+                $captAddress = 'Адрес';
+            }
+
+            $captBalance = 'Баланс';
+            $userInfo = $captRealname . ': ' . $realName . '; ' . $captCellPhone . ': ' . $cellPhone . '; ' . $captAddress . ': ' . $address . '; ' . $captBalance . ': ' . $userBalance . ';';
+
+            $apiReply = '
 <?xml version="1.0" encoding="windows-1251"?>
 <response>
 <result>' . FCS_USER_EXISTS . '</result>
-<text></text>
+<text>
+' . $userInfo . '
+</text>
 <comment>Account exists.</comment>
 ' . $companyCode . '
 </response>
-                    ';
+';
+        } else {
+            $apiReply = $noSuchUserReply;
+        }
     } else {
-        $apiReply = '
-<?xml version="1.0" encoding="windows-1251"?>
-<response>
-<result>' . FCS_NO_SUCH_USER . '</result>
-<text></text>
-<comment>Account does not exist.</comment>                        
-</response>
-                    ';
+        $apiReply = $noSuchUserReply;
     }
 
     return ($apiReply);
@@ -275,15 +427,16 @@ function fcs_ReplyPaymentCheck($paymentID) {
 
 
 $defaultAgentCode = FCS_DEFAULT_AGENTCODE;
-$useAgentCodes = (FCS_USE_AGENTCODES or !empty($defaultAgentCode));
+$useAgentCodes = (FCS_USE_AGENTCODES_STRICT < 2 and (FCS_USE_AGENTCODES or !empty($defaultAgentCode)));
 
 if (fcs_CheckGet($requiredGETParams) and $_GET['merchantid'] == FCS_MERCH_ID) {
-    $fcsAPIAction = $_GET['cmd'];
+    $fcsAPIAction   = $_GET['cmd'];
+    $fcsIDProject  = (empty($_GET['id_project']) ? '' : $_GET['id_project']);
 
     switch ($fcsAPIAction) {
         case 'verify':
             if (isset($_GET['account'])) {
-                $chekReply = fcs_ReplyCheck($_GET['account'], $useAgentCodes);
+                $chekReply = fcs_ReplyCheck($_GET['account'], $useAgentCodes, $fcsIDProject);
                 die(mb_convert_encoding($chekReply, 'windows-1251'));
             } else {
                 die('FCS ERROR: WRONG API VERIFY REQUEST PARAMETERS');
