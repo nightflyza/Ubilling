477507da2f7ac695c5eb8b1e64b275bb02a4319c
Nightfly <nightfly@email.ua>
2021-12-30 11:29:41 +0200

    fixed undefined control notice

--- api/libs/api.mapscommon.php
+++ api/libs/api.mapscommon.php
@@ -705,8 +705,9 @@ function sm_MapInitBasic($center, $zoom, $type, $placemarks = '', $editor = '', 
 function sm_ShowMapContainer() {
     $container = wf_tag('div', false, '', 'id="ubmap" style="width: 1000; height:800px;"');
     $container .= wf_tag('div', true);
+    $controls = '';
     if (cfr('USERSMAP')) {
-        $controls = wf_Link("?module=usersmap", wf_img('skins/ymaps/build.png') . ' ' . __('Builds map'), false, 'ubButton');
+        $controls .= wf_Link("?module=usersmap", wf_img('skins/ymaps/build.png') . ' ' . __('Builds map'), false, 'ubButton');
     }
     $controls .= wf_Link("?module=switchmap", wf_img('skins/ymaps/network.png') . ' ' . __('Switches map'), false, 'ubButton');
     if (cfr('SWITCHESEDIT')) {
