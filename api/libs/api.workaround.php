f0d8097c2e293d67d46f976784880ca4fb4072b0
Nightfly <nightfly@email.ua>
2022-05-13 18:50:37 +0300

    more unused code removed

--- api/libs/api.workaround.php
+++ api/libs/api.workaround.php
@@ -6609,31 +6609,6 @@ function zb_CleanMobileNumber($number) {
     return ($number);
 }
 
-/**
- * Returns list of available free Juniper NASes
- * 
- * @return string
- */
-function web_JuniperListClients() {
-    $result = __('Nothing found');
-    $query = "SELECT * from `jun_clients`";
-    $all = simple_queryall($query);
-    if (!empty($all)) {
-        $cells = wf_TableCell(__('IP'));
-        $cells .= wf_TableCell(__('NAS name'));
-        $cells .= wf_TableCell(__('Radius secret'));
-        $rows = wf_TableRow($cells, 'row1');
-        foreach ($all as $io => $each) {
-            $cells = wf_TableCell($each['nasname']);
-            $cells .= wf_TableCell($each['shortname']);
-            $cells .= wf_TableCell($each['secret']);
-            $rows .= wf_TableRow($cells, 'row3');
-        }
-        $result = wf_TableBody($rows, '100%', '0', 'sortable');
-    }
-
-    return ($result);
-}
 
 /**
  * Returns list of available MultiGen NASes
