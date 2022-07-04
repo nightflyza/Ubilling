2df87cbdc733e534ef00bc3851f757a585b84d4c
pautiina <alex.chernyk@gmail.com>
2022-07-04 23:52:07 +0300

    add translate for new smszillamacros

---
+++ languages/ukrainian/smszilla.php
@@ -0,0 +1,3 @@
+<?php
+$lang['def']['The remaining number of days to use the service'] = 'Кількість днів використання сервісу, що залишилася.';
+$lang['def']['Service end date'] = 'Дата закінчення послуги';
