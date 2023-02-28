806a05a6265e13ae76b78fc48a36bc871200ce99
l1ght13aby <l1ght13aby@yahoo.com>
2023-02-28 15:17:32 +0100

    implementing mass registration

--- modules/general/zteunreg/index.php
+++ modules/general/zteunreg/index.php
@@ -22,8 +22,13 @@ if (@$altcfg[OnuRegister::MODULE_CONFIG]) {
             $avidity_b = $avidity['M']['LAI'];
             if (wf_CheckGet(array('oltlist'))) {
                 if (wf_CheckGet(array('oltid'))) {
-                    if(wf_CheckGet(array('massfix','preview'))) {
+                    if (wf_CheckGet(array('massfix', 'preview'))) {
+                        $actionLinks = wf_Link(OnuRegister::UNREG_MASS_FIX_RUN_OLT_URL . $_GET['oltid'], __('Run'), false, 'ubButton');
                         $register->listFixable();
+                    }
+                    if (wf_CheckGet(array('massfix', 'run'))) {
+                        $register->onuMassRegister();
+                        show_window(__('Result'), $register->result);
                     }
                     if (wf_CheckGet(array(OnuRegister::OLTIP_FIELD, OnuRegister::INTERFACE_FIELD, OnuRegister::TYPE_FIELD))) {
                         if ($altcfg['ONUREG_ALWAYS_SHOW_UNREGISTERED']) {
@@ -52,8 +57,12 @@ if (@$altcfg[OnuRegister::MODULE_CONFIG]) {
                 } else {
                     show_window(__('All ZTE OLTs'), $register->$avidity_b(false));
                 }
-            } elseif (wf_CheckGet(array('massfix','preview'))) {
+            } elseif (wf_CheckGet(array('massfix', 'preview'))) {
+                $actionLinks = wf_Link(OnuRegister::UNREG_MASS_FIX_RUN_URL, __('Run'), false, 'ubButton');
                 $register->listFixable();
+            } elseif (wf_CheckGet(array('massfix', 'run'))) {
+                $register->onuMassRegister();
+                show_window(__('Result'), $register->result);
             } else {
                 if (wf_CheckGet(array(OnuRegister::OLTIP_FIELD, OnuRegister::INTERFACE_FIELD, OnuRegister::TYPE_FIELD))) {
                     if ($altcfg['ONUREG_ALWAYS_SHOW_UNREGISTERED']) {
