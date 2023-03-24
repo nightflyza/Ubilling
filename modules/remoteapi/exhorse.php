241bb9f6a4719bf5660fbbe11621d7af68954ca3
Nightfly <nightfly@email.ua>
2023-03-24 12:48:57 +0200

    exhorse process now managed by StarDust

--- modules/remoteapi/exhorse.php
+++ modules/remoteapi/exhorse.php
@@ -3,10 +3,16 @@
 //existential horse
 if ($_GET['action'] == 'exhorse') {
     if ($alterconf['EXHORSE_ENABLED']) {
-        $exhorse = new ExistentialHorse();
-        $exhorse->runHorse();
-
-        die('OK: EXHORSE');
+        $existentialHorseProcess = new StarDust('EXHORSE');
+        if ($existentialHorseProcess->notRunning()) {
+            $existentialHorseProcess->start();
+            $exhorse = new ExistentialHorse();
+            $exhorse->runHorse();
+            $existentialHorseProcess->stop();
+            die('OK: EXHORSE');
+        } else {
+            die('SKIP:EXHORSE_ALREADY_RUNNING');
+        }
     } else {
         die('ERROR: EXHORSE DISABLED');
     }
