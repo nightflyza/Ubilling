86474f9ea01c68060451c37fe2959eac04a215d7
Nightfly <nightfly@email.ua>
2023-10-18 14:54:23 +0300

    sudden stoat!

--- api/libs/api.pononumap.php
+++ api/libs/api.pononumap.php
@@ -233,6 +233,60 @@ class PONONUMap {
         return($result);
     }
 
+//
+//       .,,.                                                                   
+//       ,KMKkkkkkkkklll. ..                             ..       .,,,.         
+//    .lkKKl,,,,,,,lkkKMKkKkll. ..                 ..,l,lKKkkkkkkkKMMKl.        
+//   .lKKl.           .,,lkkKMKkKk,. ,l.  ,l. ,l. .kKKKkkl,,,,,,,,,lkKMKkl.     
+// .,kMk.   .,,,,,.         .,,lkKMKkKMKkkKMklKMKkKKl,.       .,,,.  .,kMKl.    
+// .kMk.    .,lkKMKkkl.        ...,lKMMk,llkMMklKKl.  .... .lkKKl,.    .kMl     
+// .kMl         .,,lKMKkl.  .. lKl. .ll.   .ll. ..  ,lkKKklKMKl.        lmk.    
+//.kMk.             .lKMMKllKKkKKKl                .kMKKMMMMMl..        lmk.    
+// ,KK,              .KMKKKKklkKl..                 ,l..,lKKKKKx,l,    ,KKl.    
+//.lKK,          ..,,lMK,....  ..                         ..lKkKMMl .. lMKl.    
+// lMl         ..lKKKKMK,                                   .. lKKKlkl .kMk.    
+//.kMl         lKKKKMk,.                                       ...lKMK, lMk.    
+// lMk.      ,lkMk.,l.                                             ,lkl,KK,     
+// ,KMl     .kkll,                     .,.                ..         ..,Kk,.    
+// .kMk.    lMl      .lkkkkkkkl,,.    ,KMl                lk,     .lkkkkKMMK,   
+//  ,KMl    ,l.     .lklkMk,,,lKMKkl,lKMMl                lMKl,,lkkkl,,kMMMMk.  
+//   lMK.               lMl    .,lkkKMKl,.                .lKMMKkl.   .kMKKMK,  
+//   ,KMKl.             lMk.       .kMl                     ,KMk.  .,lKKl.,KMk. 
+// .,lKKkl.             ,KMKl,,,,,lKKl.      .lkkkkkkkkkl.   ,KMKkkKKkl.  lMK,  
+// ,KMKk,.               .lkkkkkkkkl.        lMMMMMMMMMMMl    .,,,,,.     ,KMk. 
+// .lKMMK,                                   .kMMMMMMMMMMl                 lMMl 
+//  .lkKMk..l,                .ll,.           .lKMMMMMMkl.       .,ll.     lMK. 
+// .,,lKKl.lMKkl.            ,KK,               .,lKMKl.           ,KK,    lMK, 
+// .lKMMk,..,,,,.            lMl                  .kMl              lMl   ,KMMK,
+//   ,KMMK.   ,Kl            lMl          .,,,,,lkKMMKkl,,.         lMl   lMKkl.
+//  .kMkl.   ,KK,            lMl          .kMMMMKKMMMMKKMMKl.      ,KMl   lMKl. 
+//  .lkKKkkl.lMKkkl.         ,KKl.         .lkKMkkMKKMkkMk,.     .lKMk.   .kMMl 
+//     .lKMMl.,,,,. ..        ,KMKkl.         .lKKl..lKKl.      .kKkl.   ,kKMK, 
+//      .kMMKk,    .kl.,,.     .,,lkl.   ..     ..    ..  ..   .ll.   .,,kMMk.  
+//     .lKMMMKl,,,.lMKKKl.               lKl.           .lKl          lMKKMK,   
+//    .kMMKkkkkkKMl.lkk,                 .kMKl,.     .,lKKl.       .lkKMl.,.    
+//   .lKMMk.    lMKkKMMKk.,,              .lkKMKkkkkkKMKl.      ,k,lKKMK,       
+//     ,KKl. .. .,,,,kMKl.lK, .,,.           .,,,,,,,,,.   ,;,lkKMKk,.,.        
+//   .lKMk. .kl.l,   .,.  lMKkKKKklkk,.,,,,.             .,kMMKKMk,.            
+//   .lKMK,.kMKkk,        ,kl,,.lMKlkKKKkKMK,.,,lllllkkkkkKKll,lMl              
+//   .lKMk..,,,.                .,. lKl. .ll.lKKMKkl,lKKl...  .kMKl.            
+//  .lKMk.                   ,l. .. ..       ...l,    ..     ,KMKl,.            
+//  ,KMKl.  ..               lMklKl                          lMMk.              
+// .lKMkl.  lk.,l.  .lx,     ,kMKl.                                             
+// .lKk.    lMKKMl lKKl     
+//
+//
+//
+//
+//                    ,d                            ,d     
+//                    88                            88     
+//        ,adPPYba, MM88MMM ,adPPYba,  ,adPPYYba, MM88MMM  
+//        I8[    ""   88   a8"     "8a ""     `Y8   88     
+//         `"Y8ba,    88   8b       d8 ,adPPPPP88   88     
+//        aa    ]8I   88,  "8a,   ,a8" 88,    ,88   88,    
+//        `"YbbdP"'   "Y888 `"YbbdP"'  `"8bbdP"Y8   "Y888  
+//    
+
     /**
      * Returns a list of placemarks to render
      * 
