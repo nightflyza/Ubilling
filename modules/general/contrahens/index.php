c9ae0430f54f4863071892e8f2d07cd97035af4c
bobr-kun <demon.bobr@gmial.com>
2023-10-27 01:52:31 +0300

    Contragents:     Extended info recs now can be removed

--- modules/general/contrahens/index.php
+++ modules/general/contrahens/index.php
@@ -71,11 +71,14 @@ if (cfr('AGENTS')) {
                                    ubRouting::post('extinfintpaysysid'), ubRouting::post('extinfintpaysyssrvid'),
                                    ubRouting::post('extinfintpaysystoken'), ubRouting::post('extinfintpaysyskey'),
                                    ubRouting::post('extinfintpaysyspasswd'));
+        //  add extended agent info
         } elseif (ubRouting::checkPost('extinfeditmode', false) and ubRouting::checkPost('extinfagentid')) {
             zb_CreateAgentExtInfoRec(ubRouting::post('extinfagentid'), ubRouting::post('extinfsrvtype'),
                                      ubRouting::post('extinfintpaysysname'), ubRouting::post('extinfintpaysysid'),
                                      ubRouting::post('extinfintpaysyssrvid'), ubRouting::post('extinfintpaysystoken'),
                                      ubRouting::post('extinfintpaysyskey'), ubRouting::post('extinfintpaysyspasswd'));
+        } elseif (ubRouting::checkGet('delete')) {
+            zb_DeleteAgentExtInfoRec(ubRouting::get('delete'));
         }
 
         show_window(__('Extended info'), zb_RenderAgentExtInfoTable(ubRouting::get('extinfo')) .
