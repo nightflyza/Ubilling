b299ede0348194dcc1640ad1d63fcb21678c4558
bobr-kun <demon.bobr@gmial.com>
2023-11-13 20:15:29 +0200

    Updated release .SQL file to match the current release index

---
+++ content/updates/sql/1.4.2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `contrahens_extinfo` ADD `paysys_token` VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE `contrahens_extinfo` ADD `paysys_secret_key` VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE `contrahens_extinfo` ADD `paysys_password` VARCHAR(255) NOT NULL DEFAULT '';
