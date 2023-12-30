304005083b4affa062b9287e6bdf1aa26a4573f9
Nightfly <nightfly@email.ua>
2023-12-30 16:36:06 +0200

    begin of OphanimFlow integration

---
+++ api/libs/api.ophanimflow.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * OphanimFlow integration implementation
+ */
+class OphanimFlow {
+
+    //some predefined stuff here
+    const TABLE_TRAFFDATA = 'oph_nostromo';
+
+    public function __construct() {
+        // BE NOT AFRAID
+    }
+}
--- content/updates/sql/1.4.3.sql
+++ content/updates/sql/1.4.3.sql
@@ -1,1 +1,11 @@
-ALTER TABLE `envydevices` ADD `port` INT NULL DEFAULT NULL AFTER `cutend`; 
+ALTER TABLE `envydevices` ADD `port` INT NULL DEFAULT NULL AFTER `cutend`;
+
+CREATE TABLE IF NOT EXISTS `oph_nostromo` (
+  `login` VARCHAR(50) DEFAULT NULL,
+  `month` tinyint(4) DEFAULT NULL,
+  `year` SMALLINT(6) DEFAULT NULL,
+  `U0` BIGINT(20) DEFAULT NULL,
+  `D0` BIGINT(20) DEFAULT NULL,
+  `cash` DOUBLE DEFAULT NULL,
+  KEY `login` (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
