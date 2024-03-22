a11fc6d5d6792bdf80fa59cea4f7423a0735b5a2
bobr-kun <demon.bobr@gmial.com>
2024-03-22 19:30:18 +0200

    XMLAgent:     forgotten request exclusion

--- userstats/modules/engine/api.xmlagent.php
+++ userstats/modules/engine/api.xmlagent.php
@@ -178,6 +178,7 @@ class XMLAgent {
                                     'opayz',
                                     'agentassigned',
                                     'tariffvservices',
+                                    'activetariffsvservices',
                                     'feecharges',
                                     'ticketcreate'
                                     ),
