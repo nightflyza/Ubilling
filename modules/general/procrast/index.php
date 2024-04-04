34b49a13b5cb83be1fb6bfc60050064e2300e364
Nightfly <nightfly@email.ua>
2024-04-04 18:37:18 +0300

    procrast: all apps now are remotely invokable

--- modules/general/procrast/index.php
+++ modules/general/procrast/index.php
@@ -2,51 +2,6 @@
 
 if (cfr('PROCRAST')) {
 
-    if (ubRouting::checkGet('run')) {
-        $application = ubRouting::get('run', 'vf');
-        switch ($application) {
-            case 'tetris':
-                $jsTetris = file_get_contents('modules/jsc/procrastdata/jstetris/tetris.html');
-                $jsTetris = str_replace('START_LABEL', __('Press space to play'), $jsTetris);
-                $jsTetris = str_replace('SCORE_LABEL', __('score'), $jsTetris);
-                $jsTetris = str_replace('ROWS_LABEL', __('rows'), $jsTetris);
-                show_window(__('Tetris'), $jsTetris);
-                break;
-            case '2048':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/2048/2048.html');
-                show_window(__('2048'), $jsCode);
-                break;
-            case 'motox3m':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/motox3m.html');
-                show_window(__('Moto X3M'), $jsCode, 'center');
-                break;
-            case 'astray':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/astray/run.html');
-                show_window(__('Astray'), $jsCode, 'center');
-                break;
-            case 'hextris':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/hextris/run.html');
-                show_window(__('Hextris'), $jsCode, 'center');
-                break;
-            case 'duckhunt':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/duckhunt.html');
-                show_window(__('Duck hunt'), $jsCode, 'center');
-                break;
-            case 'circus':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/circus.html');
-                show_window(__('Circus Charlie'), $jsCode, 'center');
-                break;
-            case 'pixeldefense':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/pixeldefense.html');
-                show_window(__('PixelDefense'), $jsCode, 'center');
-                break;
-            case 'doom':
-                $jsCode = file_get_contents('modules/jsc/procrastdata/doom.html');
-                show_window(__('Doom'), $jsCode, 'center');
-                break;
-        }
-        show_window('', wf_BackLink('?module=procrast'));
-    } else {
         //CDN invokable apps here
         $cachingTimeout = 86400;
         $cdnUrl = 'https://cdn.ubilling.net.ua/';
@@ -92,25 +47,7 @@ if (cfr('PROCRAST')) {
         } else {
             //rendering apps list
             $applicationsList = '';
-            $applicationArr = array(
-                'tetris' => __('Tetris'),
-                '2048' => __('2048'),
-                'astray' => __('Astray'),
-                'hextris' => __('Hextris'),
-                'duckhunt' => __('Duck hunt'),
-                'motox3m' => __('Moto X3M'),
-                'circus' => __('Circus Charlie'),
-                'pixeldefense' => __('PixelDefense'),
-            );
-
-
-            if (!empty($applicationArr)) {
-                foreach ($applicationArr as $io => $each) {
-                    $applicationsList .= zb_buildGameIcon('?module=procrast&run=' . $io, $io . '.png', $each);
-                }
-            }
-
-
+            
             if (!empty($invocableApps)) {
                 foreach ($invocableApps as $io => $each) {
                     $applicationsList .= zb_buildGameIcon($routeInvoke . $io, $cdnUrl . $each['icon'], $each['name']);
@@ -120,7 +57,7 @@ if (cfr('PROCRAST')) {
             $applicationsList .= wf_CleanDiv();
             show_window(__('Procrastination helper'), $applicationsList);
         }
-    }
+    
     zb_BillingStats(true);
 } else {
     show_error(__('Access denied'));
