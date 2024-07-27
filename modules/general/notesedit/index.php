73142a782e7fed8bbaff252cae20b71d22c3f789
Nightfly <nightfly@email.ua>
2024-07-27 14:21:04 +0300

    few fixes

--- modules/general/notesedit/index.php
+++ modules/general/notesedit/index.php
@@ -1,12 +1,11 @@
 <?php
 
 if (cfr('NOTES')) {
-
-    if (isset($_GET['username'])) {
-        $login = vf($_GET['username']);
+    if (ubRouting::checkGet('username')) {
+        $login = ubRouting::get('username', 'login');
         // change notes if need
-        if (isset($_POST['newnotes'])) {
-            $notes = $_POST['newnotes'];
+        if (ubRouting::checkPost('newnotes', false)) {
+            $notes = ubRouting::post('newnotes', 'safe');
             zb_UserDeleteNotes($login);
             zb_UserCreateNotes($login, $notes);
             rcms_redirect("?module=notesedit&username=" . $login);
@@ -16,7 +15,7 @@ if (cfr('NOTES')) {
         $useraddress = zb_UserGetFullAddress($login) . ' (' . $login . ')';
 
 
-// Edit form construct
+        // Edit form construct
         $fieldnames = array('fieldname1' => __('Current notes'), 'fieldname2' => __('New notes'));
         $fieldkey = 'newnotes';
         $form = web_EditorStringDataForm($fieldnames, $fieldkey, $useraddress, $current_notes);
@@ -24,16 +23,15 @@ if (cfr('NOTES')) {
 
         show_window(__('Edit notes'), $form);
 
-//additional notes
+        //additional notes
         $altCfg = $ubillingConfig->getAlter();
         if ($altCfg['ADCOMMENTS_ENABLED']) {
             $adcomments = new ADcomments('USERNOTES');
             show_window(__('Additional comments'), $adcomments->renderComments($login));
         }
-//user controls here
+        //user controls here
         show_window('', web_UserControls($login));
     }
 } else {
     show_error(__('You cant control this module'));
 }
-?>
