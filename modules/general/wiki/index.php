aacc22b4b3c3bcc12f5f1c775554b0ffea938996
Nightfly <nightfly@email.ua>
2024-08-07 14:54:12 +0300

    WIKI_URL option behaviour improved, ?blank suffix now opens link in new tab

--- modules/general/wiki/index.php
+++ modules/general/wiki/index.php
@@ -5,7 +5,19 @@ if (cfr('TASKBAR')) {
     if (@$altCfg['WIKI_URL']) {
         $wikiUrl = $altCfg['WIKI_URL'];
         if (!empty($wikiUrl)) {
-            rcms_redirect($wikiUrl);
+            if (ispos($wikiUrl, '?blank')) {
+                $wikiUrl = str_replace('?blank', '', $wikiUrl);
+                $backUrl = (@$_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+                $redirect = wf_tag('script');
+                $redirect .= 'window.open("' . $wikiUrl . '", "_blank");';
+                if ($backUrl) {
+                    $redirect .= 'window.open("' . $backUrl . '", "_self");';
+                }
+                $redirect .= wf_tag('script', true);
+                show_window(__('Redirecting'), $redirect);
+            } else {
+                ubRouting::nav($wikiUrl);
+            }
         } else {
             show_error('WIKI_URL ' . __('is empty'));
         }
