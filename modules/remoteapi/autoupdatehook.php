bed65cbf2641b61fb5f10fc4a480a82102e6d2c7
Nightfly <nightfly@email.ua>
2024-09-06 16:00:28 +0300

    alter.ini: UPDMGR_DEFADM_KILL option implemented, RemoteAPI: autoupdatehook now runs postautoupgrade OnePunch script if its exists

--- modules/remoteapi/autoupdatehook.php
+++ modules/remoteapi/autoupdatehook.php
@@ -1,5 +1,37 @@
 <?php
 
 if (ubRouting::get('action') == 'autoupdatehook') {
- die('OK:DUMMY');
+    log_register('UPDMGR AUTOUPDATEHOOK STARTED');
+
+    //optional default administrator account cleanup
+    $defaultAdminLogin = 'admin';
+    if ($ubillingConfig->getAlterParam('UPDMGR_DEFADM_KILL')) {
+        if (file_exists(USERS_PATH . $defaultAdminLogin)) {
+            user_delete($defaultAdminLogin);
+            if (!file_exists(USERS_PATH . $defaultAdminLogin)) {
+                log_register('UPDMGR KILL {' . $defaultAdminLogin . '} SUCCESS');
+            } else {
+                log_register('UPDMGR KILL {' . $defaultAdminLogin . '} FAILED');
+            }
+        } else {
+            log_register('UPDMGR KILL {' . $defaultAdminLogin . '} NOT_EXISTS');
+        }
+    } else {
+        log_register('UPDMGR KILL {' . $defaultAdminLogin . '} SKIPPED');
+    }
+
+    //custom post-upgrage script execution
+    $postUpgradeScriptAlias = 'postautoupgrade';
+    $onePunch = new OnePunch($postUpgradeScriptAlias);
+    $postUpgradeScriptCode = $onePunch->getScriptContent($postUpgradeScriptAlias);
+    if (!empty($postUpgradeScriptCode)) {
+        log_register('UPDMGR OPPOSTSCRIPT RUN');
+        eval($postUpgradeScriptCode);
+        log_register('UPDMGR OPPOSTSCRIPT EXECUTED');
+    } else {
+        log_register('UPDMGR OPPOSTSCRIPT EMPTY');
+    }
+
+    log_register('UPDMGR AUTOUPDATEHOOK FINISHED');
+    die('OK:AUTOUPDATEHOOK');
 }
