af675ff6084424773ff585287b221c11deeb1b91
bobr-kun <demon.bobr@gmial.com>
2024-10-14 21:05:43 +0300

    AgentsExtInfo: Added new field "payment_fee_info"
    OpenPayz:
        Added ability to consider payment fee settings for Providex frontend

--- modules/general/contrahens/index.php
+++ modules/general/contrahens/index.php
@@ -71,14 +71,15 @@ if (cfr('AGENTS')) {
                                    ubRouting::post('extinfsrvtype'), ubRouting::post('extinfintpaysysname'),
                                    ubRouting::post('extinfintpaysysid'), ubRouting::post('extinfintpaysyssrvid'),
                                    ubRouting::post('extinfintpaysystoken'), ubRouting::post('extinfintpaysyskey'),
-                                   ubRouting::post('extinfintpaysyspasswd'), ubRouting::post('extinfintpaysyscallbackurl'));
+                                   ubRouting::post('extinfintpaysyspasswd'), ubRouting::post('extinfintpayfeeinfo'),
+                                   ubRouting::post('extinfintpaysyscallbackurl'));
         //  add extended agent info
         } elseif (ubRouting::checkPost('extinfeditmode', false) and ubRouting::checkPost('extinfagentid')) {
             zb_CreateAgentExtInfoRec(ubRouting::post('extinfagentid'), ubRouting::post('extinfsrvtype'),
                                      ubRouting::post('extinfintpaysysname'), ubRouting::post('extinfintpaysysid'),
                                      ubRouting::post('extinfintpaysyssrvid'), ubRouting::post('extinfintpaysystoken'),
                                      ubRouting::post('extinfintpaysyskey'), ubRouting::post('extinfintpaysyspasswd'),
-                                     ubRouting::post('extinfintpaysyscallbackurl'));
+                                     ubRouting::post('extinfintpayfeeinfo'), ubRouting::post('extinfintpaysyscallbackurl'));
         } elseif (ubRouting::checkGet('delete')) {
             zb_DeleteAgentExtInfoRec(ubRouting::get('delete'));
         }
