f3e6db58b99cd99d25f3edb2fc15b0551d97283f
Pautina <pautina@pitline.net>
2024-10-29 21:31:42 +0200

    add translate for module contrahen

---
+++ languages/ukrainian/contrahens.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['def']['Automatic assigns'] = 'Автоматичні присвоєння';
+$lang['def']['assignments'] = 'присвоєнь';
+$lang['def']['Show automatic agent assignments'] = 'Показати автоматичні призначення агентів';
