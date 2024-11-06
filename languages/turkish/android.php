e85bdb21a2854b4022d0b5077fa11ef9dda4afa7
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:37:32 +0300

    Create android.php

---
+++ languages/turkish/android.php
@@ -0,0 +1,5 @@
+<?php
+$lang['def']['Right to use android integration API'] = 'Android entegrasyon APIsini kullanma hakkı';
+$lang['def']['Right to use android API debug information'] = 'Android API hata ayıklama bilgisini kullanma hakkı';
+$lang['def']['Username cannot be empty'] = 'Kullanıcı adı boş olamaz';
+?>
