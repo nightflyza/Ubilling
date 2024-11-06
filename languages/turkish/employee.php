2aea93fbb6293b0aec72abc5be80bcd2919b6050
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:53:33 +0300

    Create employee.php

---
+++ languages/turkish/employee.php
@@ -0,0 +1,4 @@
+<?php
+$lang['def']['Monthly top up limit'] = 'Aylık bakiye yükleme limiti';
+$lang['def']['Create new employee'] = 'Yeni çalışan oluştur';
+$lang['def']['Employee real name'] = 'Çalışanın tam adı';
