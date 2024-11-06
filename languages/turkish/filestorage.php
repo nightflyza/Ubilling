67dca3344440f1a4ac6e78bbe7e885356bfab9c0
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 21:14:37 +0300

    Create filestorage.php

---
+++ languages/turkish/filestorage.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['def']['File upload'] = 'Dosya yükleme';
+$lang['def']['File upload complete'] = 'Dosya yükleme tamamlandı';
+$lang['def']['File upload failed'] = 'Dosya yükleme başarısız oldu';
+$lang['def']['right to use filestorage'] = 'Dosya deposunu kullanma hakkı';
+$lang['def']['right to delete filestorage files'] = 'Dosya deposundan dosya silme hakkı';
+$lang['def']['Filestorage'] = 'Dosya deposu';
