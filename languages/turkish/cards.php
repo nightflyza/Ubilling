2104f43c339ea70d0c9d4d196a8089dd38bd5a3c
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:47:47 +0300

    Create cards.php

---
+++ languages/turkish/cards.php
@@ -0,0 +1,8 @@
+<?php
+$lang['def']['Serial number of the card with duplicates'] = 'Yinelenen kartların seri numarası';
+$lang['def']['There are duplicate serial numbers of cards'] = 'Yinelenen kart seri numaraları mevcut';
+$lang['def']['Serial number length'] = 'Seri numarası uzunluğu';
+$lang['def']['Count of cards cannot be empty'] = 'Kart sayısı boş olamaz';
+$lang['def']['Price of cards cannot be empty'] = 'Kartların fiyatı boş olamaz';
+
+?>
