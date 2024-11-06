0c29ea895a2044988cd34d9874ccb8b220fe0d59
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:35:34 +0300

    Create address_exten.php

---
+++ languages/turkish/address_exten.php
@@ -0,0 +1,6 @@
+<?
+$lang['def']['Postal code'] = 'Posta kodu';
+$lang['def']['Town/District/Region'] = 'İlçe/Bölge/Şehir';
+$lang['def']['Extended address'] = 'Ek adres';
+$lang['def']['Extended address info'] = 'Adres ile ilgili ek bilgi';
+?>
