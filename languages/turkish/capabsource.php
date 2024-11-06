b77cac78f2d99a91dacfca5fd6182ebc1503d8d7
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:46:18 +0300

    Create capabsource.php

---
+++ languages/turkish/capabsource.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['def']['Capabylity source'] = 'Başvuru kaynağı';
+$lang['def']['Previously used'] = 'Daha önce kullanıldı';
+$lang['def']['Friends / relatives'] = 'Arkadaşlar veya akrabalar';
+$lang['def']['Social networks'] = 'Sosyal ağlar';
+$lang['def']['Advertising'] = 'Reklam';
+$lang['def']['Direct sales department'] = 'Doğrudan satış departmanı';
+$lang['def']['Saw employee'] = 'Çalışan görüldü';
+$lang['def']['We dont know'] = 'Bilmiyoruz';
+$lang['def']['Telemarketing'] = 'Tele-pazarlama';
+$lang['def']['Billboard / Citylight'] = 'Billboard / şehir ışığı';
+$lang['def']['Newspaper'] = 'Gazete';
+$lang['def']['Using at another address'] = 'Başka bir adreste kullanılıyor';
+$lang['def']['Front office'] = 'Ön ofis';
