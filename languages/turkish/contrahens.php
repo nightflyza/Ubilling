9dffeda26badf203ca0dd571a120598265452108
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:49:03 +0300

    Create contrahens.php

---
+++ languages/turkish/contrahens.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['def']['Automatic assigns'] = 'Otomatik atamalar';
+$lang['def']['assignments'] = 'atamalar';
+$lang['def']['Show automatic agent assignments'] = 'Otomatik ajan atamalarını göster';
