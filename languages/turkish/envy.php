d09fc3c6d1be6a792a4d205b46ffc8a6b359d7ed
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:56:20 +0300

    Create envy.php

---
+++ languages/turkish/envy.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['def']['Envy'] = 'Envy';
+$lang['def']['Available envy scripts'] = 'Mevcut envy scriptleri';
+$lang['def']['Create new script'] = 'Yeni script oluştur';
+$lang['def']['Create new device'] = 'Yeni cihaz oluştur';
+$lang['def']['Enable password'] = 'şifre etkinleştir';
+$lang['def']['Custom field'] = 'Özel alan';
+$lang['def']['No envy devices available'] = 'Mevcut envy cihazı yok';
+$lang['def']['Available envy devices'] = 'Mevcut envy cihazları';
+$lang['def']['Scripts'] = 'Scriptler';
+$lang['def']['Previously collected devices configs'] = 'Daha önce toplanmış cihaz yapılandırmaları';
+$lang['def']['Backup device configuration to archive'] = 'Cihaz yapılandırmasını arşive yedekle';
+$lang['def']['Store all devices configs into archive'] = 'Tüm cihaz yapılandırmalarını arşive kaydet';
+$lang['def']['Store all'] = 'Hepsini kaydet';
+$lang['def']['right to control envy'] = 'Envy kontrol hakkı';
+$lang['def']['Changes'] = 'Değişiklikler';
+$lang['def']['Unknown'] = 'Bilinmeyen';
+$lang['def']['Compare'] = 'Karşılaştır';
+$lang['def']['Same configs selected'] = 'Aynı yapılandırmalar seçildi';
+$lang['def']['No difference between this two configurations'] = 'Bu iki yapılandırma arasında fark yok';
+$lang['def']['Something is different in this two configurations'] = 'Bu iki yapılandırma arasında bir fark var';
+$lang['def']['Latest config'] = 'En son yapılandırma';
+$lang['def']['All configs for each of devices will be deleted from the archive except the last one'] = 'Her cihazın arşivdeki tüm yapılandırmaları, sonuncusu hariç silinecek';
+$lang['def']['Lines to cut at start'] = 'Başlangıçta kesilecek satırlar';
+$lang['def']['Lines to cut at end'] = 'Bitişte kesilecek satırlar';
+$lang['def']['Envy script available'] = 'Envy scripti mevcut';
+$lang['def']['Envy script unavailable'] = 'Envy scripti mevcut değil';
