6bff3ce442924824bd8e636836b238172fe931eb
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:50:23 +0300

    Create covid.php

---
+++ languages/turkish/covid.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['def']['Confirmed'] = 'Onaylandı';
+$lang['def']['Deaths'] = 'Ölümler';
+$lang['def']['Recovered'] = 'İyileşenler';
+$lang['def']['Country'] = 'Ülke';
+$lang['def']['right to view COVID-19 cases'] = 'COVID-19 vakalarını görüntüleme hakkı';
+$lang['def']['World'] = 'Dünya';
+$lang['def']['For the last day'] = 'Son 24 saatte';
