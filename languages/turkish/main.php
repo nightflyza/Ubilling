0448a43825637dbdab9e449648c56f5a1bd9f280
Ali Çömez | Slaweally <sys@rootali.net>
2024-11-06 19:31:23 +0300

    Create main.php

---
+++ languages/turkish/main.php
@@ -0,0 +1,89 @@
+////////////////////////////////////////////////////////////////////////////////
+//   Copyright (C) ReloadCMS Development Team                                 //
+//   http://reloadcms.sf.net                                                  //
+//                                                                            //
+//   Bu program faydalı olacağı ümidiyle dağıtılmaktadır, ancak               //
+//   TİCARİ ELVERİŞLİLİK veya BELİRLİ BİR AMACA UYGUNLUK garantisi olmaksızın //
+//   dağıtılmaktadır.                                                          //
+//                                                                            //
+//   Bu ürün GNU Genel Kamu Lisansı v2 kapsamında yayınlanmıştır.             //
+////////////////////////////////////////////////////////////////////////////////
+
+/*********************************************************************************
+* Saat Dilimleri                                                                *
+*********************************************************************************/
+$lang['tz']['-12'] = 'GMT - 12 Saat';
+$lang['tz']['-11'] = 'GMT - 11 Saat';
+$lang['tz']['-10'] = 'GMT - 10 Saat';
+$lang['tz']['-9'] = 'GMT - 9 Saat';
+$lang['tz']['-8'] = 'GMT - 8 Saat';
+$lang['tz']['-7'] = 'GMT - 7 Saat';
+$lang['tz']['-6'] = 'GMT - 6 Saat';
+$lang['tz']['-5'] = 'GMT - 5 Saat';
+$lang['tz']['-4'] = 'GMT - 4 Saat';
+$lang['tz']['-3.5'] = 'GMT - 3.5 Saat';
+$lang['tz']['-3'] = 'GMT - 3 Saat';
+$lang['tz']['-2'] = 'GMT - 2 Saat';
+$lang['tz']['-1'] = 'GMT - 1 Saat';
+$lang['tz']['0'] = 'GMT';
+$lang['tz']['1'] = 'GMT + 1 Saat';
+$lang['tz']['2'] = 'GMT + 2 Saat';
+$lang['tz']['3'] = 'GMT + 3 Saat';
+$lang['tz']['3.5'] = 'GMT + 3.5 Saat';
+$lang['tz']['4'] = 'GMT + 4 Saat';
+$lang['tz']['4.5'] = 'GMT + 4.5 Saat';
+$lang['tz']['5'] = 'GMT + 5 Saat';
+$lang['tz']['5.5'] = 'GMT + 5.5 Saat';
+$lang['tz']['6'] = 'GMT + 6 Saat';
+$lang['tz']['6.5'] = 'GMT + 6.5 Saat';
+$lang['tz']['7'] = 'GMT + 7 Saat';
+$lang['tz']['8'] = 'GMT + 8 Saat';
+$lang['tz']['9'] = 'GMT + 9 Saat';
+$lang['tz']['9.5'] = 'GMT + 9.5 Saat';
+$lang['tz']['10'] = 'GMT + 10 Saat';
+$lang['tz']['11'] = 'GMT + 11 Saat';
+$lang['tz']['12'] = 'GMT + 12 Saat';
+$lang['tz']['13'] = 'GMT + 13 Saat';
+
+/*********************************************************************************
+* Tarih Yerelleştirme                                                            *
+*********************************************************************************/
+$lang['datetime']['Sunday'] = 'Pazar';
+$lang['datetime']['Monday'] = 'Pazartesi';
+$lang['datetime']['Tuesday'] = 'Salı';
+$lang['datetime']['Wednesday'] = 'Çarşamba';
+$lang['datetime']['Thursday'] = 'Perşembe';
+$lang['datetime']['Friday'] = 'Cuma';
+$lang['datetime']['Saturday'] = 'Cumartesi';
+$lang['datetime']['Sun'] = 'Paz';
+$lang['datetime']['Mon'] = 'Pts';
+$lang['datetime']['Tue'] = 'Sal';
+$lang['datetime']['Wed'] = 'Çar';
+$lang['datetime']['Thu'] = 'Per';
+$lang['datetime']['Fri'] = 'Cum';
+$lang['datetime']['Sat'] = 'Cts';
+$lang['datetime']['January'] = 'Ocak';
+$lang['datetime']['February'] = 'Şubat';
+$lang['datetime']['March'] = 'Mart';
+$lang['datetime']['April'] = 'Nisan';
+$lang['datetime']['May'] = 'Mayıs';
+$lang['datetime']['June'] = 'Haziran';
+$lang['datetime']['July'] = 'Temmuz';
+$lang['datetime']['August'] = 'Ağustos';
+$lang['datetime']['September'] = 'Eylül';
+$lang['datetime']['October'] = 'Ekim';
+$lang['datetime']['November'] = 'Kasım';
+$lang['datetime']['December'] = 'Aralık';
+$lang['datetime']['Jan'] = 'Oca';
+$lang['datetime']['Feb'] = 'Şub';
+$lang['datetime']['Mar'] = 'Mar';
+$lang['datetime']['Apr'] = 'Nis';
+$lang['datetime']['May'] = 'May';
+$lang['datetime']['Jun'] = 'Haz';
+$lang['datetime']['Jul'] = 'Tem';
+$lang['datetime']['Aug'] = 'Ağu';
+$lang['datetime']['Sep'] = 'Eyl';
+$lang['datetime']['Oct'] = 'Eki';
+$lang['datetime']['Nov'] = 'Kas';
+$lang['datetime']['Dec'] = 'Ara';
+?>
