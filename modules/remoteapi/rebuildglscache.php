83b1fd7a16dade2645217b2a8e2ee6c74a524157
Nightfly <nightfly@email.ua>
2025-03-15 15:35:09 +0200

    global search cache rebuild now controlled by stardust

--- modules/remoteapi/rebuildglscache.php
+++ modules/remoteapi/rebuildglscache.php
@@ -1,13 +1,20 @@
 <?php
 
-/*
+/**
  * GlobalSearch cache rebuild
  */
 if (ubRouting::get('action') == 'rebuildglscache') {
     if (!$ubillingConfig->getAlterParam('SPHINX_SEARCH_ENABLED')) {
-        $globalSearch = new GlobalSearch();
-        $globalSearch->ajaxCallback(true);
-        die('OK:REBUILDGLSCACHE');
+        $rebuildProcess = new StarDust('GLSREBUILD');
+        if ($rebuildProcess->notRunning()) {
+            $rebuildProcess->start();
+            $globalSearch = new GlobalSearch();
+            $globalSearch->ajaxCallback(true);
+            $rebuildProcess->stop();
+            die('OK:REBUILDGLSCACHE');
+        } else {
+            die('SKIPPED:ALREADY_RUNNING');
+        }
     } else {
         die('SKIPPED:SPHINX_SEARCH_ENABLED');
     }
