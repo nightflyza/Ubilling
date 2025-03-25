1f074f961c4a0761ec9ee79ee50797b4a6bfbe51
Nightfly <nightfly@email.ua>
2025-03-25 16:58:03 +0200

    AUTO_LOGOUT_IDLE code cleanup

--- api/libs/api.idlelogout.php
+++ api/libs/api.idlelogout.php
@@ -4,35 +4,82 @@
  * Basic user-idle auto logout class
  */
 class AutoLogout {
-    
-    const LOGOUT_URL='?idleTimerAutoLogout=true';
-    
+    /**
+     * Contains raw value of idle config option in minutes
+     *
+     * @var int
+     */
+    protected $idleLogout = 0;
+
+    /**
+     * Contains list of excluded admins if set
+     *
+     * @var array
+     */
+    protected $excludedAdmins = array();
+
+    //some predefined stuff
+    const LOGOUT_URL = '?idleTimerAutoLogout=true';
+    const OPTION_IDLE = 'AUTO_LOGOUT_IDLE';
+    const OPTION_EXCLUDE = 'AUTO_LOGOUT_EXCLUDE';
+
+
+    //         .-.-.
+    //    ((  (__I__)  ))
+    //      .'_....._'.
+    //     / / .12 . \ \
+    //    | | '  |  ' | |
+    //    | | 9  /  3 | |
+    //     \ \ '.6.' / /
+    //      '.`-...-'.'
+    //       /'-- --'\
+    //      `"""""""""`    
+    public function __construct() {
+        $this->setOptions();
+    }
+
+    /**
+     * Sets current instance properties
+     *
+     * @return void
+     */
+    protected function setOptions() {
+        global $ubillingConfig;
+        $this->idleLogout = $ubillingConfig->getAlterParam(self::OPTION_IDLE, 0);
+        if ($this->idleLogout) {
+            $excludeRaw = $ubillingConfig->getAlterParam(self::OPTION_EXCLUDE, '');
+            if (!empty($excludeRaw)) {
+                $excludeRaw =  explode(',', $excludeRaw);
+                $this->excludedAdmins =  array_flip($excludeRaw);
+            }
+        }
+    }
+
     /**
      * returns logout dialog localised template
      * 
      * @return string
      */
     protected function createDialog() {
-        $autoLogoutTimerContainer=  wf_tag('div', false, '', 'id="idledialog" title="'.__('Your session is about to expire!').'"');
-        $autoLogoutTimerContainer.= wf_tag('span', false, 'ui-icon ui-icon-alert', 'style="float:left; margin:0 7px 50px 0;"').wf_tag('span', true);
-        $autoLogoutTimerContainer.= __('You will be logged off in').' ';
-        $autoLogoutTimerContainer.= wf_tag('span', false, '', 'id="dialog-countdown" style="font-weight:bold"').wf_tag('span', true);
-        $autoLogoutTimerContainer.= ' '. __('seconds').  wf_delimiter();
-        $autoLogoutTimerContainer.= wf_tag('center',false).  wf_tag('img', false, '', 'src="skins/idleicon.gif" width="160"').  wf_tag('center',true);
-        $autoLogoutTimerContainer.=  wf_tag('div', true);
+        $autoLogoutTimerContainer =  wf_tag('div', false, '', 'id="idledialog" title="' . __('Your session is about to expire!') . '"');
+        $autoLogoutTimerContainer .= wf_tag('span', false, 'ui-icon ui-icon-alert', 'style="float:left; margin:0 7px 50px 0;"') . wf_tag('span', true);
+        $autoLogoutTimerContainer .= __('You will be logged off in') . ' ';
+        $autoLogoutTimerContainer .= wf_tag('span', false, '', 'id="dialog-countdown" style="font-weight:bold"') . wf_tag('span', true);
+        $autoLogoutTimerContainer .= ' ' . __('seconds') .  wf_delimiter();
+        $autoLogoutTimerContainer .= wf_tag('center', false) .  wf_tag('img', false, '', 'src="skins/idleicon.gif" width="160"') .  wf_tag('center', true);
+        $autoLogoutTimerContainer .=  wf_tag('div', true);
         return ($autoLogoutTimerContainer);
     }
-    
+
     /**
      * returns JQuery subroutine for auto-logout
      * 
      * @return string
      */
-    protected function createTimer($idle) {
-        $idle=$idle*60;
-        $autoLogoutTimerJs='
-           <script type="text/javascript">
-                // setup the dialog
+    protected function createTimer() {
+        $idle = $this->idleLogout * 60; //in seconds
+        $autoLogoutTimerJs = wf_tag('script', false, '', 'type="text/javascript"');
+        $autoLogoutTimerJs .= '
                 $("#idledialog").dialog({
                 autoOpen: false,
                 modal: true,
@@ -42,97 +89,73 @@ class AutoLogout {
                 draggable: false,
                 resizable: false,
                 buttons: {
-                        \''.__('Yes, keep working').'\': function(){
-                        $(this).dialog(\'close\');
+                        \'' . __('Yes, keep working') . '\': function(){
+                             $(this).dialog(\'close\');
                         },
-                        \''.__('No, logout').'\': function(){
-                        $.idleTimeout.options.onTimeout.call(this);
+                        \'' . __('No, logout') . '\': function(){
+                           $.idleTimeout.options.onTimeout.call(this);
                         }
                 }
                 });
 
-            // cache a reference to the countdown element so we don\'t have to query the DOM for it on each ping.
+           
             var $countdown = $("#dialog-countdown");
 
-            // start the idle timer plugin
             $.idleTimeout(\'#idledialog\', \'div.ui-dialog-buttonpane button:first\', {
-            idleAfter: '.$idle.',
+            idleAfter: ' . $idle . ',
             pollingInterval: 50,
             keepAliveURL: \'RELEASE\',
             serverResponseEquals: \'OK\',
+
             onTimeout: function(){
-            window.location = "'.self::LOGOUT_URL.'";
+                 window.location = "' . self::LOGOUT_URL . '";
             },
+
             onIdle: function(){
-            $(this).dialog("open");
+                $(this).dialog("open");
             },
+
             onCountdown: function(counter){
-            $countdown.html(counter); // update the counter
+                $countdown.html(counter);
             }
            });
-           
-        </script>
         ';
-        
+        $autoLogoutTimerJs .= wf_tag('script', true);
         return ($autoLogoutTimerJs);
-
     }
-    
+
     /**
      * renders idle timeout auto logout scripts id needed
      * 
      * @return string
      */
-      public function render() {
-        global $ubillingConfig;
-        $altCfg=$ubillingConfig->getAlter();
-        $excludeAdmins=array();
-        $result='';
-        
-        if (isset($altCfg['AUTO_LOGOUT_IDLE'])) {
-            if ($altCfg['AUTO_LOGOUT_IDLE']) {
-                 $myLogin=whoami();
-                 $idleTimeout=$altCfg['AUTO_LOGOUT_IDLE'];
-                 if (file_exists(USERS_PATH.$myLogin)) {
-                   if (isset($altCfg['AUTO_LOGOUT_EXCLUDE'])) {
-                       //extract exclude admins
-                       if (!empty($altCfg['AUTO_LOGOUT_EXCLUDE'])) {
-                           $excludeAdmins=  explode(',', $altCfg['AUTO_LOGOUT_EXCLUDE']);
-                           $excludeAdmins=  array_flip($excludeAdmins);
-                       }
-                       //push timer script
-                       if (!isset($excludeAdmins[$myLogin])) {
-                       $result=$this->createDialog();
-                       $result.=  $this->createTimer($altCfg['AUTO_LOGOUT_IDLE']);
-                       } 
-                   } 
-                 }
-                   
+    public function render() {
+        $result = '';
+        if ($this->idleLogout) {
+            $myLogin = whoami();
+            if (file_exists(USERS_PATH . $myLogin)) {
+                //push timer script
+                if (!isset($this->excludedAdmins[$myLogin])) {
+                    $result = $this->createDialog();
+                    $result .=  $this->createTimer();
+                }
             }
         }
-      
-        
+
         return ($result);
-      }
-    
+    }
 }
 
 /**
- * run idle auto logout object into template
+ * Runs idle auto logout object into template
  * 
  * @return string
  */
 function zb_IdleAutologoutRun() {
-    $result='';
-        if (LOGGED_IN) {
-        $idleTimer=new AutoLogout();
-        $result=$idleTimer->render();
+    $result = '';
+    if (LOGGED_IN) {
+        $idleTimer = new AutoLogout();
+        $result = $idleTimer->render();
     }
-    return($result);
+    return ($result);
 }
-
-
-
-
-
-?>
