dc6b9085295bbd83be5d092e915351fb5d07bb80
Nightfly <nightfly@email.ua>
2025-06-06 18:02:37 +0300

    wf_JuiComboBox now looks better

--- api/libs/api.astral.php
+++ api/libs/api.astral.php
@@ -2342,162 +2342,154 @@ function wf_JuiComboBox($name, $params, $label, $selected = '', $br = false) {
 
     if (!empty($params)) {
         foreach ($params as $io => $each) {
-            $flag_selected = (!empty($selected) and $selected == $io) ? 'SELECTED' : '';
+            $flag_selected = (!empty($selected) && $selected == $io) ? 'SELECTED' : '';
             $select .= '<option value="' . $io . '" ' . $flag_selected . '>' . $each . '</option>' . "\n";
         }
     }
 
     $result = '
-
- <style>
+<style>
 .custom-combobox_' . $id . ' {
-position: relative;
-display: inline-block;
+    position: relative;
+    display: inline-block;
 }
 .custom-combobox-toggle_' . $id . ' {
-position: absolute;
-top: 0;
-bottom: 0;
-margin-left: -1px;
-padding: 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin-left: -1px;
+    padding: 0;
 }
 .custom-combobox-input_' . $id . ' {
-margin: 0;
-padding: 5px 10px;
+    margin: 0;
+    padding: 5px 10px;
 }
-
 .ui-autocomplete {
     max-height: 400px;
-    overflow-y: auto;   /* prevent horizontal scrollbar */
-    overflow-x: hidden; /* add padding to account for vertical scrollbar */
-    z-index:1000 !important;
+    overflow-y: auto;
+    overflow-x: hidden;
+    z-index: 1000 !important;
 }
 </style>
 <script>
-(function( $ ) {
-$.widget( "custom.combobox_' . $id . '", {
-_create: function() {
-this.wrapper = $( "<span>" )
-.addClass( "custom-combobox_' . $id . '" )
-.insertAfter( this.element );
-this.element.hide();
-this._createAutocomplete();
-this._createShowAllButton();
-},
-_createAutocomplete: function() {
-var selected = this.element.children( ":selected" ),
-value = selected.val() ? selected.text() : "";
-this.input = $( "<input>" )
-.appendTo( this.wrapper )
-.val( value )
-.attr( "title", "" )
-.addClass( "custom-combobox-input_' . $id . ' ui-widget_' . $id . ' ui-widget-content ui-state-default ui-corner-left" )
-.autocomplete({
-delay: 0,
-minLength: 0,
-source: $.proxy( this, "_source" )
-})
-.tooltip({
-tooltipClass: "ui-state-highlight"
-});
-this._on( this.input, {
-autocompleteselect: function( event, ui ) {
-ui.item.option.selected = true;
-this._trigger( "select", event, {
-item: ui.item.option
-});
-},
-autocompletechange: "_removeIfInvalid"
-});
-},
-_createShowAllButton: function() {
-var input = this.input,
-wasOpen = false;
-$( "<a>" )
-.attr( "tabIndex", -1 )
-.attr( "title", "' . __('Show all') . '" )
-.tooltip()
-.appendTo( this.wrapper )
-.button({
-icons: {
-primary: "ui-icon-triangle-1-s"
-},
-text: false
-})
-.removeClass( "ui-corner-all" )
-.addClass( "custom-combobox-toggle_' . $id . ' ui-corner-right" )
-.mousedown(function() {
-wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-})
-.click(function() {
-input.focus();
-// Close if already visible
-if ( wasOpen ) {
-return;
-}
-// Pass empty string as value to search for, displaying all results
-input.autocomplete( "search", "" );
-});
-},
-_source: function( request, response ) {
-var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-response( this.element.children( "option" ).map(function() {
-var text = $( this ).text();
-if ( this.value && ( !request.term || matcher.test(text) ) )
-return {
-label: text,
-value: text,
-option: this
-};
-}) );
-},
-_removeIfInvalid: function( event, ui ) {
-// Selected an item, nothing to do
-if ( ui.item ) {
-return;
-}
-// Search for a match (case-insensitive)
-var value = this.input.val(),
-valueLowerCase = value.toLowerCase(),
-valid = false;
-this.element.children( "option" ).each(function() {
-if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-this.selected = valid = true;
-return false;
-}
-});
-// Found a match, nothing to do
-if ( valid ) {
-return;
-}
-
-this.input.autocomplete( "instance" ).term = "";
-},
-_destroy: function() {
-this.wrapper.remove();
-this.element.show();
-}
-});
-})( jQuery );
+(function($) {
+    $.widget("custom.combobox_' . $id . '", {
+        _create: function() {
+            this.wrapper = $("<span>")
+                .addClass("custom-combobox_' . $id . '")
+                .insertAfter(this.element);
+            this.element.hide();
+            this._createAutocomplete();
+            this._createShowAllButton();
+        },
+        _createAutocomplete: function() {
+            var selected = this.element.children(":selected"),
+                value = selected.val() ? selected.text() : "";
+            this.input = $("<input>")
+                .appendTo(this.wrapper)
+                .val(value)
+                .attr("title", "")
+                .addClass("custom-combobox-input_' . $id . ' ui-widget_' . $id . ' ui-widget-content ui-state-default ui-corner-left")
+                .autocomplete({
+                    delay: 0,
+                    minLength: 0,
+                    source: $.proxy(this, "_source")
+                })
+                .tooltip({
+                    tooltipClass: "ui-state-highlight"
+                });
+            this._on(this.input, {
+                autocompleteselect: function(event, ui) {
+                    ui.item.option.selected = true;
+                    this._trigger("select", event, {
+                        item: ui.item.option
+                    });
+                },
+                autocompletechange: "_removeIfInvalid"
+            });
+        },
+        _createShowAllButton: function() {
+            var input = this.input,
+                wasOpen = false;
+            $("<a>")
+                .attr("tabIndex", -1)
+                .attr("title", "' . __('Show all') . '")
+                .tooltip()
+                .appendTo(this.wrapper)
+                .button({
+                    icons: {
+                        primary: "ui-icon-triangle-1-s"
+                    },
+                    text: false
+                })
+                .removeClass("ui-corner-all")
+                .addClass("custom-combobox-toggle_' . $id . ' ui-corner-right")
+                .mousedown(function() {
+                    wasOpen = input.autocomplete("widget").is(":visible");
+                })
+                .click(function() {
+                    input.focus();
+                    if (wasOpen) {
+                        return;
+                    }
+                    input.autocomplete("search", "");
+                });
+        },
+        _source: function(request, response) {
+            var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+            response(this.element.children("option").map(function() {
+                var text = $(this).text();
+                if (this.value && (!request.term || matcher.test(text))) {
+                    return {
+                        label: text,
+                        value: text,
+                        option: this
+                    };
+                }
+            }));
+        },
+        _removeIfInvalid: function(event, ui) {
+            if (ui.item) {
+                return;
+            }
+            var value = this.input.val(),
+                valueLowerCase = value.toLowerCase(),
+                valid = false;
+            this.element.children("option").each(function() {
+                if ($(this).text().toLowerCase() === valueLowerCase) {
+                    this.selected = valid = true;
+                    return false;
+                }
+            });
+            if (valid) {
+                return;
+            }
+            this.input.autocomplete("instance").term = "";
+        },
+        _destroy: function() {
+            this.wrapper.remove();
+            this.element.show();
+        }
+    });
+})(jQuery);
 
 $(function() {
-$( "#combobox_' . $id . '" ).combobox_' . $id . '();
+    $("#combobox_' . $id . '").combobox_' . $id . '();
 });
 </script>
 
-
 <div class="ui-widget_' . $id . '">
-<label for="combobox_' . $id . '">' . $label . '</label>
-<select id="combobox_' . $id . '" name=' . $name . '>
-' . $select . '
-</select>
-</div>
-';
+    <label for="combobox_' . $id . '">' . $label . '</label>
+    <select id="combobox_' . $id . '" name="' . $name . '">
+        ' . $select . '
+    </select>
+</div>';
+
     if ($br) {
         $result .= wf_tag('br');
     }
 
-    return ($result);
+    return $result;
 }
 
 /**
