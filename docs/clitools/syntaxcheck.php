3a8d4b8d92454c59d49b45f259bfd9b00856486b
Nightfly <nightfly@email.ua>
2025-11-21 15:11:38 +0200

    syntaxcheck now checks main and us libs

--- docs/clitools/syntaxcheck.php
+++ docs/clitools/syntaxcheck.php
@@ -5,29 +5,71 @@ require_once(__DIR__ . '/../../api/libs/api.workaround.php');
 require_once(__DIR__ . '/../../modules/system/filesystem.php');
 require_once(__DIR__ . '/../../api/libs/api.ubrouting.php');
 
+$errorCount = 0;
+$baseLibsPath= __DIR__ . '/../../';
+$mainLibsPath = 'api/libs/';
+$userstatsLibsPath ='userstats/modules/engine/';
+$failedLibs = array();
+$allLibsToCheck = array();
 
-if (ubRouting::optionCliCheck('run', false)) {
-    $errorCount = 0;
-    $ubLibsPath = __DIR__ . '/../../api/libs/';
-    $failedLibs = array();
 
-    $allUbLibs = rcms_scandir($ubLibsPath, '*.php');
-    if (!empty($allUbLibs)) {
-        $allUbLibs = array_flip($allUbLibs);
-        foreach ($allUbLibs as $eachUbLib => $index) {
-            $lintResult = shell_exec('php -l ' . $ubLibsPath . $eachUbLib.' 2>&1');
+if (ubRouting::optionCliCheck('main', false) or ubRouting::optionCliCheck('us', false) or ubRouting::optionCliCheck('runall', false)) {
+
+    if (ubRouting::optionCliCheck('main', false)) {
+        $mainLibsToCheck = rcms_scandir($baseLibsPath . $mainLibsPath, '*.php');
+        if (!empty($mainLibsToCheck)) {
+            foreach ($mainLibsToCheck as $index=>$eachUbLib) {
+                $allLibsToCheck[$mainLibsPath.$eachUbLib] = $baseLibsPath . $mainLibsPath . $eachUbLib;
+            }
+        }
+    }
+
+    if (ubRouting::optionCliCheck('us', false)) {
+        $userstatsLibsToCheck = rcms_scandir($baseLibsPath . $userstatsLibsPath, '*.php');
+        if (!empty($userstatsLibsToCheck)) {
+            foreach ($userstatsLibsToCheck as $index=>$eachUbLib) {
+                $allLibsToCheck[$userstatsLibsPath.$eachUbLib] = $baseLibsPath . $userstatsLibsPath . $eachUbLib;
+            }
+        }
+    }
+
+    if (ubRouting::optionCliCheck('runall', false)) {
+        $mainLibsToCheck = rcms_scandir($baseLibsPath . $mainLibsPath, '*.php');
+        if (!empty($mainLibsToCheck)) {
+            foreach ($mainLibsToCheck as $index=>$eachUbLib) {
+                $subMain[$mainLibsPath.$eachUbLib] = $baseLibsPath . $mainLibsPath . $eachUbLib;
+            }
+        }
+
+        $userstatsLibsToCheck = rcms_scandir($baseLibsPath . $userstatsLibsPath, '*.php');
+        if (!empty($userstatsLibsToCheck)) {
+            foreach ($userstatsLibsToCheck as $index=>$eachUbLib) {
+                $subUs[$userstatsLibsPath.$eachUbLib] = $baseLibsPath . $userstatsLibsPath . $eachUbLib;
+            }
+        }
+
+        $allLibsToCheck = array_merge($subMain, $subUs);
+    }
+
+    
+    if (!empty($allLibsToCheck)) {
+        foreach ($allLibsToCheck as $index=>$eachUbLib) {
+            $lintResult = shell_exec('php -l ' . $eachUbLib.' 2>&1');
+            $libLabel=$index;
             if (ispos($lintResult, 'PHP ')) {
                 $errorCount++;
-                $failedLibs[] = $eachUbLib;
-                print('⚠️ FAILED: ' . $eachUbLib . PHP_EOL);
+                $failedLibs[] = $libLabel;
+                print('⚠️ FAILED: ' . $libLabel . PHP_EOL);
                 print('🔍 Details:' . PHP_EOL);
                 print('=========================' . PHP_EOL);
                 print($lintResult.PHP_EOL);
                 print('=========================' . PHP_EOL);
             } else {
-                print('✅ OK: ' . $eachUbLib . PHP_EOL);
+                print('✅ OK: ' . $libLabel . PHP_EOL);
             }
         }
+    } else {
+        print('❌ No libs found' . PHP_EOL);
     }
 
     //summary here
@@ -43,5 +85,5 @@ if (ubRouting::optionCliCheck('run', false)) {
         print('✨ Everything is Ok' . PHP_EOL);
     }
 } else {
-    print('ℹ️ Usage: php ./docs/clitools/syntaxcheck.php --run' . PHP_EOL);
+    print('ℹ️ Usage: php ./docs/clitools/syntaxcheck.php --[main|us|runall]' . PHP_EOL);
 }
