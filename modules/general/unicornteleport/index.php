3ee238eb731c427afd905c112cddc4f9a0de0216
Nightfly <nightfly@email.ua>
2025-11-25 01:08:04 +0200

    check of host system added

--- modules/general/unicornteleport/index.php
+++ modules/general/unicornteleport/index.php
@@ -3,13 +3,18 @@
 error_reporting(E_ALL);
 if (cfr('ROOT')) {
     set_time_limit(0);
-    $teleport = new UnicornTeleport();
-    if (ubRouting::checkGet(UnicornTeleport::ROUTE_DOWNLOAD)) {
-        $teleport->catchFileDownload();
+    $sysInfo=new SystemHwInfo();
+    $hostOs=$sysInfo->getOs();
+    if ($hostOs=='FreeBSD') {
+        $teleport = new UnicornTeleport();
+        if (ubRouting::checkGet(UnicornTeleport::ROUTE_DOWNLOAD)) {
+            $teleport->catchFileDownload();
+        } else {
+            show_window(__('Unicorn Teleport'), $teleport->renderTeleportForm());
+        }
     } else {
-        show_window(__('Unicorn Teleport'), $teleport->renderTeleportForm());
+        show_error(__('Sorry your system is currently unsupported'));
     }
-
     show_window('', wf_BackLink(DevConsole::URL_ME));
 } else {
     show_error(__('Access denied'));
