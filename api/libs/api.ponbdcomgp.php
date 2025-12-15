8e1252dae8f1602a321c1528bf8e3d5fd15a6254
Nightfly <nightfly@email.ua>
2025-12-15 11:42:06 +0200

    UNIOPERSTATUS and DEREGREASON implemented for PONBdcomGP by @mazaxaaka. Merged as is, not tested

--- api/libs/api.ponbdcomgp.php
+++ api/libs/api.ponbdcomgp.php
@@ -11,6 +11,10 @@ class PONBdcomGP extends PONBdcom {
      * @return void
      */
     public function collect() {
+        global $ubillingConfig;
+        $this->ubConfig = $ubillingConfig;
+        $this->onuUniStatusEnabled = $this->ubConfig->getAlterParam('PON_ONU_UNI_STATUS_ENABLED', false);
+
         $oltModelId = $this->oltParameters['MODELID'];
         $oltid = $this->oltParameters['ID'];
         $oltIp = $this->oltParameters['IP'];
@@ -55,6 +59,14 @@ class PONBdcomGP extends PONBdcom {
                     }
                 }
             }
+            // Get UniOperStatusIndex
+            if (isset($this->snmpTemplates[$oltModelId]['misc']['UNIOPERSTATUS']) and $this->onuUniStatusEnabled) {
+                $uniOperStatusIndexOID = $this->snmpTemplates[$oltModelId]['misc']['UNIOPERSTATUS'];
+                $uniOperStatusIndex = $this->snmp->walk($oltIp . ':' . self::SNMPPORT, $oltCommunity, $uniOperStatusIndexOID, self::SNMPCACHE);
+                $uniOperStatusIndex = str_replace($uniOperStatusIndexOID . '.', '', $uniOperStatusIndex);
+                $uniOperStatusIndex = str_replace(array($this->snmpTemplates[$oltModelId]['misc']['UNIOPERSTATUSVALUE'], '"'), '', $uniOperStatusIndex);
+                $uniOperStatusIndex = explodeRows($uniOperStatusIndex);
+            }
         }
 
         // getting others system data from OLT
@@ -95,13 +107,23 @@ class PONBdcomGP extends PONBdcom {
                     $this->distanceParseGPBd($distIndex, $onuIndex);
                     // processing interfaces data
                     $this->interfaceParseBd($intIndex);
+                    // processing UniOperStauts
+                    if (isset($this->snmpTemplates[$oltModelId]['misc']['UNIOPERSTATUS']) and $this->onuUniStatusEnabled and !empty($uniOperStatusIndex)) {
+                        $this->uniParseGPBd($uniOperStatusIndex);
+                    }
                     // processing FDB data
                     if (!$oltNoFDBQ) {
                         $this->FDBParseGPBd($FDBIndex, $oltModelId);
                     }
 
-                    if (isset($this->snmpTemplates[$oltModelId]['misc']['DEREGREASON'])) {
                     // processing last dereg reason data
+                    if (isset($this->snmpTemplates[$oltModelId]['misc']['DEREGREASON'])) {
+                        $deregIndexOid = $this->snmpTemplates[$oltModelId]['misc']['DEREGREASON'];
+                        $deregIndex = $this->snmp->walk($oltIp . ':' . self::SNMPPORT, $oltCommunity, $deregIndexOid, self::SNMPCACHE);
+                        $deregIndex = str_replace($deregIndexOid . '.', '', $deregIndex);
+                        $deregIndex = str_replace($this->snmpTemplates[$oltModelId]['misc']['DEREGVALUE'], '', $deregIndex);
+                        $deregIndex = explodeRows($deregIndex);
+
                         $this->lastDeregParseBd($deregIndex);
                     }
                 }
@@ -231,6 +253,112 @@ class PONBdcomGP extends PONBdcom {
                 // saving ONUs cache
                 $this->olt->writeOnuCache($onuTmp);
             }
+        }
+    }
+
+    /**
+     * Parses & stores in cache OLT ONU dereg reaesons (GPON-specific BDCOM)
+     *
+     * @param array $deregIndex
+     *
+     * @return void
+     */
+    protected function lastDeregParseBd($deregIndex) {
+        $result = array();
+
+        // deregistration reasons preprocessing
+        $DeregReasonsMap = array(
+            0  => array('color' => '"#000000"', 'text' => 'No Reason'),
+            1  => array('color' => '"#6500FF"', 'text' => 'Power off'),
+            2  => array('color' => '"#000000"', 'text' => 'Laser always on'),
+            3  => array('color' => '"#FF4400"', 'text' => 'Admin Down'),
+            4  => array('color' => '"#000000"', 'text' => 'omcc-down'),
+            5  => array('color' => '"#000000"', 'text' => 'Unknown'),
+            6  => array('color' => '"#000000"', 'text' => 'pon-los'),
+            7  => array('color' => '"#000000"', 'text' => 'lcdg'),
+            8  => array('color' => '"#F80000"', 'text' => 'Wire down'),
+            9  => array('color' => '"#000000"', 'text' => 'omci-mismatch'),
+            10 => array('color' => '"#000000"', 'text' => 'password-mismatch'),
+            11 => array('color' => '"#000000"', 'text' => 'Reboot'),
+            12 => array('color' => '"#000000"', 'text' => 'ranging-failed')
+        );
+
+        if ((!empty($deregIndex)) and ( !empty($this->onuDevIndexProcessed))) {
+            foreach ($deregIndex as $io => $eachdereg) {
+                $line = explode('=', $eachdereg);
+                
+                //dereg is present
+                if (isset($line[1])) {
+                    $deregRaw = trim($line[1]); // dereg
+                    $devIndex = trim($line[0]); // device index
+                    
+                    $deregCode = (int)$deregRaw;
+                   
+                    // Setting default values
+                    $TxtColor = '"#000000"';
+                    $tmpONULastDeregReasonStr = 'Unknown reason';
+
+                    // Check if there is code in the mapping
+                    if (isset($DeregReasonsMap[$deregCode])) {
+                        $TxtColor = $DeregReasonsMap[$deregCode]['color'];
+                        $tmpONULastDeregReasonStr = $DeregReasonsMap[$deregCode]['text'];
+                    }
+
+                    // Using wf_tag for formatting
+                    $tmpONULastDeregReasonStr = wf_tag('font', false, '', 'color=' . $TxtColor . '') .
+                            $tmpONULastDeregReasonStr .
+                            wf_tag('font', true);
+
+                    // Store the index in serial number format
+                    if (isset($this->onuDevIndexProcessed[$devIndex])) {
+                        $result[$this->onuDevIndexProcessed[$devIndex]] = $tmpONULastDeregReasonStr;
+                    }
+                }
+            }
+
+            // saving dereg reasons cache
+            $this->olt->writeDeregs($result);
+        }
+    }
+    /**
+     * Performs UNI port oper status preprocessing for GPON BDCOM (GP3608)
+     *
+     * @param array $uniOperStatusIndex
+     *
+     * @return void
+     */
+    protected function uniParseGPBd($uniOperStatusIndex) {
+        $result = array();
+
+        if (!empty($this->onuDevIndexProcessed) && !empty($uniOperStatusIndex)) {
+            foreach ($uniOperStatusIndex as $io => $eachRow) {
+                $line = explode('=', $eachRow);
+                if (empty($line[0]) || empty($line[1])) {
+                    continue;
+                }
+
+                $ifIndex = trim(ltrim($line[0], '.'));
+                $statusRaw = trim($line[1]);
+
+                // Extract a number from a string of type "up(1)" or "down(2)"
+                if (preg_match('/\((\d+)\)/', $statusRaw, $matches)) {
+                    $tmpUniStatus = (int)$matches[1];
+                } else {
+                    $tmpUniStatus = (int)$statusRaw; // in case it's already a pure number
+                }
+
+                // Convert to 1/0
+                $tmpUniStatus = ($tmpUniStatus == 1) ? 1 : 0;
+
+                // Mapping ifIndex -> ONU Serial
+                if (isset($this->onuDevIndexProcessed[$ifIndex])) {
+                    $onuSerial = $this->onuDevIndexProcessed[$ifIndex];
+                    $result[$onuSerial]['eth1'] = $tmpUniStatus;
+                }
+            }
+
+            // saving UniOperStats
+            $this->olt->writeUniOperStats($result);
         }
     }
 }
