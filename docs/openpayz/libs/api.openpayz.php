9990f3cc628f02f5e590c1f8fbd1b5c86e42e654
Nightfly <nightfly@email.ua>
2025-12-26 14:41:25 +0200

    fixed Deprecated: Function xml_parser_free() is deprecated since 8.5

--- docs/openpayz/libs/api.openpayz.php
+++ docs/openpayz/libs/api.openpayz.php
@@ -104,7 +104,9 @@ function xml2array($contents, $get_attributes = 1, $priority = 'tag') {
     xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
     xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
     xml_parse_into_struct($parser, trim($contents), $xml_values);
-    xml_parser_free($parser);
+    if (PHP_VERSION_ID < 80000) {
+     xml_parser_free($parser); // Deprecated in PHP 8.5
+    }
 
     if (!$xml_values) {
         return; //Hmm...
