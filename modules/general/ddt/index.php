5efb64d7fdf0ddf9bd31132eb4ee3e3b6762b9c3
Nightfly <nightfly@email.ua>
2026-03-24 16:43:28 +0200

    ddt: license not required

--- modules/general/ddt/index.php
+++ modules/general/ddt/index.php
@@ -4,87 +4,74 @@ if (cfr('DDT')) {
     $altCfg = $ubillingConfig->getAlter();
     if (@$altCfg['DDT_ENABLED']) {
         if (@$altCfg['DEALWITHIT_ENABLED']) {
-            $greed = new Avarice();
-            $avidity = $greed->runtime('DOOMSDAYTARIFFS');
-            if (!empty($avidity)) {
-                $ddt = new DoomsDayTariffs();
+            $ddt = new DoomsDayTariffs();
 
-                if (ubRouting::checkPost($avidity['V']['CS'])) {
-                    $avidity_m = $avidity['M']['ODIN'];
-                    $avidity['V']['CR'] = $ddt->$avidity_m();
-                    if (empty($avidity['V']['CR'])) {
-                        ubRouting::nav($ddt::URL_ME);
-                    } else {
-                        show_error($avidity['V']['CR']);
-                        show_window('', wf_BackLink($ddt::URL_ME));
-                    }
-                }
-
-                if (ubRouting::checkGet($avidity['V']['DT'])) {
-                    $avidity_m = $avidity['M']['CHIKA'];
-                    $avidity['V']['DR'] = $ddt->$avidity_m(ubRouting::get($avidity['V']['DT']));
-                    if (empty($avidity['V']['DR'])) {
-                        ubRouting::nav($ddt::URL_ME);
-                    } else {
-                        show_error($avidity['V']['DR']);
-                        show_window('', wf_BackLink($ddt::URL_ME));
-                    }
-                }
-
-                if (ubRouting::checkPost($ddt::PROUTE_CH_CREATE)) {
-                    $chRcR = $ddt->createChargeRule();
-                    if (empty($chRcR)) {
-                        ubRouting::nav($ddt::URL_ME);
-                    } else {
-                        show_error($chRcR);
-                        show_window('', wf_BackLink($ddt::URL_ME));
-                    }
-                }
-
-                if (ubRouting::checkGet($ddt::ROUTE_CH_DELETE)) {
-                    $chRdR = $ddt->deleteChargeRule(ubRouting::get($ddt::ROUTE_CH_DELETE));
-                    if (empty($chRdR)) {
-                        ubRouting::nav($ddt::URL_ME);
-                    } else {
-                        show_error($chRdR);
-                        show_window('', wf_BackLink($ddt::URL_ME));
-                    }
-                }
-
-                if (ubRouting::checkGet($ddt::ROUTE_CH_HISTAJX)) {
-                    $ddt->getChargesHistoryAjax();
-                }
-
-                $avidity_m = $avidity['M']['ASAKO'];
-                show_window('', $ddt->$avidity_m());
-
-                if (!ubRouting::checkGet($avidity['Y']['TWELVE'])) {
-                    $avidity_m = $avidity['M']['MEGUMIN'];
-                    show_window(__($avidity['L']['RAM']), $ddt->$avidity_m());
-                    show_window(__('Forced tariffs charge'), $ddt->renderChargeOpsList());
+            if (ubRouting::checkPost('createnewddtsignal')) {
+                $creationResult = $ddt->createTariffDDT();
+                if (empty($creationResult)) {
+                    ubRouting::nav($ddt::URL_ME);
                 } else {
-                    if (!ubRouting::checkGet('mode')) {
-                        if (ubRouting::checkGet($avidity['V']['SAD'])) {
-                            $avidity_m = $avidity['M']['BUTTRUE'];
-                            $ddt->$avidity_m();
-                        }
+                    show_error($creationResult);
+                    show_window('', wf_BackLink($ddt::URL_ME));
+                }
+            }
 
-                        $avidity_m = $avidity['M']['SUBARU'];
-                        show_window(__($avidity['L']['LYAK']), $ddt->$avidity_m(ubRouting::get($avidity['V']['ZHEKA'])));
-                    } else {
-                        $hMode=ubRouting::get('mode','gigasafe');
-                        switch ($hMode) {
-                            case 'fch':
-                                show_window(__('Forced charges history'),$ddt->renderChargesHistoryContainer());
-                                break;
-                        }
-                        
+            if (ubRouting::checkGet('deleteddtariff')) {
+                $deletionResult = $ddt->deleteTariffDDT(ubRouting::get('deleteddtariff'));
+                if (empty($deletionResult)) {
+                    ubRouting::nav($ddt::URL_ME);
+                } else {
+                    show_error($deletionResult);
+                    show_window('', wf_BackLink($ddt::URL_ME));
+                }
+            }
+
+            if (ubRouting::checkPost($ddt::PROUTE_CH_CREATE)) {
+                $chRcR = $ddt->createChargeRule();
+                if (empty($chRcR)) {
+                    ubRouting::nav($ddt::URL_ME);
+                } else {
+                    show_error($chRcR);
+                    show_window('', wf_BackLink($ddt::URL_ME));
+                }
+            }
+
+            if (ubRouting::checkGet($ddt::ROUTE_CH_DELETE)) {
+                $chRdR = $ddt->deleteChargeRule(ubRouting::get($ddt::ROUTE_CH_DELETE));
+                if (empty($chRdR)) {
+                    ubRouting::nav($ddt::URL_ME);
+                } else {
+                    show_error($chRdR);
+                    show_window('', wf_BackLink($ddt::URL_ME));
+                }
+            }
+
+            if (ubRouting::checkGet($ddt::ROUTE_CH_HISTAJX)) {
+                $ddt->getChargesHistoryAjax();
+            }
+
+            show_window('', $ddt->renderControls());
+
+            if (!ubRouting::checkGet('history')) {
+                show_window(__('Available doomsday tariffs'), $ddt->renderTariffsList());
+                show_window(__('Forced tariffs charge'), $ddt->renderChargeOpsList());
+            } else {
+                if (!ubRouting::checkGet('mode')) {
+                    if (ubRouting::checkGet('ajax')) {
+                        $ddt->getHistoryAjax();
+                    }
+
+                    show_window(__('History'), $ddt->renderHistoryContainer(ubRouting::get('username')));
+                } else {
+                    $hMode = ubRouting::get('mode', 'gigasafe');
+                    switch ($hMode) {
+                        case 'fch':
+                            show_window(__('Forced charges history'), $ddt->renderChargesHistoryContainer());
+                            break;
                     }
                 }
-                zb_BillingStats(true);
-            } else {
-                show_error(__('No license key available'));
             }
+            zb_BillingStats(true);
         } else {
             show_error(__('Module') . ' "' . __('Deal with it') . '" ' . __('disabled'));
         }
