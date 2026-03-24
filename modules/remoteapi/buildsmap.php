27830b31a433a4af72815d54d3c5ed78084493e8
Nightfly <nightfly@email.ua>
2026-03-24 23:58:26 +0200

    remoteapi: buildsmap now using stardust

--- modules/remoteapi/buildsmap.php
+++ modules/remoteapi/buildsmap.php
@@ -3,7 +3,15 @@
 //periodic builds map cache update
 if (ubRouting::get('action') == 'buildsmap') {
     if ($alterconf['SWYMAP_ENABLED']) {
-        um_MapDrawBuilds();
+        $buildsMapProcess=new StarDust('BUILDSMAP');
+        if ($buildsMapProcess->notRunning()) {
+        $buildsMapProcess->start();
+         um_MapDrawBuilds();
+         $buildsMapProcess->stop();
+         die('OK:BUILDSMAP');
+        } else {
+            die('SKIP:BUILDSMAP_ALREADY_RUNNING');
+        }
     } else {
         die('ERROR: SWYMAP DISABLED');
     }
