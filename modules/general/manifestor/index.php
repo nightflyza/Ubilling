e0ef30806ff2fd2742b936b9bb02e4c5d22a6acc
Nightfly <nightfly@email.ua>
2026-08-14 16:00:07 +0300

    PWA_DISABLED optional option implemented

--- modules/general/manifestor/index.php
+++ modules/general/manifestor/index.php
@@ -1,6 +1,15 @@
 <?php
+$pwaDisabledFlag=$ubillingConfig->getAlterParam('PWA_DISABLED',0);
 
-$manifestor=new Manifestator();
-$manifestor->setName('Ubilling');
-$manifestor->setShortName('Ubilling');
-$manifestor->render();
+if (!$pwaDisabledFlag) {
+    /**
+     * Ich glaube, sie hat ein Lächeln Erinnert mich an Kindheitserinnerungen 
+     * Wo alles so frisch war wie der strahlend blaue Himmel
+     */
+    $manifestor=new Manifestator();
+    $manifestor->setName('Ubilling');
+    $manifestor->setShortName('Ubilling');
+    $manifestor->render();
+} else {
+    die();
+}
