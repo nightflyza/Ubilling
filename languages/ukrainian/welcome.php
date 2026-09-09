7ff0dba358af56e029d476d0b777dfcb1542bd20
Nightfly <nightfly@email.ua>
2026-09-09 14:15:59 +0300

    typo

--- languages/ukrainian/welcome.php
+++ languages/ukrainian/welcome.php
@@ -8,5 +8,5 @@ $lang['def']['Read documentation'] = 'Ознайомитись з докумен
 $lang['def']['Join our community chat'] = 'Приєднатись до чату спільноти';
 $lang['def']['Donate to Armed Forces of Ukraine'] = 'Задонатити на ЗСУ';
 $lang['def']['Dont show me this anymore'] = 'Не показуйте мені цього більше';
-$lang['def']['On behalf of the development team and everyone involved in the project, we would like to thank you for choosing Ubilling.'] = 'Від імені команди розробників та всіх хто приймає участь у проекті, ми хочемо подякувати вам за вибір Ubilling.';
+$lang['def']['On behalf of the development team and everyone involved in the project, we would like to thank you for choosing Ubilling.'] = 'Від імені команди розробників та всіх хто приймає участь у проєкті, ми хочемо подякувати вам за вибір Ubilling.';
 $lang['def']['We hope you enjoy using it as much as we enjoyed working on it.'] = 'Ми сподіваємось, що вам сподобається його використання, так само як нам сподобалось працювати над ним.';
